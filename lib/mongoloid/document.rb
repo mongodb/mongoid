a993388e7efde6b3b28ca62f54c4ed396414e1e8
Durran Jordan <durran@gmail.com>
2009-08-29 12:46:15 -0400

    Adding some basic doc in Document

--- lib/mongoloid/document.rb
+++ lib/mongoloid/document.rb
@@ -5,36 +5,46 @@ module Mongoloid
 
     class << self
 
+      # Get the XGen::Mongo::Collection associated with this Document.
       def collection
         @collection ||= Mongoloid.database.collection(self.class.to_s.downcase)
       end
 
+      # Create a new Document with the supplied attribtues, and insert it into the database.
       def create(attributes = nil)
         new(attributes).save
       end
-
+      
+      # Find all Documents for the given selector, or return all Documents if selector is nil.
       def find(selector = nil)
         collection.find(selector).collect { |doc| new(doc) }
       end
 
     end
 
+    # Get the XGen::Mongo::Collection associated with this Document.
     def collection
       self.class.collection
     end
 
+    # Get the XGen::Mongo::ObjectID associated with this object.
+    # This is in essence the primary key.
     def id
       @attributes[:_id]
     end
 
+    # Instantiate a new Document, setting the Document's attirbutes if given.
+    # If no attributes are provided, they will be initialized with an empty Hash.
     def initialize(attributes = nil)
       @attributes = attributes || {}
     end
 
+    # Returns true is the Document has not been persisted to the database, false if it has.
     def new_record?
       @attributes[:_id].nil?
     end
 
+    # Save this Document to the database, and return self.
     def save
       collection.save(@attributes)
       self
