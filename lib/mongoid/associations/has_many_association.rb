0f40ec603a5044bda4c512d40e9e4e0bd2a3a5c0
Durran Jordan <durran@gmail.com>
2009-09-23 23:00:11 -0400

    a little trimmy-trim-trim.

--- lib/mongoid/associations/has_many_association.rb
+++ lib/mongoid/associations/has_many_association.rb
@@ -2,17 +2,17 @@ module Mongoid #:nodoc:
   module Associations #:nodoc:
     class HasManyAssociation < DelegateClass(Array) #:nodoc:
 
-      # Creates the new association by finding the attributes in 
-      # the parent document with its name, and instantiating a 
-      # new document for each one found. These will then be put in an 
+      # Creates the new association by finding the attributes in
+      # the parent document with its name, and instantiating a
+      # new document for each one found. These will then be put in an
       # internal array.
       #
-      # This then delegated all methods to the array class since this is 
+      # This then delegated all methods to the array class since this is
       # essentially a proxy to an array itself.
       def initialize(association_name, document)
         @klass = association_name.to_s.classify.constantize
         attributes = document.attributes[association_name]
-        @documents = attributes ? attributes.collect do |attribute| 
+        @documents = attributes ? attributes.collect do |attribute|
           child = @klass.new(attribute)
           child.parent = document
           child
