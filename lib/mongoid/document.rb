a4d6b69d2f1527dcfbed99a6caadf433c8644c6c
Durran Jordan <durran@gmail.com>
2009-10-01 19:48:28 -0400

    Moving finders into their own module

--- lib/mongoid/document.rb
+++ lib/mongoid/document.rb
@@ -2,9 +2,7 @@ module Mongoid #:nodoc:
   class Document #:nodoc:
     include ActiveSupport::Callbacks
     include Validatable
-
-    AGGREGATE_REDUCE = "function(obj, prev) { prev.count++; }"
-    GROUP_BY_REDUCE = "function(obj, prev) { prev.group.push(obj); }"
+    extend Finders
 
     attr_reader :attributes, :parent
 
@@ -50,38 +48,6 @@ module Mongoid #:nodoc:
         end
       end
 
-      # Find all Documents in several ways.
-      # Model.find(:first, :attribute => "value")
-      # Model.find(:all, :attribute => "value")
-      def find(*args)
-        type, selector = args[0], args[1]
-        case type
-        when :all then find_all(selector[:conditions])
-        when :first then find_first(selector[:conditions])
-        else find_first(Mongo::ObjectID.from_string(type.to_s))
-        end
-      end
-
-      # Find a single Document given the passed selector, which is a Hash of attributes that
-      # must match the Document in the database exactly.
-      def find_first(selector = nil)
-        new(collection.find_one(selector))
-      end
-
-      # Find all Documents given the passed selector, which is a Hash of attributes that
-      # must match the Document in the database exactly.
-      def find_all(selector = nil)
-        collection.find(selector).collect { |doc| new(doc) }
-      end
-
-      # Find all Documents given the supplied criteria, grouped by the fields
-      # provided.
-      def group_by(fields, selector)
-        collection.group(fields, selector, { :group => [] }, GROUP_BY_REDUCE).collect do |docs|
-          group!(docs)
-        end
-      end
-
       # Create a one-to-many association between Documents.
       def has_many(association_name)
         add_association(:has_many, association_name.to_s.classify, association_name)
@@ -96,19 +62,6 @@ module Mongoid #:nodoc:
       # :unique => false. It will default to the latter.
       def index(name, options = { :unique => false })
         collection.create_index(name, options)
-      end
-
-      # Find all documents in paginated fashion given the supplied arguments.
-      # If no parameters are passed just default to offset 0 and limit 20.
-      def paginate(params = {})
-        WillPaginate::Collection.create(
-          params[:page] || 1,
-          params[:per_page] || 20,
-          0) do |pager|
-            results = collection.find(params[:conditions], { :limit => pager.per_page, :offset => pager.offset })
-            pager.total_entries = results.count
-            pager.replace(results.collect { |doc| new(doc) })
-        end
       end
 
     end
