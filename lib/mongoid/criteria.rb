1ed82503bb96ec235ed91ff7a56c1d2facd5abd2
Durran Jordan <durran@gmail.com>
2009-10-10 10:19:52 -0400

    Adding a whole bunch of doco to Criteria

--- lib/mongoid/criteria.rb
+++ lib/mongoid/criteria.rb
@@ -1,85 +1,265 @@
 module Mongoid #:nodoc:
-  class Criteria #:nodoc:
+  # The +Criteria+ class is the core object needed in Mongoid to retrieve
+  # objects from the database. It is a DSL that essentially sets up the
+  # selector and options arguments that get passed on to a <tt>Mongo::Collection</tt>
+  # in the Ruby driver. Each method on the +Criteria+ returns self to they
+  # can be chained in order to create a readable criterion to be executed
+  # against the database.
+  #
+  # Example setup:
+  #
+  # <tt>criteria = Criteria.new</tt>
+  #
+  # <tt>criteria.select(:field => "value").only(:field).skip(20).limit(20)</tt>
+  #
+  # <tt>criteria.execute</tt>
+  class Criteria
     attr_reader :selector, :options, :type
 
-    # Supply a hash of arrays for field values that must match every single
-    # element in the array.
+    # Adds a criterion to the +Criteria+ that specifies values that must all
+    # be matched in order to return results. Similar to an "in" clause but the
+    # underlying conditional logic is an "AND" and not an "OR". The MongoDB
+    # conditional operator that will be used is "$all".
+    #
+    # Options:
+    #
+    # selections: A +Hash+ where the key is the field name and the value is an 
+    # +Array+ of values that must all match.
+    #
+    # Example:
+    #
+    # <tt>criteria.all(:field => ["value1", "value2"])</tt>
+    #
+    # <tt>criteria.all(:field1 => ["value1", "value2"], :field2 => ["value1"])</tt>
+    #
+    # Returns: <tt>self</tt>
     def all(selections = {})
       selections.each { |key, value| @selector[key] = { "$all" => value } }; self
     end
 
-    # Excludes the parameters passed using $ne in the selector.
+    # Adds a criterion to the +Criteria+ that specifies values that are not allowed
+    # to match any document in the database. The MongoDB conditional operator that
+    # will be used is "$ne".
+    #
+    # Options:
+    #
+    # excludes: A +Hash+ where the key is the field name and the value is a
+    # value that must not be equal to the corresponding field value in the database.
+    #
+    # Example:
+    #
+    # <tt>criteria.excludes(:field => "value1")</tt>
+    #
+    # <tt>criteria.excludes(:field1 => "value1", :field2 => "value1")</tt>
+    #
+    # Returns: <tt>self</tt>
     def excludes(exclusions = {})
       exclusions.each { |key, value| @selector[key] = { "$ne" => value } }; self
     end
 
-    # Execute the criteria, which will retrieve the results from
-    # the collection.
+    # Execute the criteria. This will take the internally built selector and options
+    # and pass them on to the Ruby driver's +find()+ method on the collection. The
+    # collection itself will be retrieved from the class provided, and once the
+    # query has returned new documents of the type of class provided will be instantiated.
+    #
+    # If this is a +Criteria+ to only find the first object, this will return a
+    # single object of the type of class provided.
+    #
+    # If this is a +Criteria+ to find multiple results, will return an +Array+ of
+    # objects of the type of class provided.
     def execute(klass)
       return klass.new(klass.collection.find_one(@selector, @options)) if type == :first
       return klass.collection.find(@selector, @options).collect { |doc| klass.new(doc) }
     end
 
-    # Defines criteria for matching any of the supplied parameters, similar to
-    # a SQL in statement.
+    # Adds a criterion to the +Criteria+ that specifies values where any can
+    # be matched in order to return results. This is similar to an SQL "IN"
+    # clause. The MongoDB conditional operator that will be used is "$in".
+    #
+    # Options:
+    #
+    # inclusions: A +Hash+ where the key is the field name and the value is an
+    # +Array+ of values that any can match.
+    #
+    # Example:
+    #
+    # <tt>criteria.in(:field => ["value1", "value2"])</tt>
+    #
+    # <tt>criteria.in(:field1 => ["value1", "value2"], :field2 => ["value1"])</tt>
+    #
+    # Returns: <tt>self</tt>
     def in(inclusions = {})
       inclusions.each { |key, value| @selector[key] = { "$in" => value } }; self
     end
 
-    # Adds an _id criteria to the selector.
+    # Adds a criterion to the +Criteria+ that specifies an id that must be matched.
+    #
+    # Options:
+    #
+    # object_id: A +String+ representation of a <tt>Mongo::ObjectID</tt>
+    #
+    # Example:
+    #
+    # <tt>criteria.id("4ab2bc4b8ad548971900005c")</tt>
+    #
+    # Returns: <tt>self</tt>
     def id(object_id)
       @selector[:_id] = Mongo::ObjectID.from_string(object_id); self
     end
 
-    # Create the new Criteria object. Does not take any params, just
-    # initializes the selector and options hashes that will be 
-    # eventually passed to the driver.
+    # Create the new +Criteria+ object. This will initialize the selector
+    # and options hashes, as well as the type of criteria.
+    #
+    # Options:
+    #
+    # type: One of :all, :first:, or :last
     def initialize(type)
       @selector, @options, @type = {}, {}, type
     end
 
-    # Limits the number of results returned by the query, usually used in
-    # conjunction with skip() for pagination.
+    # Adds a criterion to the +Criteria+ that specifies the maximum number of
+    # results to return. This is mostly used in conjunction with <tt>skip()</tt>
+    # to handle paginated results.
+    #
+    # Options:
+    #
+    # value: An +Integer+ specifying the max number of results. Defaults to 20.
+    #
+    # Example:
+    #
+    # <tt>criteria.limit(100)</tt>
+    #
+    # Returns: <tt>self</tt>
     def limit(value = 20)
       @options[:limit] = value; self
     end
 
-    # The conditions that must prove true on each record in the
-    # database in order for them to be a part of the result set.
-    # This is a hash that maps to a selector in the driver.
+    # Adds a criterion to the +Criteria+ that specifies values that must
+    # be matched in order to return results. This is similar to a SQL "WHERE"
+    # clause. This is the actual selector that will be provided to MongoDB, 
+    # similar to the Javascript object that is used when performing a find()
+    # in the MongoDB console.
+    #
+    # Options:
+    #
+    # selectior: A +Hash+ that must match the attributes of the +Document+.
+    #
+    # Example:
+    #
+    # <tt>criteria.select(:field1 => "value1", :field2 => 15)</tt>
+    #
+    # Returns: <tt>self</tt>
     def select(selector = {})
       @selector = selector; self
     end
 
-    # Defines a clause that the criteria should ignore.
+    # Adds a criterion to the +Criteria+ that specifies values where none
+    # should match in order to return results. This is similar to an SQL "NOT IN"
+    # clause. The MongoDB conditional operator that will be used is "$nin".
+    #
+    # Options:
+    #
+    # exclusions: A +Hash+ where the key is the field name and the value is an
+    # +Array+ of values that none can match.
+    #
+    # Example:
+    #
+    # <tt>criteria.not_in(:field => ["value1", "value2"])</tt>
+    #
+    # <tt>criteria.not_in(:field1 => ["value1", "value2"], :field2 => ["value1"])</tt>
+    #
+    # Returns: <tt>self</tt>
     def not_in(exclusions)
       exclusions.each { |key, value| @selector[key] = { "$nin" => value } }; self
     end
 
-    # Define extras for the criteria.
+    # Adds a criterion to the +Criteria+ that specifies additional options
+    # to be passed to the Ruby driver, in the exact format for the driver.
+    #
+    # Options:
+    #
+    # extras: A +Hash+ that gets set to the driver options.
+    #
+    # Example:
+    #
+    # <tt>criteria.extras(:limit => 20, :skip => 40)</tt>
+    #
+    # Returns: <tt>self</tt>
     def extras(extras)
       @options = extras; self
     end
 
-    # Specifies how to sort this Criteria. Current valid params
-    # are: { :field => 1 } or { :field => -1 }
+    # Adds a criterion to the +Criteria+ that specifies the sort order of
+    # the returned documents in the database. Similar to a SQL "ORDER BY".
+    #
+    # Options:
+    #
+    # params: A +Hash+ where the key is the field name and the value is an
+    # integer specifying the direction: 1 for ascending, -1 for descending.
+    #
+    # Example:
+    #
+    # <tt>criteria.order_by(:field1 => 1, :field2 => -1)</tt>
+    #
+    # Returns: <tt>self</tt>
     def order_by(params = {})
       @options[:sort] = params; self
     end
 
-    # Specify what fields to be returned from the database.
-    # Similar to a SQL select field1, field2, field3
+    # Adds a criterion to the +Criteria+ that specifies the fields that will
+    # get returned from the Document. Used mainly for list views that do not
+    # require all fields to be present. This is similar to SQL "SELECT" values.
+    #
+    # Options:
+    #
+    # params: A list of field names to retrict the returned fields to.
+    #
+    # Example:
+    #
+    # <tt>criteria.only(:field1, :field2, :field3)</tt>
+    #
+    # Returns: <tt>self</tt>
     def only(*args)
       @options[:fields] = args.flatten; self
     end
 
-    # Skips the supplied number of records, as offset behaves in traditional
-    # pagination.
+    # Adds a criterion to the +Criteria+ that specifies how many results to skip
+    # when returning Documents. This is mostly used in conjunction with 
+    # <tt>limit()</tt> to handle paginated results, and is similar to the 
+    # traditional "offset" parameter.
+    #
+    # Options:
+    #
+    # value: An +Integer+ specifying the number of results to skip. Defaults to 0.
+    #
+    # Example:
+    #
+    # <tt>criteria.skip(20)</tt>
+    #
+    # Returns: <tt>self</tt>
     def skip(value = 0)
       @options[:skip] = value; self
     end
 
-    # Translate the supplied arguments into a criteria object.
+    # Translate the supplied arguments into a +Criteria+ object. 
+    # 
+    # If the passed in args is a single +String+, then it will 
+    # construct an id +Criteria+ from it.
+    #
+    # If the passed in args are a type and a hash, then it will construct
+    # the +Criteria+ with the proper selector, options, and type.
+    #
+    # Options:
+    #
+    # args: either a +String+ or a +Symbol+, +Hash combination.
+    #
+    # Example:
+    #
+    # <tt>Criteria.translate("4ab2bc4b8ad548971900005c")</tt>
+    #
+    # <tt>Criteria.translate(:all, :conditions => { :field => "value"}, :limit => 20)</tt>
+    #
+    # Returns a new +Criteria+ object.
     def self.translate(*args)
       type, params = args[0], args[1] || {}
       return new(:first).id(type.to_s) if type.is_a?(String)
