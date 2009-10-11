b6c34de9b7db3feec160f57613d85609e3a50ec6
Durran Jordan <durran@gmail.com>
2009-10-11 17:25:09 -0400

    Adding doco to field.

--- lib/mongoid/field.rb
+++ lib/mongoid/field.rb
@@ -6,12 +6,24 @@ module Mongoid #:nodoc:
       :key,
       :name
 
+    # Create the new field with a name and optional additional options. Valid
+    # options are :default, :key
+    #
+    # Options:
+    #
+    # name: The name of the field as a +Symbol+.
+    # options: A +Hash+ of options for the field.
+    #
+    # Example:
+    #
+    # <tt>Field.new(:score, :default => 0)</tt>
     def initialize(name, options = {})
       @name = name
       @default = options[:default]
       @key = options[:key]
     end
 
+    # Returns true if this field acts as a primary key.
     def key?
       @key ? key : false
     end
