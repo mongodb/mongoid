d8aca0a6245aaa1936d46cf71558b044cf64b8bc
Durran Jordan <durran@gmail.com>
2009-11-19 22:37:07 -0500

    Making sure timestamps specs use right describe syntax

--- spec/unit/mongoid/timestamps_spec.rb
+++ spec/unit/mongoid/timestamps_spec.rb
@@ -2,7 +2,7 @@ require File.expand_path(File.join(File.dirname(__FILE__), "/../../spec_helper.r
 
 describe Mongoid::Timestamps do
 
-  describe "#included" do
+  describe ".included" do
 
     before do
       @person = Person.new
