cef1a602537343138f8f2e49a25579599d9b8a04
Durran Jordan <durran@gmail.com>
2009-12-09 11:40:06 -0500

    Updating DeleteAll command to drop collection if no conditions provided.

--- spec/unit/mongoid/commands/delete_all_spec.rb
+++ spec/unit/mongoid/commands/delete_all_spec.rb
@@ -8,13 +8,34 @@ describe Mongoid::Commands::DeleteAll do
       @doc = mock
       @docs = [@doc]
       @klass = mock
-      @conditions = { :conditions => { :title => "Sir" } }
     end
 
-    it "destroys each document that the criteria finds" do
-      @klass.expects(:find).with(:all, @conditions).returns(@docs)
-      Mongoid::Commands::Delete.expects(:execute).with(@doc)
-      Mongoid::Commands::DeleteAll.execute(@klass, @conditions)
+    context "when conditions supplied" do
+
+      before do
+        @conditions = { :conditions => { :title => "Sir" } }
+      end
+
+      it "deletes each document that the criteria finds" do
+        @klass.expects(:find).with(:all, @conditions).returns(@docs)
+        Mongoid::Commands::Delete.expects(:execute).with(@doc)
+        Mongoid::Commands::DeleteAll.execute(@klass, @conditions)
+      end
+
+    end
+
+    context "when no conditions supplied" do
+
+      before do
+        @collection = mock
+      end
+
+      it "drops the collection" do
+        @klass.expects(:collection).returns(@collection)
+        @collection.expects(:drop)
+        Mongoid::Commands::DeleteAll.execute(@klass)
+      end
+
     end
 
   end
