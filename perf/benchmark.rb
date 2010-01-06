852c86c0f8af369318a21acb700fe65f4b87433b
Durran Jordan <durran@gmail.com>
2010-01-06 13:05:43 -0500

    Adding benchmarking over profile

--- perf/benchmark.rb
+++ perf/benchmark.rb
@@ -1,5 +1,6 @@
 require "rubygems"
 require "ruby-prof"
+require "benchmark"
 require "mongoid"
 
 connection = Mongo::Connection.new
@@ -10,7 +11,6 @@ Mongoid.database.collection("people").drop
 class Person
   include Mongoid::Document
   include Mongoid::Timestamps
-  include Mongoid::Versioning
   field :birth_date, :type => Date
   has_one :name
   has_one :address
@@ -19,7 +19,6 @@ end
 
 class Name
   include Mongoid::Document
-  include Mongoid::Timestamps
   field :given
   field :family
   field :middle
@@ -28,7 +27,6 @@ end
 
 class Address
   include Mongoid::Document
-  include Mongoid::Timestamps
   field :street
   field :city
   field :state
@@ -39,36 +37,39 @@ end
 
 class Phone
   include Mongoid::Document
-  include Mongoid::Timestamps
   field :country_code, :type => Integer
   field :number
   field :phone_type
   belongs_to :person, :inverse_of => :phones
 end
 
-RubyProf.start
+# RubyProf.start
 
 puts "Starting benchmark..."
 
-1000.times do |n|
-  person = Person.new(:birth_date => Date.new(1970, 1, 1))
-  name = Name.new(:given => "James", :family => "Kirk", :middle => "Tiberius")
-  address = Address.new(
-    :street => "1 Starfleet Command Way",
-    :city => "San Francisco",
-    :state => "CA",
-    :post_code => "94133",
-    :type => "Work"
-  )
-  phone = Phone.new(:country_code => 1, :number => "415-555-1212", :type => "Mobile")
-  person.name = name
-  person.address = address
-  person.phones << phone
-  person.save
+Benchmark.bm do |bm|
+  bm.report("Mongoid") do
+    10000.times do |n|
+      person = Person.new(:birth_date => Date.new(1970, 1, 1))
+      name = Name.new(:given => "James", :family => "Kirk", :middle => "Tiberius")
+      address = Address.new(
+        :street => "1 Starfleet Command Way",
+        :city => "San Francisco",
+        :state => "CA",
+        :post_code => "94133",
+        :type => "Work"
+      )
+      phone = Phone.new(:country_code => 1, :number => "415-555-1212", :type => "Mobile")
+      person.name = name
+      person.address = address
+      person.phones << phone
+      person.save
+    end
+  end
 end
 
-result = RubyProf.stop
-printer = RubyProf::FlatPrinter.new(result)
-printer.print(STDOUT, 0)
+# result = RubyProf.stop
+# printer = RubyProf::FlatPrinter.new(result)
+# printer.print(STDOUT, 0)
 
 # Mongoid.database.collection("people").drop
