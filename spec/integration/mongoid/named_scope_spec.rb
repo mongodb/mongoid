6eaf6d0189182cfeef8f508951dd6d82344433c8
Durran Jordan <durran@gmail.com>
2010-01-11 20:12:15 -0500

    Updating named scope integration specs

--- spec/integration/mongoid/named_scope_spec.rb
+++ spec/integration/mongoid/named_scope_spec.rb
@@ -5,19 +5,40 @@ describe Mongoid::NamedScope do
   describe ".named_scope" do
 
     class Person
-      named_scope :doctors, {:where => {:title => 'Dr.'}}
+      named_scope :doctors, {:where => {:title => 'Dr.'} }
+      named_scope :old, criteria.where(:age.gt => 50)
     end
 
     before do
-      @document = Person.create(:title => "Dr.")
+      @document = Person.create(:title => "Dr.", :age => 65, :terms => true)
     end
 
     after do
       Person.delete_all
     end
 
-    it "returns the document" do
-      Person.doctors.first.should == @document
+    context "accessing a single named scope" do
+
+      it "returns the document" do
+        Person.doctors.first.should == @document
+      end
+
+    end
+
+    context "chaining named scopes" do
+
+      it "returns the document" do
+        Person.old.doctors.first.should == @document
+      end
+
+    end
+
+    context "mixing named scopes and class methods" do
+
+      it "returns the document" do
+        Person.accepted.old.doctors.first.should == @document
+      end
+
     end
 
   end
