1457a8c910a33464d6bebb0fc4d3c3325276f4c6
brainopia <ravwar@gmail.com>
2010-01-16 13:22:57 +0300

    Remove terminator for callbacks

--- lib/mongoid/callbacks.rb
+++ lib/mongoid/callbacks.rb
@@ -11,7 +11,8 @@ module Mongoid #:nodoc:
           :destroy,
           :save,
           :update,
-          :validation
+          :validation,
+          :terminator => false
       end
     end
   end
