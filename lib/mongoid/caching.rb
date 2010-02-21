42e6576b29d46bdb5a8a7814c3dcafd68405f7d5
Durran Jordan <durran@gmail.com>
2010-02-21 13:35:48 -0500

    Updating cache documentation

--- lib/mongoid/caching.rb
+++ lib/mongoid/caching.rb
@@ -12,7 +12,11 @@ module Mongoid #:nodoc:
     end
 
     module ClassMethods #:nodoc
-      # Sets caching on for this class.
+      # Sets caching on for this class. This class level configuration will
+      # default all queries to cache the results of the first iteration over
+      # the cursor into an internal array. This should only be used for queries
+      # that return a small number of results or have small documents, as after
+      # the first iteration the entire results will be stored in memory.
       #
       # Example:
       #
