926f324230ed6923c5b5f3359961c432c1285c1e
Durran Jordan <durran@gmail.com>
2010-04-01 20:43:27 -0500

    Adding in Document#upsert via persistence module

--- lib/mongoid/persistence.rb
+++ lib/mongoid/persistence.rb
@@ -5,7 +5,16 @@ require "mongoid/persistence/insert_embedded"
 require "mongoid/persistence/update"
 
 module Mongoid #:nodoc:
-  module Persistence #:nodoc:
+  # The persistence module is a mixin to provide database accessor methods for
+  # the document. These correspond to the appropriate accessors on a
+  # +Mongo::Collection+ and retain the same DSL.
+  #
+  # Examples:
+  #
+  # <tt>document.insert</tt>
+  # <tt>document.update</tt>
+  # <tt>document.upsert</tt>
+  module Persistence
     # Insert a new +Document+ into the database. Will return the document
     # itself whether or not the save was successful.
     #
@@ -23,6 +32,20 @@ module Mongoid #:nodoc:
     # <tt>document.update</tt>
     def update
       Update.new(self).persist
+    end
+
+    # Upsert the document - will perform an insert if the document is new, and
+    # update if not.
+    #
+    # Example:
+    #
+    # <tt>document.upsert</tt>
+    #
+    # Returns:
+    #
+    # A +Boolean+ for updates, the +Document+ for inserts.
+    def upsert
+      new_record? ? insert : update
     end
   end
 end
--- spec/unit/mongoid/persistence_spec.rb
+++ spec/unit/mongoid/persistence_spec.rb
@@ -37,4 +37,50 @@ describe Mongoid::Persistence do
       person.update.should == true
     end
   end
+
+  describe "#upsert" do
+
+    context "when the document is new" do
+
+      let(:insert) do
+        stub.quacks_like(Mongoid::Persistence::Insert.allocate)
+      end
+
+      before do
+        Mongoid::Persistence::Insert.expects(:new).with(person).returns(insert)
+      end
+
+      it "delegates to the insert persistence command" do
+        insert.expects(:persist).returns(person)
+        person.upsert
+      end
+
+      it "returns the document" do
+        insert.expects(:persist).returns(person)
+        person.upsert.should == person
+      end
+    end
+
+    context "when the document is not new" do
+
+      let(:update) do
+        stub.quacks_like(Mongoid::Persistence::Update.allocate)
+      end
+
+      before do
+        person.instance_variable_set(:@new_record, false)
+        Mongoid::Persistence::Update.expects(:new).with(person).returns(update)
+      end
+
+      it "delegates to the update persistence command" do
+        update.expects(:persist).returns(true)
+        person.upsert
+      end
+
+      it "returns a boolean" do
+        update.expects(:persist).returns(true)
+        person.upsert.should == true
+      end
+    end
+  end
 end
