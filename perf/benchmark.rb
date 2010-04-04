6fb5229493a962fb88a956195bcf9f45a927143a
Durran Jordan <durran@gmail.com>
2010-04-04 12:59:44 -0500

    benchmarks version more clear

--- perf/benchmark.rb
+++ perf/benchmark.rb
@@ -123,7 +123,7 @@ Benchmark.bm do |bm|
   end
 end
 
-# Start:                                           User     System      Total        Real
+# 1.2.15:                                           User     System      Total        Real
 #
 # Saving 10k New Documents                    25.440000   0.670000  26.110000 ( 29.945368)
 # Querying & Iterating 10k Documents           2.440000   0.110000   2.550000 (  2.736474)
@@ -131,7 +131,7 @@ end
 # Updating An Embedded Document 10k Times     16.810000   0.610000  17.420000 ( 19.051299)
 # Appending A New Embedded Document 10k Times 17.330000   0.650000  17.980000 ( 19.706136)
 # ---------------------------------------------------------------------------------------
-# First pass:
+# 2.0.0.beta1:
 #
 # Saving 10k New Documents                    24.500000   0.440000  24.940000 ( 25.091105)
 # Querying & Iterating 10k Documents           3.140000   0.110000   3.250000 (  3.275101)
@@ -139,20 +139,10 @@ end
 # Updating An Embedded Document 10k Times     16.580000   0.570000  17.150000 ( 18.471384)
 # Appending A New Embedded Document 10k Times 16.720000   0.560000  17.280000 ( 18.491286)
 # ---------------------------------------------------------------------------------------
-# Write in place:
-#
-# Saving 10k New Documents                    24.070000   0.430000  24.500000 ( 24.651677)
-# Querying & Iterating 10k Documents           3.000000   0.110000   3.110000 (  3.135015)
-# Updating The Root Document 10k Times        11.980000   0.470000  12.450000 ( 13.569498)
-# Updating An Embedded Document 10k Times     13.230000   0.560000  13.790000 ( 14.985528)
-# Appending A New Embedded Document 10k Times 13.140000   0.580000  13.720000 ( 14.937596)
-# ---------------------------------------------------------------------------------------
-# 2.0.0.beta2
+# 2.0.0.beta2:
 #
 # Saving 10k New Documents                    23.700000   0.380000  24.080000 ( 24.171105)
 # Querying & Iterating 10k Documents           2.980000   0.100000   3.080000 (  3.090209)
 # Updating The Root Document 10k Times        11.550000   0.440000  11.990000 ( 13.047147)
 # Updating An Embedded Document 10k Times     12.250000   0.500000  12.750000 ( 13.786223)
 # Appending A New Embedded Document 10k Times 12.320000   0.510000  12.830000 ( 14.392891)
-
-# Mongoid.database.collection("people").drop
