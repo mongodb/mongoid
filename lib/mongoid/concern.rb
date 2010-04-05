f57636737a2a55c697775748f404b8d3c497b906
Bernerd Schaefer and Durran Jordan <dev+bernerdschaefer+durran@hashrocket.com>
2010-04-05 10:21:09 -0500

    Adding in monkey-patched concern

---
+++ lib/mongoid/concern.rb
@@ -0,0 +1,31 @@
+unless defined?(ActiveSupport::Concern)
+  module ActiveSupport
+    module Concern
+      def self.extended(base)
+        base.instance_variable_set("@_dependencies", [])
+      end
+
+      def append_features(base)
+        if base.instance_variable_defined?("@_dependencies")
+          base.instance_variable_get("@_dependencies") << self
+          return false
+        else
+          return false if base < self
+          @_dependencies.each { |dep| base.send(:include, dep) }
+          super
+          base.extend const_get("ClassMethods") if const_defined?("ClassMethods")
+          base.send :include, const_get("InstanceMethods") if const_defined?("InstanceMethods")
+          base.class_eval(&@_included_block) if instance_variable_defined?("@_included_block")
+        end
+      end
+
+      def included(base = nil, &block)
+        if base.nil?
+          @_included_block = block
+        else
+          super
+        end
+      end
+    end
+  end
+end
