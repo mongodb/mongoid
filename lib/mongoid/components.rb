1fee311fb8b9615b98213027d237b2db506b1d1d
Bernerd Schaefer and Durran Jordan <dev+bernerdschaefer+durran@hashrocket.com>
2010-04-05 10:25:01 -0500

    Adding in paths

--- lib/mongoid/components.rb
+++ lib/mongoid/components.rb
@@ -1,25 +1,25 @@
 # encoding: utf-8
 module Mongoid #:nodoc
   module Components #:nodoc
-    def self.included(base)
-      base.class_eval do
-        # All modules that a +Document+ is composed of are defined in this
-        # module, to keep the document class from getting too cluttered.
-        include Mongoid::Associations
-        include Mongoid::Attributes
-        include Mongoid::Callbacks
-        include Mongoid::Commands
-        include Mongoid::Extras
-        include Mongoid::Fields
-        include Mongoid::Indexes
-        include Mongoid::Matchers
-        include Mongoid::Memoization
-        include Mongoid::Observable
-        include Mongoid::State
-        include Validatable
-        extend Mongoid::Finders
-        extend Mongoid::NamedScope
-      end
+    extend ActiveSupport::Concern
+    included do
+      # All modules that a +Document+ is composed of are defined in this
+      # module, to keep the document class from getting too cluttered.
+      include Mongoid::Associations
+      include Mongoid::Attributes
+      include Mongoid::Callbacks
+      include Mongoid::Commands
+      include Mongoid::Extras
+      include Mongoid::Fields
+      include Mongoid::Indexes
+      include Mongoid::Matchers
+      include Mongoid::Memoization
+      include Mongoid::Observable
+      include Mongoid::Path
+      include Mongoid::State
+      include Validatable
+      extend Mongoid::Finders
+      extend Mongoid::NamedScope
     end
   end
 end
