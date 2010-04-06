d485b37830e6a0012500f1ed9d75bebda8d0512e
Brian Durand <bdurand@bdurand.local>
2010-04-06 16:31:05 -0500

    convert datetime to time for storage

--- lib/mongoid/extensions/datetime/conversions.rb
+++ lib/mongoid/extensions/datetime/conversions.rb
@@ -4,12 +4,16 @@ module Mongoid #:nodoc:
     module DateTime #:nodoc:
       module Conversions #:nodoc:
         def set(value)
-          return nil if value.blank?
-          ::DateTime.parse(value.to_s).utc
+            return nil if value.blank?
+            time = (value.is_a?(::DateTime) || value.is_a?(::Time)) ? value.to_time : ::Time.parse(value.to_s)
+            # Convert time to milliseconds since BSON stores dates with that accurracy, but Ruby uses microseconds
+            ::Time.at((time.to_f * 1000).round / 1000.0).utc if time
+          rescue ArgumentError
+            value
         end
         def get(value)
           return nil if value.blank?
-          ::Time.zone ? ::Time.parse(value.to_s).getlocal.to_datetime : value.to_datetime
+          ::Time.zone ? value.getlocal.to_datetime : value
         end
       end
     end
