fff751da49ee0b780bc7fa6b6a397218cb0e7f55
saks <saksmlz@gmail.com>
2010-04-08 06:47:51 +0800

    Ability to pass in pagination options.
    For a more will_paginate like experience, this allows you to .paginate(:page => params[:page], :per_page => 20) at the end of a criteria chain.

--- lib/mongoid/contexts/paging.rb
+++ lib/mongoid/contexts/paging.rb
@@ -6,12 +6,20 @@ module Mongoid #:nodoc:
       #
       # Example:
       #
-      # <tt>context.paginate</tt>
+      # <tt>context.paginate(:page => 6, :per_page => 25)</tt>
       #
       # Returns:
       #
       # A collection of documents paginated.
-      def paginate
+      # All previous <tt>limit</tt> and <tt>skip</tt> call will be ignored.
+      def paginate(pager_options={})
+        if pager_options[:per_page]
+          options[:limit] = pager_options[:per_page].to_i
+          if pager_options[:page]
+            options[:skip]  = (pager_options[:page].to_i - 1) * pager_options[:per_page].to_i
+          end
+        end
+
         @collection ||= execute(true)
         WillPaginate::Collection.create(page, per_page, count) do |pager|
           pager.replace(@collection.to_a)
