4f1b0deb13cbe8ec0789076cb39139f0ada19bec
Durran Jordan <durran@gmail.com>
2010-04-09 13:03:33 -0400

    add more optimized conversions for Time

--- lib/mongoid/extensions/time/conversions.rb
+++ lib/mongoid/extensions/time/conversions.rb
@@ -5,12 +5,20 @@ module Mongoid #:nodoc:
       module Conversions #:nodoc:
         def set(value)
           return nil if value.blank?
-          time = ::Time.parse(value.is_a?(::Time) ? value.strftime("%Y-%m-%d %H:%M:%S %Z") : value.to_s)
-          time.utc? ? time : time.utc
+          time = convertable?(value) ? value.to_time : ::Time.parse(value.to_s)
+          # Convert time to milliseconds since BSON stores dates with that accurracy, but Ruby uses microseconds
+          ::Time.at((time.to_f * 1000).round / 1000.0).utc if time
+        rescue ArgumentError
+          value
         end
         def get(value)
           return nil if value.blank?
           ::Time.zone ? value.getlocal : value
+        end
+
+        protected
+        def convertable?(value)
+          value.is_a?(::Time) || value.is_a?(::Date) || value.is_a?(::DateTime)
         end
       end
     end
