facf9ccd11fd5fbf9875930c5c614fcd35e7ff2a
Durran Jordan <durran@gmail.com>
2010-04-09 13:05:27 -0400

    optimize data conversions

--- lib/mongoid/extensions/boolean/conversions.rb
+++ lib/mongoid/extensions/boolean/conversions.rb
@@ -3,9 +3,9 @@ module Mongoid #:nodoc:
   module Extensions #:nodoc:
     module Boolean #:nodoc:
       module Conversions #:nodoc:
+        BOOLEAN_MAP = {true => true, "true" => true, "TRUE" => true, "1" => true, 1 => true, 1.0 => true}
         def set(value)
-          val = value.to_s
-          val == "true" || val == "1"
+          BOOLEAN_MAP.include?(value)
         end
         def get(value)
           value
