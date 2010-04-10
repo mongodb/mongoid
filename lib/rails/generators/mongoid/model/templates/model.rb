c9924efb1c4fbed65d844cebccbd74db0d7148da
guillaume belleguic <guillaume.belleguic@cixi.eu>
2010-04-10 13:15:05 +0800

    add references support

--- lib/rails/generators/mongoid/model/templates/model.rb
+++ lib/rails/generators/mongoid/model/templates/model.rb
@@ -9,7 +9,13 @@ class <%= class_name %><%= " < #{options[:parent].classify}" if options[:parent]
 
 <%= 'include Mongoid::Versioning' if options[:versioning] %>
 
-<% attributes.each do |attribute| -%>
-  field :<%= attribute.name -%>, :type => <%= attribute.type_class %>
-<% end %>
+<% attributes.reject{|attr| attr.reference?}.each do |attribute| -%>
+  field :<%= attribute.name %>, :type => <%= attribute.type_class %>
+<% end -%>
+
+<% attributes.select{|attr| attr.reference? }.each do |attribute| -%>
+  belongs_to :<%= attribute.name%>, :inverse_of => :<%= class_name.tableize %>
+<% end -%>
+
+
 end
