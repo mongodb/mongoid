71b390f3dc7461c8476662472e16b0b2d77a3e76
Ivan Porto Carrero <ivan@flanders.co.nz>
2010-04-19 17:01:34 +0800

    Adds collectionize method on nil class

--- lib/mongoid/extensions/nil/assimilation.rb
+++ lib/mongoid/extensions/nil/assimilation.rb
@@ -7,6 +7,10 @@ module Mongoid #:nodoc:
         def assimilate(parent, options, type = nil)
           parent.remove_attribute(options.name); self
         end
+        
+        def collectionize
+          to_s.collectionize
+        end
       end
     end
   end
