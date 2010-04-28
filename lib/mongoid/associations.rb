47a97094b32448aa09965c854a24c78803c7f42e
Durran Jordan <durran@gmail.com>
2010-04-28 17:26:20 -0400

    Document#save saves new embedded children added via appending (Github: 71)

--- lib/mongoid/associations.rb
+++ lib/mongoid/associations.rb
@@ -31,7 +31,7 @@ module Mongoid # :nodoc:
       # Update all the dirty child documents after an update.
       def update_embedded(name)
         association = send(name)
-        association.to_a.each { |doc| doc.save if doc.changed? } unless association.blank?
+        association.to_a.each { |doc| doc.save if doc.changed? || doc.new_record? } unless association.blank?
       end
 
       # Update the one-to-one relational association for the name.
