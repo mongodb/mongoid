cc4f7d0b5d3510fe260afd291352c81c3767fc0f
Durran Jordan <durran@gmail.com>
2010-04-29 15:34:28 -0400

    Cleaning up Mongoid railtie, adding some documentation

--- lib/mongoid/railtie.rb
+++ lib/mongoid/railtie.rb
@@ -2,7 +2,7 @@
 if defined?(Rails::Railtie)
   module Rails #:nodoc:
     module Mongoid #:nodoc:
-      class Railtie < Rails::Railtie
+      class Railtie < Rails::Railtie #:nodoc:
 
         # do we want a custom log subscriber for mongoid?
         # log_subscriber :mongoid, ::Mongoid::Railties::LogSubscriber.new
@@ -13,63 +13,30 @@ if defined?(Rails::Railtie)
           load "mongoid/railties/database.rake"
         end
 
+        # Initialize Mongoid. This will look for a mongoid.yml in the config
+        # directory and configure mongoid appropriately.
+        #
+        # Example mongoid.yml:
+        #
+        #   defaults: &defaults
+        #     host: localhost
+        #     slaves:
+        #       # - host: localhost
+        #         # port: 27018
+        #       # - host: localhost
+        #         # port: 27019
+        #     allow_dynamic_fields: false
+        #     parameterize_keys: false
+        #     persist_in_safe_mode: false
+        #
+        #   development:
+        #     <<: *defaults
+        #     database: mongoid
         initializer "setup database" do
           config_file = Rails.root.join("config", "mongoid.yml")
           if config_file.file?
             settings = YAML.load(ERB.new(config_file.read).result)[Rails.env]
-            if settings.present?
-              ::Mongoid.configure do |config|
-                database = settings["database"]
-                host = settings["host"]
-                port = settings["port"]
-                opts = {}
-
-                # allow passing in of connection options
-                if settings["connection"].is_a?(Hash)
-                  opts = settings["connection"]
-                end
-
-                config.master = Mongo::Connection.new(host, port, opts).db(database)
-
-                # handle authentication is a username is specified
-                if settings.has_key?("username")
-                  config.master.authenticate(settings["username"].to_s, settings["password"].to_s)
-                end
-
-                if settings.has_key?("allow_dynamic_fields")
-                  config.allow_dynamic_fields = !!settings["allow_dynamic_fields"]
-                end
-
-                if settings.has_key?("max_successive_reads")
-                  config.max_successive_reads = settings["max_successive_reads"].to_i
-                end
-
-                if settings.has_key?("parameterize_keys")
-                  config.parameterize_keys = !!settings["parameterize_keys"]
-                end
-
-                if settings.has_key?("persist_in_safe_mode")
-                  config.persist_in_safe_mode = !!settings["persist_in_safe_mode"]
-                end
-
-                if settings.has_key?("raise_not_found_error")
-                  config.raise_not_found_error = !!settings["raise_not_found_error"]
-                end
-
-                if settings.has_key?("reconnect_time")
-                  config.reconnect_time = settings["reconnect_time"].to_i
-                end
-
-                config.slaves = []
-                settings["slaves"].to_a.each do |slave_config|
-                  config.slaves << Mongo::Connection.new(
-                    slave_config["host"] || host,
-                    slave_config["port"] || port,
-                    :slave_ok => true
-                  ).db(database)
-                end
-              end
-            end
+            Mongoid.from_hash(settings) if settings.present?
           end
         end
 
