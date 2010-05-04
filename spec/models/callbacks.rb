baa9b0e783b024622d617c4c435e0c072881977d
Chris Griego <cgriego@gmail.com>
2010-05-04 17:58:01 -0500

    Fix for terminating callbacks

--- spec/models/callbacks.rb
+++ spec/models/callbacks.rb
@@ -3,9 +3,14 @@ class Artist
   field :name
   embeds_many :songs
 
+  before_create :before_create_stub
   after_create :create_songs
 
   protected
+  def before_create_stub
+    true
+  end
+
   def create_songs
     2.times { |n| songs.create!(:title => "#{n}") }
   end
