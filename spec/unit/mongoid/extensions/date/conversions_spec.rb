a3c9e51dd1fd683902f2223dbff457f7e72e5722
Emily Bache and Nicklas Ramhoj <dev+emily+nicklas@elabs.se>
2010-05-06 15:12:55 +0200

    Improved test coverage and organization. Lots of well documented cases, especially for when several time zones are involved.¨

--- spec/unit/mongoid/extensions/date/conversions_spec.rb
+++ spec/unit/mongoid/extensions/date/conversions_spec.rb
@@ -1,108 +1,198 @@
 require "spec_helper"
 
 describe Mongoid::Extensions::Date::Conversions do
-
   before do
-    Mongoid::Config.instance.time_zone = nil
-    Time.zone = "Stockholm"
-    @time = Date.today.to_time
+    Time.zone = "Canberra"
+    @time = Time.local(2010, 11, 19)
   end
 
-  after do
-    Time.zone = nil
-  end
+  after { Time.zone = nil }
 
-  describe "#set" do
-
-    context "when string provided" do
-
-      context "when string is a non utc time" do
-
-        it "returns a utc date from the string" do
-          Date.set(@time.to_s).should == Time.utc(@time.year, @time.month, @time.day)
-        end
-
+  describe ".set" do
+    context "when given nil" do
+      it "returns nil" do
+        Date.set(nil).should be_nil
       end
-
-      context "when string is a date" do
-
-        it "returns a time from the string" do
-          date = RUBY_VERSION.start_with?("1.9") ? "15/01/2007" : "01/15/2007"
-          Date.set(date).should == Time.utc(2007, 1, 15)
-        end
-
-      end
-
-      context "when string is empty" do
-
-        it "returns nil" do
-          Date.set("").should == nil
-        end
-
-      end
-
     end
 
-    context "when time provided" do
+    context "when string is empty" do
+      it "returns nil" do
+        Date.set("").should be_nil
+      end
+    end
+
+    context "when given a string" do
+      it "converts to a utc time" do
+        Date.set(@time.to_s).utc_offset.should == 0
+      end
+
+      it "returns the right day" do
+        Date.set(@time.to_s).day.should == @time.day
+      end
+
+      it "returns a utc date from the string" do
+        Date.set(@time.to_s).should == Time.utc(@time.year, @time.month, @time.day)
+      end
+    end
+
+    context "when given a DateTime" do
+      it "returns a time" do
+        Date.set(@time.to_datetime).should == Time.utc(@time.year, @time.month, @time.day)
+      end
+    end
+
+    context "when given a Time" do
+      it "converts to a utc time" do
+        Date.set(@time).utc_offset.should == 0
+      end
+
+      it "strips miliseconds" do
+        Date.set(Time.now).usec.should == 0
+      end
+
+      it "returns utc times the same day, but at midnight" do
+        Date.set(@time.utc).should == Time.utc(@time.year, @time.month, @time.day)
+      end
 
       it "returns the date for the time" do
         Date.set(@time).should == Time.utc(@time.year, @time.month, @time.day)
       end
-
     end
 
-    context "when a date provided" do
+    context "when given an ActiveSupport::TimeWithZone" do
+      before { @time = 1.hour.ago }
 
-      it "returns a time from the date" do
-        Date.set(@time.to_date).should == Time.utc(@time.year, @time.month, @time.day)
+      it "converts it to utc" do
+        Date.set(@time.in_time_zone("Alaska")).should == Time.utc(@time.year, @time.month, @time.day)
+      end
+    end
+
+    context "when given a Date" do
+      before { @date = Date.today }
+
+      it "converts to a utc time" do
+        Date.set(@date).should == Time.utc(@date.year, @date.month, @date.day)
       end
 
     end
-
   end
 
-  describe "#get" do
+  describe ".get" do
+    before { @time = Time.now.utc }
 
-    context "when value is nil" do
+    it "converts the time back to a date" do
+      Date.get(@time).should == @time.to_date
+    end
 
+    context "when the time zone is not defined" do
+      before do
+        Mongoid::Config.instance.time_zone = nil
+        Time.zone = "Stockholm"
+      end
+
+      context "when the local time is not observing daylight saving" do
+        before { @time = Time.utc(2010, 11, 19) }
+
+        it "returns the same day" do
+          Date.get(@time).day.should == 19
+        end
+      end
+
+      context "when the local time is observing daylight saving" do
+        before { @time = Time.utc(2010, 9, 19) }
+
+        it "returns the same day" do
+          Date.get(@time).day.should == 19
+        end
+      end
+    end
+
+    context "when the time zone is defined as something other than UTC" do
+      before do
+        Mongoid::Config.instance.time_zone = "Alaska"
+        Time.zone = "Stockholm"
+      end
+
+      context "when the local time is not observing daylight saving" do
+        before { @time = Time.utc(2010, 11, 19) }
+
+        it "returns the same day" do
+          Date.get(@time).day.should == @time.day - 1
+        end
+      end
+
+      context "when the local time is observing daylight saving" do
+        before { @time = Time.utc(2010, 9, 19) }
+
+        it "returns the same day" do
+          Date.get(@time).day.should == @time.day - 1
+        end
+      end
+    end
+
+    context "when the time zone is defined as UTC" do
+      before { Mongoid::Config.instance.time_zone = "UTC" }
+      after { Mongoid::Config.instance.time_zone = nil }
+
+      it "returns the same day" do
+         Date.get(@time.dup.utc).day.should == @time.day
+      end
+    end
+
+    context "when time is nil" do
       it "returns nil" do
         Date.get(nil).should be_nil
       end
-
     end
-
-    context "when value is not nil" do
-
-      context "when time is UTC" do
-
-        before do
-          @utc = Time.utc(1974, 12, 1)
-        end
-
-        context "when time zone is not utc" do
-
-          before do
-            Time.zone = "Eastern Time (US & Canada)"
-          end
-
-          after do
-            Time.zone = nil
-          end
-
-          it "converts to the proper date" do
-            Date.get(@utc).should == Date.new(1974, 12, 1)
-          end
-
-        end
-
-      end
-
-      it "converts the time back to a date" do
-        Date.get(@time).should == @time.to_date
-      end
-
-    end
-
   end
 
+  describe "round trip - set then get" do
+    context "when the time zone is not defined" do
+      before do
+        Mongoid::Config.instance.time_zone = nil
+        Time.zone = "Stockholm"
+      end
+
+      context "when the local time is not observing daylight saving" do
+        before { @time = Date.set(Time.zone.local(2010, 11, 19, 0, 30)) }
+
+        it "does not change the day" do
+          Date.get(@time).day.should == 19
+        end
+      end
+
+      context "when the local time is observing daylight saving" do
+        before { @time = Date.set(Time.zone.local(2010, 9, 19, 0, 30)) }
+
+        it "does not change the day" do
+          Date.get(@time).day.should == 19
+        end
+      end
+    end
+
+    context "when the time zone is defined as something other than UTC" do
+      before do
+        Mongoid::Config.instance.time_zone = "Alaska"
+        Time.zone = "Stockholm"
+      end
+
+      context "when the local time is not observing daylight saving" do
+        before { @time = Date.set(Time.zone.local(2010, 11, 19)) }
+
+        it "returns the previous day since Alaska is behind Stockholm" do
+          Date.get(@time).day.should == @time.day - 1
+        end
+      end
+
+      context "when the local time is observing daylight saving" do
+        before do
+          @time = Date.set(Time.zone.local(2010, 9, 19))
+        end
+
+        it "returns the previous day since Alaska is behind Stockholm" do
+          Date.get(@time).day.should == @time.day - 1
+        end
+      end
+    end
+  end
 end
--- spec/unit/mongoid/extensions/datetime/conversions_spec.rb
+++ spec/unit/mongoid/extensions/datetime/conversions_spec.rb
@@ -1,105 +1,14 @@
 require "spec_helper"
 
 describe Mongoid::Extensions::DateTime::Conversions do
-
-  before do
-    Mongoid::Config.instance.time_zone = nil
-    Time.zone = "Eastern Time (US & Canada)"
-    @time = Time.local(1976, 11, 19)
-  end
-
-  after do
-    Time.zone = nil
-  end
-
-  describe "#set" do
-
+  describe ".get" do
     before do
-      @date_time = DateTime.civil(2005, 11, 19).new_offset(1800)
+      @time = Time.now.utc
+      ::Mongoid::Extensions::TimeConversions.stubs(:get).returns(@time)
     end
 
-    context "when value is a string" do
-
-      it "converts to a utc time" do
-        DateTime.set(@date_time.to_s).utc_offset.should == 0
-      end
-
+    it "converts to a datetime" do
+      DateTime.get(@time).should be_kind_of(DateTime)
     end
-
-    context "when value is a date_time" do
-
-      it "converts to a utc time" do
-        DateTime.set(@date_time).utc_offset.should == 0
-      end
-
-      it "does not lose time zone or switch day" do
-        Time.zone = "Stockholm"
-        @date_time = Time.local(1976, 11, 19, 0, 30).to_datetime
-        DateTime.set(@date_time).should == Time.utc(@date_time.year, @date_time.month, @date_time.day, @date_time.hour, @date_time.min, @date_time.sec)
-      end
-
-    end
-
-    context "when value is nil" do
-
-      it "returns nil" do
-        DateTime.set(nil).should be_nil
-      end
-
-    end
-
-  end
-
-  describe "#get" do
-
-    context "when no time zone is configured" do
-
-      context "when time is provided" do
-
-        it "returns a DateTime" do
-          DateTime.get(@time.dup).should be_a_kind_of(DateTime)
-        end
-
-        it "returns the local date_time" do
-          DateTime.get(@time.dup).should == Time.local(1976, 11, 19).to_datetime
-        end
-
-        it "preserves the utc offset" do
-          DateTime.get(@time.dup).utc_offset.should == @time.utc_offset
-        end
-
-      end
-
-      context "when Alaska time zone is configured" do
-
-        before do
-          Mongoid::Config.instance.time_zone = "Alaska"
-        end
-
-        after do
-          Mongoid::Config.instance.time_zone = nil
-        end
-
-        it "returns a DateTime" do
-          DateTime.get(@time.dup).should be_a_kind_of(DateTime)
-        end
-
-        it "returns a Alaskan date_time" do
-          @time = Time.local(1999, 12, 15) # time in winter when no DST
-          DateTime.get(@time.dup).utc_offset.should == ActiveSupport::TimeZone["Alaska"].utc_offset
-        end
-
-      end
-
-      context "when time is nil" do
-
-        it "returns nil" do
-          DateTime.get(nil).should be_nil
-        end
-
-      end
-
-    end
-
   end
 end
