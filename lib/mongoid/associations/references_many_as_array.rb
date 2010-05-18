8cb1db442bff1ffd79eb9d9b157d87f0eaa77ac7
Durran Jordan <durran@gmail.com>
2010-05-18 12:37:14 -0400

    Adding skeleton references many as array proxy

---
+++ lib/mongoid/associations/references_many_as_array.rb
@@ -0,0 +1,10 @@
+# encoding: utf-8
+module Mongoid #:nodoc:
+  module Associations #:nodoc:
+    # Represents an relational one-to-many association with an object in a
+    # separate collection or database, stored as an array of ids on the parent
+    # document.
+    class ReferencesManyAsArray < Proxy
+    end
+  end
+end
