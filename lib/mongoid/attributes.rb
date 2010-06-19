79b4d3710d17c949544fe2b233990a4c3a648b1c
Durran Jordan <durran@gmail.com>
2010-06-18 21:58:37 -0400

    Revert "Allowed Parent#update_attributes(:child => { :child_attribute => value"
    Had spec failures after commit.
    
    This reverts commit 523c44db08d3d817830c7a60a32fa6b6bb522cf7.

--- lib/mongoid/attributes.rb
+++ lib/mongoid/attributes.rb
@@ -38,15 +38,7 @@ module Mongoid #:nodoc:
         if set_allowed?(key)
           write_attribute(key, value)
         elsif write_allowed?(key)
-          if associations.include?(key.to_s) and associations[key.to_s].embedded? and value.is_a?(Hash)
-            if association = send(key)
-              association.nested_build(value)
-            else
-              send("build_#{key}", value)
-            end          
-          else
-            send("#{key}=", value)
-          end
+          send("#{key}=", value)
         end
       end
       setup_modifications
