2e1600faeac04e0240599eebbeec0e9b9623281d
Jacques Crocker <railsjedi@gmail.com>
2010-07-19 14:40:26 -0700

    Tweaking rake script and adding one that will bundle everything

--- rake_setup.sh
+++ rake_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 rvm use ruby-1.8.7@mongoid
-bundle exec rake spec
+bundle
 rvm use ruby-1.9.1@mongoid
-bundle exec rake spec
+bundle
 rvm use ruby-1.9.2-head@mongoid
-bundle exec rake spec
+bundle
