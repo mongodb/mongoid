16a8b3a52d0f8130c2ff7da8e4e676975e3cfb86
Matt <matt.beedle@1000jobboersen.de>
2010-07-21 16:20:43 +0200

    Fixed bug where when an object belonged to another in more than one association, the wrong association was sometimes used

--- lib/mongoid/associations/references_many.rb
+++ lib/mongoid/associations/references_many.rb
@@ -233,13 +233,19 @@ module Mongoid #:nodoc:
           end
 
           if !inverse
-            association = options.klass.associations.values.detect do |metadata|
-              metadata.options.klass == target
-            end
-            inverse = association.name if association
+            association = detect_association(target, options, false)
+            association = detect_association(target, options, true) if association.blank?
+            inferred = association.name if association
           end
 
-          inverse || target.to_s.underscore
+          inverse || inferred || target.to_s.underscore
+        end
+
+        def detect_association(target, options, with_class_name = false)
+          association = options.klass.associations.values.detect do |metadata|
+            metadata.options.klass == target &&
+              (with_class_name ? true : metadata.options[:class_name].nil?)
+          end
         end
       end
     end
