cf1857bdd6fa0f6391624278d3e79d5ad1fffedd
Bernerd Schaefer and Veezus Kreist <dev+bernerdschaefer+veezus@hashrocket.com>
2010-08-03 13:07:34 -0500

    Cleanup: use let instead of instance variables

--- spec/integration/mongoid/contexts/enumerable_spec.rb
+++ spec/integration/mongoid/contexts/enumerable_spec.rb
@@ -2,32 +2,39 @@ require "spec_helper"
 
 describe Mongoid::Contexts::Enumerable do
 
-  before do
-    @person = Person.new(:title => "Sir")
+  let(:person) do
+    person = Person.new(:title => "Sir")
     10.times do |n|
-      @person.addresses << Address.new(:number => n, :street => "Upper Street")
+      person.addresses << Address.new(:number => n, :street => "Upper Street")
     end
+    person
+  end
+
+  describe "#limit" do
+
+    it "returns the specified number of documents" do
+      person.addresses.criteria.limit(5).size.should == 5
+    end
+
   end
 
   describe "#paginate" do
 
     it "paginates the embedded documents" do
-      addresses = @person.addresses.paginate(:page => nil, :per_page => 5)
+      addresses = person.addresses.paginate(:page => nil, :per_page => 5)
       addresses.current_page.should == 1
       addresses.size.should == 5
     end
+
   end
 
-  describe "limit and skip" do
+  describe "#skip" do
 
-    it "limits" do
-      @person.addresses.criteria.limit(5).size.should == 5
-    end
-
-    it "skips" do
-      @person.addresses.criteria.skip(5).limit(10).
+    it "excludes the specified number of document" do
+      person.addresses.criteria.skip(5).limit(10).
         map(&:number).should == [5, 6, 7, 8, 9]
     end
 
   end
+
 end
