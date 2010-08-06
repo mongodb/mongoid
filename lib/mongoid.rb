a8827dd2c09750ec02fddc546e42a54677c97b59
Durran Jordan <durran@gmail.com>
2010-08-05 21:32:04 -0400

    Bumping mongodb version requirement to 1.6.0

--- lib/mongoid.rb
+++ lib/mongoid.rb
@@ -94,7 +94,7 @@ I18n.load_path << File.join(File.dirname(__FILE__), "config", "locales", "en.yml
 
 module Mongoid #:nodoc
 
-  MONGODB_VERSION = "1.4.0"
+  MONGODB_VERSION = "1.6.0"
 
   class << self
 
