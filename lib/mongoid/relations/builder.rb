63261087798fa2ba98a6be45349ecc8fa97af8df
Durran Jordan <durran@gmail.com>
2010-08-10 15:56:25 -0400

    Adding builder for referenced_in relations... Nice and simple.

--- lib/mongoid/relations/builder.rb
+++ lib/mongoid/relations/builder.rb
@@ -12,6 +12,22 @@ module Mongoid # :nodoc:
       def initialize(metadata, object)
         @metadata, @object = metadata, object
       end
+
+      protected
+      # Do we need to perform a database query? It will be so if the object we
+      # have is not a +Document+, +ActiveRecord::Base+, or
+      # +DataMapper::Resource+
+      #
+      # Example:
+      #
+      # <tt>builder.query?</tt>
+      #
+      # Returns:
+      #
+      # true if the object responds to <tt>#attributes</tt>.
+      def query?
+        !@object.respond_to?(:attributes)
+      end
     end
   end
 end
--- lib/mongoid/relations/builders/referenced/in.rb
+++ lib/mongoid/relations/builders/referenced/in.rb
@@ -3,7 +3,22 @@ module Mongoid # :nodoc:
   module Relations #:nodoc:
     module Builders #:nodoc:
       module Referenced #:nodoc:
-        class In
+        class In < Builder
+
+          # This builder either takes a foreign key and queries for the
+          # object or a document, where it will just return it.
+          #
+          # Example:
+          #
+          # <tt>Builder.new(meta, attrs).build</tt>
+          #
+          # Returns:
+          #
+          # A single +Document+.
+          def build
+            return @object unless query?
+            @metadata.klass.find(@object[@metadata.foreign_key])
+          end
         end
       end
     end
