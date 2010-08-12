8d1567c8f0c8af47ac3612e611d4af13c7895e67
Durran Jordan <durran@gmail.com>
2010-08-12 10:55:22 -0400

    Adding in many to many and one builders for relational associations

--- lib/mongoid/relations/builders/referenced/many_to_many.rb
+++ lib/mongoid/relations/builders/referenced/many_to_many.rb
@@ -3,7 +3,22 @@ module Mongoid # :nodoc:
   module Relations #:nodoc:
     module Builders #:nodoc:
       module Referenced #:nodoc:
-        class ManyToMany
+        class ManyToMany < Builder
+
+          # This builder either takes a hash and queries for the
+          # object or an array of documents, where it will just return them.
+          #
+          # Example:
+          #
+          # <tt>Builder.new(meta, attrs).build</tt>
+          #
+          # Returns:
+          #
+          # An array of documents.
+          def build
+            return @object unless query?
+            @metadata.klass.find(@object)
+          end
         end
       end
     end
--- lib/mongoid/relations/builders/referenced/one.rb
+++ lib/mongoid/relations/builders/referenced/one.rb
@@ -3,7 +3,24 @@ module Mongoid # :nodoc:
   module Relations #:nodoc:
     module Builders #:nodoc:
       module Referenced #:nodoc:
-        class One
+        class One < Builder
+
+          # This builder either takes an _id or an object and queries for the
+          # inverse side using the id or sets the object.
+          #
+          # Example:
+          #
+          # <tt>Builder.new(meta, attrs).build</tt>
+          #
+          # Returns:
+          #
+          # A single +Document+.
+          def build
+            return @object unless query?
+            @metadata.klass.first(
+              :conditions => { @metadata.foreign_key => @object }
+            )
+          end
         end
       end
     end
