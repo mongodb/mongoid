da2e283863b44e99b2ff2064a820da0702483f02
Durran Jordan <durran@gmail.com>
2010-08-22 23:04:12 -0400

    Adding in unit spec for references_one binding

--- spec/unit/mongoid/relations/bindings/referenced/in_spec.rb
+++ spec/unit/mongoid/relations/bindings/referenced/in_spec.rb
@@ -2,4 +2,7 @@ require "spec_helper"
 
 describe Mongoid::Relations::Bindings::Referenced::In do
 
+  let(:klass) do
+    Mongoid::Relations::Bindings::Referenced::In
+  end
 end
--- spec/unit/mongoid/relations/bindings/referenced/one_spec.rb
+++ spec/unit/mongoid/relations/bindings/referenced/one_spec.rb
@@ -2,4 +2,73 @@ require "spec_helper"
 
 describe Mongoid::Relations::Bindings::Referenced::One do
 
+  let(:klass) do
+    Mongoid::Relations::Bindings::Referenced::One
+  end
+
+  let(:base) do
+    Person.new
+  end
+
+  let(:target) do
+    Post.new
+  end
+
+  describe "#bind" do
+
+    let(:metadata) do
+      stub(
+        :extension? => false,
+        :foreign_key_setter => "person_id=",
+        :inverse => :person,
+        :inverse_setter => "person="
+      )
+    end
+
+    let(:binding) do
+      klass.new(base, target, metadata)
+    end
+
+    context "when the base is bindable" do
+
+      before do
+        binding.expects(:bindable?).with(base).returns(true)
+      end
+
+      it "sets the base and base id on the target" do
+        target.expects(:send).with("person_id=", base.id)
+        target.expects(:send).with("person=", base)
+        binding.bind
+      end
+    end
+  end
+
+  describe "#unbind" do
+
+    let(:metadata) do
+      stub(
+        :extension? => false,
+        :foreign_key_setter => "person_id=",
+        :inverse => :post,
+        :inverse_setter => "post="
+      )
+    end
+
+    let(:binding) do
+      klass.new(base, target, metadata)
+    end
+
+    context "when the target is unbindable" do
+
+      before do
+        binding.expects(:unbindable?).with(target).returns(true)
+      end
+
+      it "removes the foreign key and inverse from the target" do
+        target.expects(:send).with("person_id=", nil)
+        target.expects(:send).with("post=", nil)
+        binding.unbind
+      end
+    end
+  end
 end
