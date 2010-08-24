f38e3efa4fb61204cddf8e98595edf966e304c1e
Jacques Crocker <railsjedi@gmail.com>
2010-08-23 22:21:24 -0700

    Update rake_rubies to use 1.9.2 release instead of 1.9.2-head

--- rake_rubies.sh
+++ rake_rubies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rvm ruby-1.8.7@mongoid ruby -S bundle install      || exit $?
 rvm ruby-1.9.1@mongoid ruby -S bundle install      || exit $?
-rvm ruby-1.9.2-head@mongoid ruby -S bundle install || exit $?
+rvm ruby-1.9.2@mongoid ruby -S bundle install || exit $?
 rvm ree@mongoid ruby -S bundle install             || exit $?
-rvm 1.8.7@mongoid,1.9.1,1.9.2-head,ree rake spec:progress
+rvm 1.8.7@mongoid,1.9.1,1.9.2,ree rake spec:progress
