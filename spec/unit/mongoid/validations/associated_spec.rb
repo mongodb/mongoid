579d5030f8fc517aa0b5de40028664d6a1ef2591
Durran Jordan <durran@gmail.com>
2010-09-16 16:48:45 -0400

    Revert "Getting more specific on validates associated messages. Fixes #255."
    This reverts commit 05d497234ff9002876abc7624daf6a2e788d51e9.

--- spec/unit/mongoid/validations/associated_spec.rb
+++ spec/unit/mongoid/validations/associated_spec.rb
@@ -4,115 +4,100 @@ describe Mongoid::Validations::AssociatedValidator do
 
   describe "#validate_each" do
 
-    let(:person) do
-      Person.new
+    before do
+      @document = Person.new
     end
 
-    let(:validator) do
-      Mongoid::Validations::AssociatedValidator.new(
-        :attributes => person.attributes
-      )
-    end
+    let(:validator) { Mongoid::Validations::AssociatedValidator.new(:attributes => @document.attributes) }
 
     context "when the association is a has one" do
 
       context "when the association is nil" do
 
         before do
-          validator.validate_each(person, :name, nil)
+          validator.validate_each(@document, :name, nil)
         end
 
         it "adds no errors" do
-          person.errors[:name].should be_empty
+          @document.errors[:name].should be_empty
         end
+
       end
 
       context "when the association is valid" do
 
-        let(:associated) do
-          stub(:valid? => true)
-        end
-
         before do
-          validator.validate_each(person, :name, associated)
+          @associated = stub(:valid? => true)
+          validator.validate_each(@document, :name, @associated)
         end
 
         it "adds no errors" do
-          person.errors[:name].should be_empty
+          @document.errors[:name].should be_empty
         end
+
       end
 
       context "when the association is invalid" do
 
-        let!(:associated) do
-          Address.new
-        end
-
         before do
-          associated.expects(:valid?).at_least(1).returns(false)
-          associated.errors.add(:street, "is required")
-          associated.errors.add(:city, "is required")
-          validator.validate_each(person, :addresses, [ associated ])
+          @associated = stub(:valid? => false)
+          validator.validate_each(@document, :name, @associated)
         end
 
         it "adds errors to the parent document" do
-          person.errors[:addresses].should_not be_empty
+          @document.errors[:name].should_not be_empty
         end
 
         it "translates the error in english" do
-          person.errors[:addresses][0].should ==
-            [ "Street is required", "City is required" ]
+          @document.errors[:name][0].should == "is invalid"
         end
+
       end
+
     end
 
-    context "when the association is a embeds many" do
+    context "when the association is a has many" do
 
       context "when the association is empty" do
 
         before do
-          validator.validate_each(person, :addresses, [])
+          validator.validate_each(@document, :addresses, [])
         end
 
         it "adds no errors" do
-          person.errors[:addresses].should be_empty
+          @document.errors[:addresses].should be_empty
         end
+
       end
 
       context "when the association has invalid documents" do
 
-
-        let(:errors) do
-          stub(:full_messages => [ "is invalid" ])
-        end
-
-        let(:associated) do
-          stub(:valid? => false, :errors => errors)
-        end
-
         before do
-          validator.validate_each(person, :addresses, [ associated ])
+          @associated = stub(:valid? => false)
+          validator.validate_each(@document, :addresses, [ @associated ])
         end
 
         it "adds errors to the parent document" do
-          person.errors[:addresses].should_not be_empty
+          @document.errors[:addresses].should_not be_empty
         end
+
       end
 
       context "when the assocation has all valid documents" do
 
-        let(:associated) do
-          stub(:valid? => true)
-        end
-
         before do
-          validator.validate_each(person, :addresses, [ associated ])
+          @associated = stub(:valid? => true)
+          validator.validate_each(@document, :addresses, [ @associated ])
         end
 
         it "adds no errors" do
-          person.errors[:addresses].should be_empty
+          @document.errors[:addresses].should be_empty
         end
+
       end
+
     end
+
   end
+
 end
