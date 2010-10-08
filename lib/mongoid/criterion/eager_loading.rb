5037a54c85f72bdeea6487266458b808cbb0c16f
Richard Huang <flyerhzm@gmail.com>
2010-10-08 23:46:52 +0800

    support referenced_in and references_one eager loading

--- lib/mongoid/criterion/eager_loading.rb
+++ lib/mongoid/criterion/eager_loading.rb
@@ -15,17 +15,36 @@ module Mongoid #:nodoc:
 
       def includes(*options)
         @eager_loadings = options
+        self
       end
 
       def preload(documents)
-        ids = documents.collect(&:id)
-        foreign_key_name = "#{documents.first.class.to_s.underscore}_id"
+        document_class = documents.first.class
         @eager_loadings.each do |eager_loading|
-          eager_associations = eager_loading.to_s.camelize.constantize.where(foreign_key_name.to_sym.in => ids)
-          documents.each do |document|
-            document.send(eager_loading, eager_associations.select { |eager_association| eager_association.send(foreign_key_name) == document.id })
+          reflection = association_reflection(document_class, eager_loading)
+          if reflection.association == Mongoid::Associations::ReferencesOne
+            ids = documents.collect(&:id)
+            eager_associations = reflection.name.camelize.constantize.where(reflection.foreign_key.to_sym.in => ids).to_a
+            documents.each do |document|
+              document.send("#{reflection.name}=", eager_associations.find { |eager_association|
+                eager_association.send(reflection.foreign_key) == document.id
+              })
+            end
+          elsif reflection.association == Mongoid::Associations::ReferencedIn
+            ids = documents.collect(&:"#{reflection.foreign_key}")
+            eager_associations = reflection.name.camelize.constantize.find(ids).to_a
+            documents.each do |document|
+              document.send("#{reflection.name}=", eager_associations.find { |eager_association|
+                eager_association.id == document.send(reflection.foreign_key)
+              })
+            end
+          else
           end
         end
+      end
+      
+      def association_reflection(document_class, eager_loading)
+        document_class.reflect_on_association(eager_loading)
       end
     end
   end
--- spec/unit/mongoid/criterion/eager_loading_spec.rb
+++ spec/unit/mongoid/criterion/eager_loading_spec.rb
@@ -3,6 +3,11 @@ require "spec_helper"
 describe Mongoid::Criterion::EagerLoading do
 
   describe "#includes" do
+    
+    it "should return self" do
+      criteria = Mongoid::Criteria.new(Person)
+      criteria.includes(:game).should == criteria
+    end
 
     it "set eager loadings" do
       criteria = Mongoid::Criteria.new(Person)
@@ -20,14 +25,39 @@ describe Mongoid::Criterion::EagerLoading do
       person2.create_game(:score => 20)
     end
 
-    it "preload references_in association" do
+    it "preload references_one association" do
       complex = stub(:key => :person_id, :operator => "in")
       Mongoid::Criterion::Complex.expects(:new).with(:key => :person_id, :operator => "in").returns(complex)
       Game.expects(:where).with(complex => [person1.id, person2.id]).returns([person1.game, person2.game])
       criteria = Mongoid::Criteria.new(Person)
-      documents = Person.all.to_a
       criteria.includes(:game)
+      criteria.preload([person1, person2])
+    end
+
+    it "preload referenced_in association" do
+      criteria = Mongoid::Criteria.new(Game)
+      documents = [person1.game, person2.game]
+      Person.expects(:find).with([person1.id, person2.id]).returns([person1, person2])
+      criteria.includes(:person)
       criteria.preload(documents)
+    end
+  end
+  
+  describe "#association_reflection" do
+    let(:person1) { Person.create(:title => "Sir", :age => 100, :aliases => ["D", "Durran"], :ssn => "666666666") }
+    let(:person2) { Person.create(:title => "Madam", :age => 1, :ssn => "098-76-5434") }
+
+    before do
+      person1.create_game(:score => 10)
+      person2.create_game(:score => 20)
+    end
+    
+    it "with referenced_in" do
+      criteria = Mongoid::Criteria.new(Person)
+      criteria.includes(:game)
+      reflection = criteria.association_reflection(Person, :game)
+      reflection.foreign_key.should == "person_id"
+      reflection.name.should == "game"
     end
   end
 end
