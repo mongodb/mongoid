25ac24dd5c821457e83776d0bae50d1b590ea20f
Richard Huang <flyerhzm@gmail.com>
2010-10-10 18:41:40 +0800

    recover data creation in eager loading benchmark

--- perf/eager_loading_benchmark.rb
+++ perf/eager_loading_benchmark.rb
@@ -7,8 +7,8 @@ Mongoid.configure do |config|
   config.master = Mongo::Connection.new.db("mongoid_perf_test", :logger => Logger.new($stdout, :info))
 end
 
-#Mongoid.master.collection("people").drop
-#Mongoid.master.collection("posts").drop
+Mongoid.master.collection("people").drop
+Mongoid.master.collection("posts").drop
 
 class Person
   include Mongoid::Document
@@ -24,33 +24,33 @@ class Post
   referenced_in :person
 end
 
-#10000.times do |n|
-  #person = Person.create(:name => "Test_#{n}")
-  #person.posts.create(:title => "Test_#{2*n}")
-  #person.posts.create(:title => "Test_#{2*n+1}")
-#end
+10000.times do |n|
+  person = Person.create(:name => "Test_#{n}")
+  person.posts.create(:title => "Test_#{2*n}")
+  person.posts.create(:title => "Test_#{2*n+1}")
+end
 
 puts "Starting benchmark..."
 Benchmark.bm do |bm|
   bm.report("Finding 10 posts with person, without eager loading") do
-    Post.all.limit(10).each { |p| p.person.name }
+    Post.limit(10).each { |p| p.person.name }
   end
 
   bm.report("Finding 10 posts with person, with eager loading") do
-    Post.includes(:person).limit(10).each { |p| p.person.name }
+    Post.limit(10).includes(:person).each { |p| p.person.name }
   end
   bm.report("Finding 50 posts with person, without eager loading") do
-    Post.all.limit(50).each { |p| p.person.name }
+    Post.limit(50).each { |p| p.person.name }
   end
 
   bm.report("Finding 50 posts with person, with eager loading") do
-    Post.includes(:person).limit(50).each { |p| p.person.name }
+    Post.limit(50).includes(:person).each { |p| p.person.name }
   end
   bm.report("Finding 100 posts with person, without eager loading") do
-    Post.all.limit(100).each { |p| p.person.name }
+    Post.limit(100).each { |p| p.person.name }
   end
 
   bm.report("Finding 100 posts with person, with eager loading") do
-    Post.includes(:person).limit(100).each { |p| p.person.name }
+    Post.limit(100).includes(:person).each { |p| p.person.name }
   end
 end
