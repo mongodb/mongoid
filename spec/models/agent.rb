ce60e34cc958720f4fb11e6a3d6ea6771bd5da30
Cyril Mougel <cyril.mougel@gmail.com>
2010-11-19 00:18:42 +0100

    Fix spec to ruby 1.8.7 with avoid useless association

--- spec/models/agent.rb
+++ spec/models/agent.rb
@@ -3,6 +3,4 @@ class Agent
   field :title
   field :number
   embeds_many :names, :as => :namable
-  references_many :posts, :foreign_key => :poster_id
-  accepts_nested_attributes_for :posts, :allow_destroy => true
 end
