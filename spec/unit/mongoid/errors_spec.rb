ba3ec548134dd2868bec78b5b8c4af58ce60d868
Nick Hoffman <nick@deadorange.com>
2010-11-22 22:28:16 -0500

    Added a failing spec for the "indentifiers" typo.

--- spec/unit/mongoid/errors_spec.rb
+++ spec/unit/mongoid/errors_spec.rb
@@ -4,6 +4,13 @@ describe Mongoid::Errors do
 
   describe Mongoid::Errors::DocumentNotFound do
 
+    describe "attribute readers" do
+
+      it 'exists for @identifiers' do
+        Mongoid::Errors::DocumentNotFound.new(Person, "3").should respond_to :identifiers
+      end
+    end
+
     describe "#message" do
 
       context "default" do
