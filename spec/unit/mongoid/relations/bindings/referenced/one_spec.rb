c0ffa0ff32b39eded088f35e678b96e127a3efb9
Chad Jablonski <chad@dinocore.net>
2010-12-04 13:53:05 -0600

    Mongoid::Relations::Bindings::Referenced::One spec created

--- spec/unit/mongoid/relations/bindings/referenced/one_spec.rb
+++ spec/unit/mongoid/relations/bindings/referenced/one_spec.rb
@@ -1,4 +1,93 @@
 require "spec_helper"
 
 describe Mongoid::Relations::Bindings::Referenced::One do
+
+  let(:klass) do
+    Mongoid::Relations::Bindings::Referenced::One
+  end
+
+  let(:user) do
+    User.new
+  end
+
+  let(:account) do
+    Account.new
+  end
+
+  let(:target) do
+    account
+  end
+
+  let(:metadata) do
+    User.relations["account"]
+  end
+
+  describe "#bind" do
+
+    let(:binding) do
+      klass.new(user, target, metadata)
+    end
+
+    context "when the document is bindable" do
+
+      before do
+        binding.bind
+      end
+
+      it "sets the inverse foreign key" do
+        account.creator_id.should == user.id
+      end
+
+      it "sets the inverse relation" do
+        account.creator.should == user
+      end
+    end
+
+    context "when the document is not bindable" do
+
+      before do
+        user.account = account
+      end
+
+      it "does nothing" do
+        account.expects(:creator=).never
+        binding.bind
+      end
+    end
+  end
+
+  describe "#unbind" do
+
+    let(:binding) do
+      klass.new(user, target, metadata)
+    end
+
+    context "when the document is bindable" do
+
+      before do
+        binding.bind
+        binding.unbind
+      end
+
+      it "removes the inverse foreign key" do
+        account.creator_id.should == nil
+      end
+
+      it "removes the inverse relation" do
+        account.creator.should == nil
+      end
+    end
+
+    context "when the document is not bindable" do
+
+      before do
+        user.account = account
+      end
+
+      it "does nothing" do
+        account.expects(:creator=).never
+        binding.bind
+      end
+    end
+  end
 end
