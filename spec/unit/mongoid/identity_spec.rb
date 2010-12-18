7c7cf091ed77ec03223add1d861e3f8963c43537
Durran Jordan <durran@gmail.com>
2010-12-18 18:47:22 +0100

    Documents now know to store type information on documents in a polymorphic relation.

--- spec/unit/mongoid/identity_spec.rb
+++ spec/unit/mongoid/identity_spec.rb
@@ -2,30 +2,55 @@ require "spec_helper"
 
 describe Mongoid::Identity do
 
-  describe ".create" do
+  describe "#create" do
 
-    let(:name) { Name.new }
+    let(:name) do
+      Name.new
+    end
+
+    context "when the class is polymorphic" do
+
+      let(:movie) do
+        Movie.new
+      end
+
+      before do
+        Mongoid::Identity.new(movie).create
+      end
+
+      it "sets the document type to the class name" do
+        movie._type.should == "Movie"
+      end
+    end
 
     context "when class is inherited" do
 
-      let(:canvas) { Canvas.new }
-
-      it "sets the document _type to the class name" do
-        Mongoid::Identity.new(canvas).create
-        canvas._type.should == "Canvas"
+      let(:canvas) do
+        Canvas.new
       end
 
+      before do
+        Mongoid::Identity.new(canvas).create
+      end
+
+      it "sets the document _type to the class name" do
+        canvas._type.should == "Canvas"
+      end
     end
 
     context "when class is a subclass" do
 
-      let(:browser) { Browser.new }
-
-      it "sets the document _type to the class name" do
-        Mongoid::Identity.new(browser).create
-        browser._type.should == "Browser"
+      let(:browser) do
+        Browser.new
       end
 
+      before do
+        Mongoid::Identity.new(browser).create
+      end
+
+      it "sets the document _type to the class name" do
+        browser._type.should == "Browser"
+      end
     end
 
     context "when not using inheritance" do
@@ -33,77 +58,84 @@ describe Mongoid::Identity do
       it "does not set the type" do
         name._type.should be_nil
       end
-
     end
 
     context "when the document has a primary key" do
 
+      let(:address) do
+        Address.allocate
+      end
+
       before do
-        @address = Address.allocate
-        @address.instance_variable_set(:@attributes, { "street" => "Market St"})
+        address.instance_variable_set(:@attributes, { "street" => "Market St"})
+        Mongoid::Identity.new(address).create
       end
 
       it "sets the id to the composite key" do
-        Mongoid::Identity.new(@address).create
-        @address.id.should == "market-st"
+        address.id.should == "market-st"
       end
-
     end
 
     context "when the document has no primary key" do
 
       context "when the document has no id" do
 
+        let(:person) do
+          Person.allocate
+        end
+
+        let(:object_id) do
+          stub(:to_s => "1")
+        end
+
         before do
-          @person = Person.allocate
-          @person.instance_variable_set(:@attributes, {})
-          @object_id = stub(:to_s => "1")
-          BSON::ObjectId.expects(:new).returns(@object_id)
+          person.instance_variable_set(:@attributes, {})
+          BSON::ObjectId.expects(:new).returns(object_id)
         end
 
         context "when using object ids" do
 
           before do
-            Person.identity :type => BSON::ObjectId
+            Person.identity(:type => BSON::ObjectId)
+            Mongoid::Identity.new(person).create
           end
 
           after do
-            Person.identity :type => String
+            Person.identity(:type => String)
           end
 
           it "sets the id to a mongo object id" do
-            Mongoid::Identity.new(@person).create
-            @person.id.should == @object_id
+            person.id.should == object_id
           end
         end
 
         context "when not using object ids" do
 
-          it "sets the id to a mongo object id string" do
-            Mongoid::Identity.new(@person).create
-            @person.id.should == "1"
+          before do
+            Mongoid::Identity.new(person).create
           end
 
+          it "sets the id to a mongo object id string" do
+            person.id.should == "1"
+          end
         end
-
       end
 
       context "when the document has an id" do
 
+        let(:person) do
+          Person.allocate
+        end
+
         before do
-          @person = Person.allocate
-          @person.instance_variable_set(:@attributes, { "_id" => "5" })
+          person.instance_variable_set(:@attributes, { "_id" => "5" })
+          Mongoid::Identity.new(person)
         end
 
         it "returns the existing id" do
-          Mongoid::Identity.new(@person)
-          @person.id.should == "5"
+          person.id.should == "5"
         end
-
       end
-
     end
-
   end
-
 end
