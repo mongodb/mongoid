18a81130d38ef77d73d43a55c60afdbc4e890f8c
Durran Jordan <durran@gmail.com>
2010-12-19 15:46:39 +0100

    Refactor of the persistence integration spec

--- spec/integration/mongoid/persistence_spec.rb
+++ spec/integration/mongoid/persistence_spec.rb
@@ -3,21 +3,29 @@ require "spec_helper"
 describe Mongoid::Persistence do
 
   before do
-    Mongoid.persist_in_safe_mode = true
-    @person = Person.new(:title => "Sir", :ssn => "6969696", :pets => true)
+    Person.delete_all
   end
 
-  after do
-    @person.delete
+  before(:all) do
+    Mongoid.persist_in_safe_mode = true
+  end
+
+  after(:all) do
     Mongoid.persist_in_safe_mode = false
   end
 
   describe ".create" do
 
-    it "saves and returns the document" do
-      person = Person.create(:title => "Sensei", :ssn => "666-66-6666")
+    let(:person) do
+      Person.create(:title => "Sensei", :ssn => "666-66-6666")
+    end
+
+    it "it saves the document" do
+      person.should be_persisted
+    end
+
+    it "returns the document" do
       person.should be_a_kind_of(Person)
-      person.should_not be_a_new_record
     end
   end
 
@@ -26,139 +34,110 @@ describe Mongoid::Persistence do
     context "inserting with a field that is not unique" do
 
       context "when a unique index exists" do
+
         before do
           Person.create_indexes
         end
 
-        after do
-          Person.delete_all
+        let!(:person) do
+          Person.create!(:ssn => "555-55-9999")
         end
 
         it "raises an error" do
-          Person.create!(:ssn => "555-55-9999")
-          lambda { Person.create!(:ssn => "555-55-9999") }.should raise_error
+          expect {
+            Person.create!(:ssn => "555-55-9999")
+          }.to raise_error
         end
       end
     end
   end
 
-  describe "#delete" do
+  [ :delete, :destroy ].each do |method|
 
-    context "deleting a root document" do
+    describe "##{method}" do
 
-      before do
-        @person.save
+      let(:person) do
+        Person.create(:ssn => "218-32-6789")
       end
 
-      it "deletes the document" do
-        @person.delete
-        lambda { Person.find(@person.id) }.should raise_error
-      end
+      context "when removing a root document" do
 
-      it "returns true" do
-        @person.delete.should be_true
-      end
-    end
+        let!(:deleted) do
+          person.send(method)
+        end
 
-    context "deleting an embedded document" do
+        it "deletes the document from the collection" do
+          expect {
+            Person.find(person.id)
+          }.to raise_error
+        end
 
-      before do
-        @address = Address.new(:street => "Bond Street")
-        @person.addresses << @address
-      end
-
-      context "when the document is not yet saved" do
-
-        it "removes the document from the parent" do
-          @address.delete
-          @person.addresses.should be_empty
-          @person.attributes[:addresses].should be_nil
+        it "returns true" do
+          deleted.should be_true
         end
       end
 
-      context "when the document has been saved" do
+      context "when removing an embedded document" do
 
-        before do
-          @address.save
+        let(:address) do
+          person.addresses.build(:street => "Bond Street")
         end
 
-        it "removes the object from the parent and database" do
-          @address.delete
-          from_db = Person.find(@person.id)
-          from_db.addresses.should be_empty
+        context "when the document is not yet saved" do
+
+          before do
+            address.send(method)
+          end
+
+          it "removes the document from the parent" do
+            person.addresses.should be_empty
+          end
+
+          it "removes the attributes from the parent" do
+            person.raw_attributes["addresses"].should be_nil
+          end
         end
-      end
-    end
-  end
 
-  describe "#destroy" do
+        context "when the document has been saved" do
 
-    context "destroying a root document" do
+          before do
+            address.save
+            address.send(method)
+          end
 
-      before do
-        @person.save
-      end
+          let(:from_db) do
+            Person.find(person.id)
+          end
 
-      it "destroys the document" do
-        @person.destroy
-        lambda { Person.find(@person.id) }.should raise_error
-      end
-
-      it "returns true" do
-        @person.destroy.should be_true
-      end
-    end
-
-    context "deleting an embedded document" do
-
-      before do
-        @address = Address.new(:street => "Bond Street", :city => "London", :country => "UK")
-        @person.addresses << @address
-      end
-
-      context "when the document is not yet saved" do
-
-        it "removes the document from the parent" do
-          @address.delete
-          @person.addresses.should be_empty
-          @person.attributes[:addresses].should be_nil
+          it "removes the object from the parent and database" do
+            from_db.addresses.should be_empty
+          end
         end
       end
 
-      context "when the document has been saved" do
+      context "when removing deeply embedded documents" do
 
-        before do
-          @address.save
-        end
+        context "when the document has been saved" do
 
-        it "removes the object from the parent and database" do
-          @address.delete
-          from_db = Person.find(@person.id)
-          from_db.addresses.should be_empty
-        end
-      end
-    end
+          let(:address) do
+            person.addresses.create(:street => "Bond Street")
+          end
 
-    context "deleting deeply embedded documents" do
+          let(:location) do
+            address.locations.create(:name => "Home")
+          end
 
-      before do
-        @address = Address.new(:street => "Bond Street", :city => "London", :country => "UK")
-        @person.addresses << @address
-      end
+          let(:from_db) do
+            Person.find(person.id)
+          end
 
-      context "when the document has been saved" do
+          before do
+            location.send(method)
+          end
 
-        before do
-          @address.save
-          @location = Location.new(:name => "Home")
-          @address.locations << @location
-          @location.save
-        end
-
-        it "removes the object from the parent and database" do
-          @location.delete
-          from_db = Person.find(@person.id)
-          from_db.addresses.first.locations.should be_empty
+          it "removes the object from the parent and database" do
+            from_db.addresses.first.locations.should be_empty
+          end
         end
       end
     end
@@ -166,26 +145,29 @@ describe Mongoid::Persistence do
 
   describe "#save" do
 
+    let(:person) do
+      Person.new(:ssn => "811-82-8345")
+    end
+
     context "when validation passes" do
 
       it "returns true" do
-        @person.save.should be_true
+        person.save.should be_true
       end
     end
 
     context "when validation fails" do
 
-      before do
-        @address = @person.addresses.create(:city => "London")
+      let(:address) do
+        person.addresses.create(:city => "London")
       end
 
-      it "returns false" do
-        @address.save.should == false
+      before do
+        address.save
       end
 
       it "has the appropriate errors" do
-        @address.save
-        @address.errors[:street].should == ["can't be blank"]
+        address.errors[:street].should == ["can't be blank"]
       end
     end
 
@@ -193,80 +175,118 @@ describe Mongoid::Persistence do
 
       context "when performing modification and insert ops" do
 
-        before do
-          @person = Person.create(:title => "Blah", :ssn => "244-01-1112")
-          @person.title = "King"
-          @address = @person.addresses.build(:street => "Bond St")
-          @person.create_name(:first_name => "Tony")
-          @person.name.first_name = "Ryan"
+        let(:person) do
+          Person.create(:title => "Blah", :ssn => "244-01-1112")
         end
 
-        it "saves the hierarchy" do
-          @person.save
-          @person.reload
-          @person.title.should == "King"
-          @person.name.first_name.should == "Ryan"
-          @person.addresses.first.street.should == "Bond St"
+        let!(:address) do
+          person.addresses.build(:street => "Bond St")
+        end
+
+        let!(:name) do
+          person.create_name(:first_name => "Tony")
+        end
+
+        let(:from_db) do
+          Person.find(person.id)
+        end
+
+        before do
+          person.title = "King"
+          name.first_name = "Ryan"
+        end
+
+        it "saves the root document" do
+          person.save
+          person.title.should == "King"
+        end
+
+        it "saves embedded many relations" do
+          person.save
+          person.addresses.first.street.should == "Bond St"
+        end
+
+        it "saves embedded one relations" do
+          person.save
+          person.name.first_name.should == "Ryan"
         end
 
         it "persists with proper set and push modifiers" do
-          @person._updates.should == {
-            "$set" => { "title" => "King", "name.first_name" => "Ryan" },
-            "$pushAll"=> { "addresses" => [{ "_id" => @address.id, "street"=>"Bond St" }]}
+          person._updates.should == {
+            "$set" => {
+              "title" => "King",
+              "name.first_name" => "Ryan"
+            },
+            "$pushAll"=> {
+              "addresses" => [ { "_id" => address.id, "street" => "Bond St" } ]
+            }
           }
-          @person.save
-          @person._updates.should == {}
         end
-
       end
 
       context "when combining modifications and pushes" do
+
+        let!(:person) do
+          Person.create(
+            :title => "Blah",
+            :ssn => "244-01-1112",
+            :addresses => [ address ]
+          )
+        end
+
+        let!(:address) do
+          Address.new(
+            :number => 101,
+            :street => 'South St',
+            :locations => [ location ]
+          )
+        end
+
+        let!(:location) do
+          Location.new(:name => 'Work')
+        end
+
+        let(:from_db) do
+          Person.find(person.id)
+        end
+
         before do
-          @location1 = Location.new(:name => 'Work')
-          @address1 = Address.new(:number => 101,
-                                  :street => 'South St',
-                                  :locations => [@location1])
-          @person = Person.create!(:addresses => [@address1])
+          address.number = 102
+          person.addresses << Address.new(:street => "North Ave")
+          person.save
         end
 
-        def append_address_save_and_reload
-          @person.addresses <<
-            Address.new(:street => 'North St')
-          @person.save
-          @person.reload
+        it "saves modifications to existing embedded docs" do
+          from_db.addresses[0].number.should == 102
         end
 
-        it "should allow modifications and pushes at the same level" do
-          @address1.number = 102
-          append_address_save_and_reload
-          @person.addresses[0].number.should == 102
-          @person.addresses[0].street.should == 'South St'
-          @person.addresses[0].locations.first.name.should == 'Work'
-          @person.addresses[1].street.should == 'North St'
+        it "saves modifications to new embedded docs" do
+          from_db.addresses[1].street.should == 'North Ave'
         end
 
-        it "should allow modifications one level deeper than pushes" do
-          @address1.locations.first.name = 'Home'
-          append_address_save_and_reload
-          @person.addresses[0].number.should == 101
-          @person.addresses[0].street.should == 'South St'
-          @person.addresses[0].locations.first.name.should == 'Home'
-          @person.addresses[1].street.should == 'North St'
+        it "saves modifications to deeply embedded docs" do
+          from_db.addresses[0].locations.first.name.should == 'Work'
         end
       end
 
       context "when removing elements without using delete or destroy" do
 
+        let!(:person) do
+          Person.create!(:title => "Blah", :ssn => "244-01-1112")
+        end
+
+        let(:from_db) do
+          Person.find(person.id)
+        end
+
         before do
-          @person = Person.create!(:title => "Blah", :ssn => "244-01-1112")
-          @person.create_name(:first_name => "Tony")
-          @person.name = nil
+          person.create_name(:first_name => "Tony")
+          person.name = nil
+          person.save
         end
 
         it "saves the hierarchy" do
-          @person.save
-          @person.reload
-          @person.name.should be_nil
+          person.name.should be_nil
         end
       end
     end
@@ -278,10 +298,16 @@ describe Mongoid::Persistence do
 
       context "when a unique index exists" do
 
-        it "raises an error" do
+        let(:person) do
+          Person.new(:ssn => "555-55-9999")
+        end
+
+        before do
           Person.create!(:ssn => "555-55-9999")
-          person = Person.new(:ssn => "555-55-9999")
-          lambda { person.save!(:ssn => "555-55-9999") }.should raise_error
+        end
+
+        it "raises an error" do
+          expect { person.save! }.to raise_error
         end
       end
     end
@@ -289,55 +315,49 @@ describe Mongoid::Persistence do
 
   describe "#update_attributes" do
 
-    before do
-      @person.save
+    let(:person) do
+      Person.create(:ssn => "717-98-9999")
     end
 
     context "when validation passes" do
 
+      let!(:saved) do
+        person.update_attributes(:pets => false)
+      end
+
+      let(:from_db) do
+        Person.find(person.id)
+      end
+
       it "returns true" do
-        @person.update_attributes(:ssn => "555-55-1234").should be_true
+        saved.should be_true
       end
 
       it "saves the attributes" do
-        @person.update_attributes(:ssn => "555-55-1235", :pets => false, :title => nil)
-        @from_db = Person.find(@person.id)
-        @from_db.ssn.should == "555-55-1235"
-        @from_db.pets.should == false
-        @from_db.title.should be_nil
+        from_db.pets.should be_false
       end
     end
   end
 
-  describe "#delete_all" do
+  [ :delete_all, :destroy_all ].each do |method|
 
-    before do
-      @person.save
-    end
+    describe "##{method}" do
 
-    it "deletes all the documents" do
-      Person.delete_all
-      Person.count.should == 0
-    end
+      let!(:person) do
+        Person.create(:ssn => "712-34-5111")
+      end
 
-    it "returns the number of documents deleted" do
-      Person.delete_all.should == 1
-    end
-  end
+      let!(:removed) do
+        Person.send(method)
+      end
 
-  describe "#destroy_all" do
+      it "removes all the documents" do
+        Person.count.should == 0
+      end
 
-    before do
-      @person.save
-    end
-
-    it "destroys all the documents" do
-      Person.destroy_all
-      Person.count.should == 0
-    end
-
-    it "returns the number of documents destroyed" do
-      Person.destroy_all.should == 1
+      it "returns the number of documents removed" do
+        removed.should == 1
+      end
     end
   end
 end
