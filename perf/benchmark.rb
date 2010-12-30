c9e6a8d6ae2d146b9d00fab87d4556c277aadc3b
Durran Jordan <durran@gmail.com>
2010-12-30 14:55:42 +0100

    Updating latest benchmark from the refactor branch

--- perf/benchmark.rb
+++ perf/benchmark.rb
@@ -177,10 +177,19 @@ end
 # Updating An Embedded Document 10k Times      2.550000   0.130000   2.680000 (  2.668367)
 # Appending A New Embedded Document 10k Times  5.080000   0.240000   5.320000 (  5.327933)
 # ---------------------------------------------------------------------------------------
-# 2.0.0.rc.1
+# 2.0.0.beta.20 (1.9.2)
 #
 # Saving 10k New Documents                    19.530000   0.270000  19.800000 ( 19.778292)
 # Querying & Iterating 10k Documents           1.150000   0.060000   1.210000 (  1.219632)
 # Updating The Root Document 10k Times         3.270000   0.120000   3.390000 (  3.387370)
 # Updating An Embedded Document 10k Times      2.680000   0.110000   2.790000 (  2.790347)
 # Appending A New Embedded Document 10k Times  7.230000   0.240000   7.470000 (  7.458122)
+#
+# ---------------------------------------------------------------------------------------
+# 2.0.0.rc.1 (1.9.2)
+
+# Saving 10k New Documents                    12.230000   0.130000  12.360000 ( 12.345798)
+# Querying & Iterating 10k Documents           0.560000   0.050000   0.610000 (  0.615744)
+# Updating The Root Document 10k Times         3.430000   0.090000   3.520000 (  3.520051)
+# Updating An Embedded Document 10k Times      1.810000   0.090000   1.900000 (  1.900921)
+# Appending A New Embedded Document 10k Times  6.950000   0.230000   7.180000 (  7.175059)
