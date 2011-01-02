16d61d94cc81ac18c2330f7afb32ad54da1f36ef
Durran Jordan <durran@gmail.com>
2011-01-02 16:03:18 +0100

    Adding safeguard integration specs around reloading documents and their relations from the database

--- spec/integration/mongoid/relations/accessors_spec.rb
+++ spec/integration/mongoid/relations/accessors_spec.rb
@@ -14,20 +14,20 @@ describe Mongoid::Relations::Accessors do
         Person.create(:ssn => "666-66-6666")
       end
 
-      context "when reloading" do
+      context "when the relation is a many to many" do
 
-        context "when reloading with a many to many" do
+        let(:preference) do
+          Preference.create(:name => "Setting")
+        end
 
-          let(:preference) do
-            Preference.create(:name => "Setting")
-          end
+        before do
+          person.preferences << preference
+        end
+
+        context "when reloading the relation directly" do
 
           let(:preferences) do
             person.preferences(true)
-          end
-
-          before do
-            person.preferences << preference
           end
 
           it "reloads the correct documents" do
@@ -36,6 +36,136 @@ describe Mongoid::Relations::Accessors do
 
           it "reloads a new instance" do
             preferences.first.should_not equal(preference)
+          end
+        end
+
+        context "when reloading via the base document" do
+
+          let(:preferences) do
+            person.reload.preferences
+          end
+
+          it "reloads the correct documents" do
+            preferences.should == [ preference ]
+          end
+
+          it "reloads a new instance" do
+            preferences.first.should_not equal(preference)
+          end
+        end
+
+        context "when performing a fresh find on the base" do
+
+          let(:preferences) do
+            Person.find(person.id).preferences
+          end
+
+          it "reloads the correct documents" do
+            preferences.should == [ preference ]
+          end
+        end
+      end
+
+      context "when the relation is a many to one" do
+
+        let(:post) do
+          Post.create(:title => "First!")
+        end
+
+        before do
+          person.posts << post
+        end
+
+        context "when reloading the relation directly" do
+
+          let(:posts) do
+            person.posts(true)
+          end
+
+          it "reloads the correct documents" do
+            posts.should == [ post ]
+          end
+
+          it "reloads a new instance" do
+            posts.first.should_not equal(post)
+          end
+        end
+
+        context "when reloading via the base document" do
+
+          let(:posts) do
+            person.reload.posts
+          end
+
+          it "reloads the correct documents" do
+            posts.should == [ post ]
+          end
+
+          it "reloads a new instance" do
+            posts.first.should_not equal(post)
+          end
+        end
+
+        context "when performing a fresh find on the base" do
+
+          let(:posts) do
+            Person.find(person.id).posts
+          end
+
+          it "reloads the correct documents" do
+            posts.should == [ post ]
+          end
+        end
+      end
+
+      context "when the relation is a references one" do
+
+        let(:game) do
+          Game.create(:name => "Centipeded")
+        end
+
+        before do
+          person.game = game
+        end
+
+        context "when reloading the relation directly" do
+
+          let(:reloaded_game) do
+            person.game(true)
+          end
+
+          it "reloads the correct documents" do
+            reloaded_game.should == game
+          end
+
+          it "reloads a new instance" do
+            reloaded_game.should_not equal(game)
+          end
+        end
+
+        context "when reloading via the base document" do
+
+          let(:reloaded_game) do
+            person.reload.game
+          end
+
+          it "reloads the correct documents" do
+            reloaded_game.should == game
+          end
+
+          it "reloads a new instance" do
+            reloaded_game.should_not equal(game)
+          end
+        end
+
+        context "when performing a fresh find on the base" do
+
+          let(:reloaded_game) do
+            Person.find(person.id).game
+          end
+
+          it "reloads the correct documents" do
+            reloaded_game.should == game
           end
         end
       end
