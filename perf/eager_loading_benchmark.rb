81b708256df9c728a59cd9581d466bded91934e7
Richard Huang <flyerhzm@gmail.com>
2011-01-20 15:39:58 +0800

    update benchmark for eager loading

--- perf/eager_loading_benchmark.rb
+++ perf/eager_loading_benchmark.rb
@@ -4,7 +4,8 @@ require "benchmark"
 require "mongoid"
 
 Mongoid.configure do |config|
-  config.master = Mongo::Connection.new.db("mongoid_perf_test", :logger => Logger.new($stdout, :info))
+  #config.master = Mongo::Connection.new('localhost', 27018, :logger => Logger.new($stdout)).db("mongoid_perf_test")
+  config.master = Mongo::Connection.new.db("mongoid_perf_test")
 end
 
 Mongoid.master.collection("people").drop
@@ -31,26 +32,43 @@ end
 end
 
 puts "Starting benchmark..."
-Benchmark.bm do |bm|
+Benchmark.bm(60) do |bm|
   bm.report("Finding 10 posts with person, without eager loading") do
     Post.limit(10).each { |p| p.person.name }
   end
-
   bm.report("Finding 10 posts with person, with eager loading") do
     Post.limit(10).includes(:person).each { |p| p.person.name }
   end
+
   bm.report("Finding 50 posts with person, without eager loading") do
     Post.limit(50).each { |p| p.person.name }
   end
-
   bm.report("Finding 50 posts with person, with eager loading") do
     Post.limit(50).includes(:person).each { |p| p.person.name }
   end
+
   bm.report("Finding 100 posts with person, without eager loading") do
     Post.limit(100).each { |p| p.person.name }
   end
-
   bm.report("Finding 100 posts with person, with eager loading") do
     Post.limit(100).includes(:person).each { |p| p.person.name }
   end
+
+  bm.report("Finding 1000 posts with person, without eager loading") do
+    Post.limit(1000).each { |p| p.person.name }
+  end
+  bm.report("Finding 1000 posts with person, with eager loading") do
+    Post.limit(1000).includes(:person).each { |p| p.person.name }
+  end
 end
+
+#                                                                  user     system      total        real
+#Finding 10 posts with person, without eager loading           0.000000   0.000000   0.000000 (  0.004790)
+#Finding 10 posts with person, with eager loading              0.010000   0.000000   0.010000 (  0.002065)
+#Finding 50 posts with person, without eager loading           0.010000   0.000000   0.010000 (  0.021131)
+#Finding 50 posts with person, with eager loading              0.010000   0.000000   0.010000 (  0.007007)
+#Finding 100 posts with person, without eager loading          0.050000   0.010000   0.060000 (  0.060699)
+#Finding 100 posts with person, with eager loading             0.010000   0.000000   0.010000 (  0.014336)
+#Finding 1000 posts with person, without eager loading         0.470000   0.020000   0.490000 (  0.549790)
+#Finding 1000 posts with person, with eager loading            0.170000   0.010000   0.180000 (  0.184104)
+
