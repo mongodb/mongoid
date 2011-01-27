2d95334cfb7c25fec2964dde3990020b8cebb6d2
Durran Jordan <durran@gmail.com>
2011-01-27 14:32:30 +0100

    Verify safe mode failures dont get picked up as validation errors. Fixes #440.

--- spec/functional/mongoid/safety_spec.rb
+++ spec/functional/mongoid/safety_spec.rb
@@ -2,9 +2,12 @@ require "spec_helper"
 
 describe Mongoid::Safety do
 
+  before(:all) do
+    Mongoid.autocreate_indexes = true
+  end
+
   before do
     Person.delete_all
-    Mongoid.autocreate_indexes = true
   end
 
   after do
@@ -23,14 +26,83 @@ describe Mongoid::Safety do
         Person.safely.create(:ssn => "432-97-1111")
       end
 
-      it "inserts the document" do
-        Person.count.should == 1
+      context "when no error occurs" do
+
+        it "inserts the document" do
+          Person.count.should == 1
+        end
       end
 
-      it "overrides the global config option" do
-        lambda {
-          Person.safely.create(:ssn => "432-97-1111")
-        }.should raise_error(Mongo::OperationFailure)
+      context "when a mongodb error occurs" do
+
+        it "bubbles up to the caller" do
+          lambda {
+            Person.safely.create(:ssn => "432-97-1111")
+          }.should raise_error(Mongo::OperationFailure)
+        end
+      end
+    end
+
+    describe ".create!" do
+
+      before do
+        Person.safely.create!(:ssn => "432-97-1112")
+      end
+
+      context "when no error occurs" do
+
+        it "inserts the document" do
+          Person.count.should == 1
+        end
+      end
+
+      context "when a mongodb error occurs" do
+
+        it "bubbles up to the caller" do
+          lambda {
+            Person.safely.create!(:ssn => "432-97-1112")
+          }.should raise_error(Mongo::OperationFailure)
+        end
+      end
+    end
+
+    describe ".save" do
+
+      before do
+        Person.safely.create(:ssn => "432-97-1113")
+      end
+
+      context "when a mongodb error occurs" do
+
+        let(:person) do
+          Person.new(:ssn => "432-97-1113")
+        end
+
+        it "bubbles up to the caller" do
+          lambda {
+            person.safely.save(:ssn => "432-97-1113")
+          }.should raise_error(Mongo::OperationFailure)
+        end
+      end
+    end
+
+    describe ".save!" do
+
+      before do
+        Person.safely.create!(:ssn => "432-97-1114")
+      end
+
+      context "when a mongodb error occurs" do
+
+        let(:person) do
+          Person.new(:ssn => "432-97-1114")
+        end
+
+        it "bubbles up to the caller" do
+          lambda {
+            person.safely.save!(:ssn => "432-97-1113")
+          }.should raise_error(Mongo::OperationFailure)
+        end
       end
     end
   end
