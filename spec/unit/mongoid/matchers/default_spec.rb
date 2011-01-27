38af7a242b85eee8eeaf71323f9d2901a022a218
Durran Jordan <durran@gmail.com>
2011-01-27 13:37:59 +0100

    Default matcher should not only check inclusion of strings in arrays, but any object. Fixes #614.

--- spec/unit/mongoid/matchers/default_spec.rb
+++ spec/unit/mongoid/matchers/default_spec.rb
@@ -2,49 +2,98 @@ require "spec_helper"
 
 describe Mongoid::Matchers::Default do
 
-
   describe "#matches?" do
 
-    let(:matcher) { Mongoid::Matchers::Default.new("Testing") }
+    context "when comparing strings" do
 
-    context "when the values are equal" do
-
-      it "returns true" do
-        matcher.matches?("Testing").should be_true
+      let(:matcher) do
+        Mongoid::Matchers::Default.new("Testing")
       end
 
-    end
+      context "when the values are equal" do
 
-    context "when the values are not equal" do
-
-      it "returns false" do
-        matcher.matches?("Other").should be_false
+        it "returns true" do
+          matcher.matches?("Testing").should be_true
+        end
       end
 
+      context "when the values are not equal" do
+
+        it "returns false" do
+          matcher.matches?("Other").should be_false
+        end
+      end
     end
 
+    context "when comparing object ids" do
+
+      let(:object_id) do
+        BSON::ObjectId.new
+      end
+
+      let(:matcher) do
+        Mongoid::Matchers::Default.new(object_id)
+      end
+
+      context "when the values are equal" do
+
+        it "returns true" do
+          matcher.matches?(object_id).should be_true
+        end
+      end
+
+      context "when the values are not equal" do
+
+        it "returns false" do
+          matcher.matches?(BSON::ObjectId.new).should be_false
+        end
+      end
+    end
+
+    context "when comparing a string to an array" do
+
+      let(:matcher) do
+        Mongoid::Matchers::Default.new(["Test1", "Test2", "Test3"])
+      end
+
+      context "when the attribute contains the value" do
+
+        it "returns true" do
+          matcher.matches?("Test1").should be_true
+        end
+      end
+
+      context "when the attribute does not contain the value" do
+
+        it "returns false" do
+          matcher.matches?("Test4").should be_false
+        end
+      end
+    end
+
+    context "when comparing an object id to an array" do
+
+      let(:object_id) do
+        BSON::ObjectId.new
+      end
+
+      let(:matcher) do
+        Mongoid::Matchers::Default.new([ object_id, BSON::ObjectId.new ])
+      end
+
+      context "when the attribute contains the value" do
+
+        it "returns true" do
+          matcher.matches?(object_id).should be_true
+        end
+      end
+
+      context "when the attribute does not contain the value" do
+
+        it "returns false" do
+          matcher.matches?(BSON::ObjectId.new).should be_false
+        end
+      end
+    end
   end
-
-  describe "#matches? when comparing a String to an Array attribute" do
-
-    let(:matcher) { Mongoid::Matchers::Default.new(["Test1", "Test2", "Test3"]) }
-
-    context "when the attribute contains the value" do
-
-      it "returns true" do
-        matcher.matches?("Test1").should be_true
-      end
-
-    end
-
-    context "when the attribute does not contain the value" do
-
-      it "returns false" do
-        matcher.matches?("Test4").should be_false
-      end
-
-    end
-
-  end
-
 end
