72952b4309bab798e3617925b9eef6ba96318d53
Konstantin Shabanov <etehtsea@gmail.com>
2011-02-07 22:30:07 +0700

    spec for #283

--- spec/functional/mongoid/modifiers/inc_spec.rb
+++ spec/functional/mongoid/modifiers/inc_spec.rb
@@ -4,12 +4,17 @@ describe Mongoid::Modifiers::Inc do
 
   describe "#inc" do
 
+    before(:all) { Person.delete_all }
+    after(:all)  { Person.delete_all }
+
     let(:person) do
       Person.create(:ssn => "777-66-1010")
     end
 
     before do
       person.inc(:age, 2)
+      person.inc(:score, 2)
+      person.inc(:high_score, 5)
     end
 
     it "increments the field by the value" do
@@ -18,6 +23,14 @@ describe Mongoid::Modifiers::Inc do
 
     it "updates the database with the new value" do
       person.reload.age.should == 102
+    end
+
+    it "increments nil field by the value" do
+      person.score.should == 2
+    end
+
+    it "sets and increments non-existent field by the value" do
+      person.high_score.should == 5
     end
   end
 end
