637432877b72abe8c3d266f110686b048201d32b
Dave Krupinski <dave@davekrupinski.com>
2011-03-03 14:58:39 -0800

    Don't switch to model_name for metadata inverse class name setup.  Only use for inverse_class_setter on polymorphic associations.  Metadata change affected ability to define relations at differing points in an inheritence tree

--- lib/mongoid/relations/macros.rb
+++ lib/mongoid/relations/macros.rb
@@ -262,7 +262,7 @@ module Mongoid # :nodoc:
             options.merge(
               :relation => relation,
               :extend => block,
-              :inverse_class_name => self.model_name,
+              :inverse_class_name => self.name,
               :name => name
             )
           )
