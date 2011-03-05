fde7aa32f3f22f11f0f40c4915cf5921e730c7b1
Durran Jordan <durran@gmail.com>
2011-03-05 15:59:39 +0100

    Stylistic refactoring around sharding and its specs to keep consistent with the target design

--- spec/unit/mongoid/sharding_spec.rb
+++ spec/unit/mongoid/sharding_spec.rb
@@ -4,53 +4,58 @@ describe Mongoid::Sharding do
 
   describe ".included" do
 
-    before do
-      @class = Class.new do
+    let(:klass) do
+      Class.new do
         include Mongoid::Sharding
       end
     end
 
     it "adds an shard_key_fields accessor" do
-      @class.should respond_to(:shard_key_fields)
+      klass.should respond_to(:shard_key_fields)
     end
 
     it "defaults shard_key_fields to an empty array" do
-      @class.shard_key_fields.should == []
+      klass.shard_key_fields.should == []
     end
-
   end
 
   describe ".shard_key" do
 
-    before do
-      @class = Class.new do
+    let(:klass) do
+      Class.new do
         include Mongoid::Sharding
       end
     end
 
-    it "specifies a shard key on the collection" do
-      @class.shard_key(:name)
-      @class.shard_key_fields.should == [:name]
+    before do
+      klass.shard_key(:name)
     end
 
+    it "specifies a shard key on the collection" do
+      klass.shard_key_fields.should == [:name]
+    end
   end
-  
-  describe ".shard_key_selector" do
-    
-    before do
-      @class = Class.new do
+
+  describe "#shard_key_selector" do
+
+    let(:klass) do
+      Class.new do
         include Mongoid::Sharding
         attr_accessor :name
       end
     end
-    
-    it "creates a hash of shard key field names mapped to values" do
-      @class.shard_key(:name)
-      jo = @class.new
-      jo.name = 'Jo'
-      jo.shard_key_selector.should == { 'name' => 'Jo' }
-    end
-    
-  end
 
+    let(:object) do
+      klass.new
+    end
+
+    before do
+      klass.shard_key(:name)
+      object.name = "Jo"
+    end
+
+    it "returns a hash of shard key names and values" do
+      object.shard_key_selector.should == { "name" => "Jo" }
+    end
+  end
 end
