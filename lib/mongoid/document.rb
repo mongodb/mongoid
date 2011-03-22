8be0df22c68bcc507b1c89b8f245efddcea58b90
Roman Shterenzon <roman.shterenzon@gmail.com>
2011-03-22 18:02:55 +0200

    Add Mongoid::Document#becomes
    Returns an instance of the specified klass with the attributes, errors
    and state of the current document. Needed for upcasting/downcasting.

--- lib/mongoid/document.rb
+++ lib/mongoid/document.rb
@@ -229,6 +229,29 @@ module Mongoid #:nodoc:
       end
     end
 
+    # Returns an instance of the specified class with the attributes
+    # and errors of the current document.
+    #
+    # @example Return a subclass document as a superclass instance.
+    #   manager.becomes(Person)
+    #
+    # @raise [ ArgumentError ] If the class doesn't include Mongoid::Document
+    #
+    # @param [ Class ] klass The class to become.
+    #
+    # @return [ Document ] An instance of the specified class.
+    def becomes(klass)
+      unless klass.include?(Mongoid::Document)
+        raise ArgumentError, 'A class which includes Mongoid::Document is expected'
+      end
+      became = klass.new
+      became.instance_variable_set('@attributes', @attributes)
+      became.instance_variable_set('@errors', @errors)
+      became.instance_variable_set('@new_record', new_record?)
+      became.instance_variable_set('@destroyed', destroyed?)
+      became
+    end
+
     module ClassMethods #:nodoc:
 
       # Performs class equality checking.
