a0f50e12ebc91e2f84ea964471a78900b4a23966
Kieran Pilkington <kieran776@gmail.com>
2011-04-14 06:39:15 +1200

    Cleaning up Range conversion implementation. Fixes #777

--- lib/mongoid/extensions/range/conversions.rb
+++ lib/mongoid/extensions/range/conversions.rb
@@ -2,22 +2,21 @@
 module Mongoid #:nodoc:
   module Extensions #:nodoc:
     module Range #:nodoc:
-      # This module converts set into mongoid related objects.
       module Conversions #:nodoc:
         extend ActiveSupport::Concern
 
+        def to_hash
+          { "min" => min, "max" => max }
+        end
+
         module ClassMethods #:nodoc:
+
           def get(value)
             value.nil? ? nil : ::Range.new(value["min"], value["max"])
           end
+
           def set(value)
             value.nil? ? nil : value.to_hash
-          end
-        end
-        
-        module InstanceMethods
-          def to_hash
-            {"min" => min, "max" => max}
           end
         end
       end
