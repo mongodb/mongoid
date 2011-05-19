304ce1da76e74d95e906cb6e2ab4e10c6e1f036d
Durran Jordan <durran@gmail.com>
2011-05-19 20:42:20 +0200

    Adding in rdoc for fields method on criteria

--- lib/mongoid/criterion/exclusion.rb
+++ lib/mongoid/criterion/exclusion.rb
@@ -24,6 +24,17 @@ module Mongoid #:nodoc:
         update_selector(attributes, "$ne")
       end
 
+      # Used when wanting to set the fields options directly using a hash
+      # instead of going through only or without.
+      #
+      # @example Set the limited fields.
+      #   criteria.fields(:field => 1)
+      #
+      # @param [ Hash ] attributes The field options.
+      #
+      # @return [ Criteria ] A newly cloned copy.
+      #
+      # @since 2.0.2
       def fields(attributes = nil)
         clone.tap { |crit| crit.options[:fields] = attributes || {} }
       end
