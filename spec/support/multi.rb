d5270bb474e7647e0ff79ca884b5a10f5b36f57c
Durran Jordan <durran@gmail.com>
2011-06-05 14:24:31 +0200

    Removing slave support from multi database specs

--- spec/support/multi.rb
+++ spec/support/multi.rb
@@ -6,11 +6,7 @@ module Support #:nodoc:
     def configured?
       begin
         master_uri = "mongodb://localhost:27020"
-        slave_one_uri = "mongodb://localhost:27021"
-        slave_two_uri = "mongodb://localhost:27022"
         Mongo::Connection.from_uri(master_uri)
-        Mongo::Connection.from_uri(slave_one_uri, :slave_ok => true)
-        Mongo::Connection.from_uri(slave_two_uri, :slave_ok => true)
         true
       rescue Mongo::ConnectionFailure => e
         false
@@ -20,15 +16,12 @@ module Support #:nodoc:
     def message
       %Q{
       ---------------------------------------------------------------------
-      The Mongoid configuration specs require a secondary master with 2
-      secondary slave databases to be running in order to properly be
-      tested. For multi-database configurations. Those specs are skipped
-      when those 3 databases are not running locally.
+      The Mongoid configuration specs require a secondary master in order
+      to properly be tested. For multi-database configurations.
+      Those specs are skipped when those 3 databases are not running locally.
 
       See the following configuration files for assistance:
         spec/config/mongod.alt.conf
-        spec/config/mongod.alt.slave.one.conf
-        spec/config/mongod.alt.slave.two.conf
       ---------------------------------------------------------------------
       }
     end
