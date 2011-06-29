482a95f265ebcafc4d2359b8b23943df123293e6
Bernerd Schaefer <bj.schaefer@gmail.com>
2011-06-29 17:21:36 +0200

    Delegate finder criteria methods

--- lib/mongoid/finders.rb
+++ lib/mongoid/finders.rb
@@ -7,13 +7,11 @@ module Mongoid #:nodoc:
 
     # Delegate to the criteria methods that are natural for creating a new
     # criteria.
-    [ :all_in, :any_in, :any_of, :asc, :ascending, :avg, :desc, :descending,
-      :excludes, :limit, :max, :min, :not_in, :only, :order_by,
-      :skip, :sum, :without, :where, :update, :update_all, :near ].each do |name|
-      define_method(name) do |*args|
-        criteria.send(name, *args)
-      end
-    end
+    critera_methods = [ :all_in, :any_in, :any_of, :asc, :ascending, :avg,
+                        :desc, :descending, :excludes, :limit, :max, :min,
+                        :not_in, :only, :order_by, :skip, :sum, :without,
+                        :where, :update, :update_all, :near ]
+    delegate *(critera_methods.dup << {:to => :criteria})
 
     # Find all documents that match the given conditions.
     #
