b54d5673b9307c14a0812887e481a9b12226335f
Durran Jordan <durran@gmail.com>
2011-07-15 19:04:28 +0200

    Ensure mongoid logger is default on replica set connections. Fixes #884.

--- lib/mongoid/config/replset_database.rb
+++ lib/mongoid/config/replset_database.rb
@@ -14,9 +14,13 @@ module Mongoid #:nodoc:
       #
       # @since 2.0.0.rc.5
       def configure
-        #yes, construction is weird but the driver wants "A list of host-port pairs ending with a hash containing any options"
-        #mongo likes symbols
-        options = self.inject({}) { |memo, (k, v)| memo[k.to_sym] = v; memo}
+        # yes, construction is weird but the driver wants 
+        # "A list of host-port pairs ending with a hash containing any options"
+        # mongo likes symbols
+        options = self.inject({ :logger => Mongoid::Logger.new }) do |memo, (k, v)|
+          memo[k.to_sym] = v
+          memo
+        end
         connection = Mongo::ReplSetConnection.new(*(hosts << options))
 
         if authenticating?
@@ -49,6 +53,25 @@ module Mongoid #:nodoc:
       # @since 2.0.2
       def method_missing(name, *args, &block)
         self[name.to_s]
+      end
+
+      # Get the options used in creating the database connection.
+      #
+      # @example Get the options.
+      #   db.options
+      #
+      # @param [ true, false ] slave Are the options for a slave db?
+      #
+      # @return [ Hash ] The hash of configuration options.
+      #
+      # @since 2.0.0.rc.1
+      def optional(slave = false)
+        ({
+          :pool_size => pool_size,
+          :logger => Mongoid::Logger.new,
+          :slave_ok => slave
+        }).merge(self).reject { |k,v| PRIVATE_OPTIONS.include? k }.
+          inject({}) { |memo, (k, v)| memo[k.to_sym] = v; memo} # mongo likes symbols
       end
 
       # Create the new db configuration class.
