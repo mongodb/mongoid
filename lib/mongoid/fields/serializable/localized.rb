e29995a386f4bd2fa327c282e19032e3a0d5d4a0
Durran Jordan and Koichi Hirano <dev+durran+koichi@1000jobboersen.de>
2011-09-14 13:45:01 +0200

    Adding in criteria conversion of localized fields.

--- lib/mongoid/fields/serializable/localized.rb
+++ lib/mongoid/fields/serializable/localized.rb
@@ -7,18 +7,33 @@ module Mongoid #:nodoc:
       class Localized
         include Serializable
 
+        # Deserialize the object based on the current locale. Will look in the
+        # hash for the current locale.
+        #
+        # @example Get the deserialized value.
+        #   field.deserialize({ "en" => "testing" })
+        #
+        # @param [ Hash ] object The hash of translations.
+        #
+        # @return [ String ] The value for the current locale.
+        #
+        # @since 2.3.0
         def deserialize(object)
-          object[locale]
+          object[::I18n.locale.to_s]
         end
 
+        # Convert the provided string into a hash for the locale.
+        #
+        # @example Serialize the value.
+        #   field.serialize("testing")
+        #
+        # @param [ String ] object The string to convert.
+        #
+        # @return [ Hash ] The locale with string translation.
+        #
+        # @since 2.3.0
         def serialize(object)
-          { locale => object.try(:to_s) }
-        end
-
-        private
-
-        def locale
-          (::I18n.locale || I18n.default_locale).to_s
+          { ::I18n.locale.to_s => object.try(:to_s) }
         end
       end
     end
--- spec/unit/mongoid/attributes_spec.rb
+++ spec/unit/mongoid/attributes_spec.rb
@@ -883,7 +883,7 @@ describe Mongoid::Attributes do
 
       before do
         person.stubs(:fields).returns(
-          { "age" => Mongoid::Fields::Serializable::Integer.new(:age) }
+          { "age" => Mongoid::Fields::Serializable::Integer.instantiate(:age) }
         )
       end
 
