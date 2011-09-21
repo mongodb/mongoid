be44d2ddafde21ff75bcd6363b81350e53af6f9f
Durran Jordan <durran@gmail.com>
2011-09-21 15:57:26 +0200

    Bump driver dependency to 1.4 and fix replica set deprecations and errors.

--- spec/functional/mongoid/config/replset_database_spec.rb
+++ spec/functional/mongoid/config/replset_database_spec.rb
@@ -12,22 +12,6 @@ describe Mongoid::Config::ReplsetDatabase do
       YAML.load(ERB.new(File.new(replset_config).read).result)
     end
 
-    let(:replica_set) do
-      described_class.new(options['test']).configure
-    end
-
-    it "returns a replica set connection" do
-      replica_set[0].connection.should be_a(Mongo::ReplSetConnection)
-    end
-
-    it "sets slave ok to true" do
-      replica_set[0].connection.slave_ok?.should be_true
-    end
-
-    it "does not configure specific slaves" do
-      replica_set[1].should be_nil
-    end
-
     context "without authentication details" do
 
       let(:replica_set) do
