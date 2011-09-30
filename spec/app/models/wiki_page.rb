1d0d267d34376d200e7b4f9f5c63a30b41458b14
Durran Jordan <durran@gmail.com>
2011-09-30 11:39:46 +0200

    Exclude _id and updated_at from versioned attributes.
    - Fixes #1288.
    - Fixes #1289.

--- spec/app/models/wiki_page.rb
+++ spec/app/models/wiki_page.rb
@@ -1,7 +1,11 @@
 class WikiPage
   include Mongoid::Document
+  include Mongoid::Timestamps
   include Mongoid::Versioning
+
   field :title, :type => String
   field :transient_property, :type => String, :versioned => false
   max_versions 5
+
+  has_many :comments, :dependent => :delete
 end
