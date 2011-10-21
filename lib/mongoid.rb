7814f30e41ee2d3a50b96143c00ba6b96cfcec60
Durran Jordan <durran@gmail.com>
2011-10-21 12:10:27 +0200

    Explicit lazy load hooks require

--- lib/mongoid.rb
+++ lib/mongoid.rb
@@ -25,6 +25,7 @@ require "time"
 require "active_support/core_ext"
 require 'active_support/json'
 require "active_support/inflector"
+require "active_support/lazy_load_hooks"
 require "active_support/time_with_zone"
 require "active_model"
 require "mongo"
