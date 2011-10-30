e1c8f1f9b79f34489003f532c0322b1d8fad2808
Seivan Heidari <seivan@kth.se>
2011-10-31 02:32:52 +0800

    cache_key method to be used inside fragment caches to auto-expire old views when memcached pops old views with updated_at
    Print out the cache key.
    Will append different values on the plural model name
    If new_record?     - will append /new
    If not             - will append /id-updated_at.to_s(:number)
    Without updated_at - will append /id
    
    This is usually called insode a cache() block
    @example Returns the cache key
    @return [String] the string with or without updated_at
    
    Added some examples and explanation

--- lib/mongoid/timestamps/updated.rb
+++ lib/mongoid/timestamps/updated.rb
@@ -21,7 +21,17 @@ module Mongoid #:nodoc:
       def set_updated_at
         self.updated_at = Time.now.utc unless self.updated_at_changed?
       end
+
+      # Print out the cache key.
+      # Will append different values on the plural model name
+      # If new_record?     - will append /new
+      # If not             - will append /id-updated_at.to_s(:number)
+      # Without updated_at - will append /id
       
+      # This is usually called insode a cache() block
+      #
+      # @example Returns the cache key
+      # @return [String] the string with or without updated_at
       def cache_key
         case
         when new_record?
