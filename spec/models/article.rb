192bb67ba945af28d4f3792540d40e8d140d48c1
Durran Jordan <durran@gmail.com>
2011-10-31 10:53:32 +0100

    Prep 2.2.4

--- spec/models/article.rb
+++ spec/models/article.rb
@@ -2,4 +2,5 @@ class Article
   include Mongoid::Document
   include Mongoid::Timestamps
   has_and_belongs_to_many :tags
+  has_and_belongs_to_many :preferences, :inverse_of => nil, :validate => false
 end
