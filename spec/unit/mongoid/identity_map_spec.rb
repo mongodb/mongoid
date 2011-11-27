6095f907ee49842124c5d9ba77e5d83f123b38d1
Frank Wöckener <fw@cubiflex.com>
2011-11-27 22:22:31 +0100

    fix code indentation

--- spec/unit/mongoid/identity_map_spec.rb
+++ spec/unit/mongoid/identity_map_spec.rb
@@ -51,77 +51,77 @@ describe Mongoid::IdentityMap do
   describe "#get" do
 
     context "normal model" do
-    let!(:person) do
-      Person.new
+      let!(:person) do
+        Person.new
+      end
+
+      context "when getting by id" do
+
+        context "when the document exists in the identity map" do
+
+          before do
+            identity_map.set(person)
+          end
+
+          let(:get) do
+            identity_map.get(Person, person.id)
+          end
+
+          it "returns the matching person" do
+            get.should eq(person)
+          end
+        end
+
+        context "when the person does not exist in the map" do
+
+          let(:get) do
+            identity_map.get(Person, person.id)
+          end
+
+          it "returns nil" do
+            get.should be_nil
+          end
+        end
+      end
+
+      context "when getting by selector" do
+
+        let!(:post_one) do
+          Post.new(:person => person)
+        end
+
+        let!(:post_two) do
+          Post.new(:person => person)
+        end
+
+        context "when there are documents in the map" do
+
+          before do
+            identity_map.set_many(post_one, :person_id => person.id)
+            identity_map.set_many(post_two, :person_id => person.id)
+          end
+
+          let(:documents) do
+            identity_map.get(Post, :person_id => person.id)
+          end
+
+          it "returns the matching documents" do
+            documents.should eq([ post_one, post_two ])
+          end
+        end
+
+        context "when there are no documents in the map" do
+
+          let(:documents) do
+            identity_map.get(Post, :person_id => person.id)
+          end
+
+          it "returns nil" do
+            documents.should be_nil
+          end
+        end
+      end
     end
-
-    context "when getting by id" do
-
-      context "when the document exists in the identity map" do
-
-        before do
-          identity_map.set(person)
-        end
-
-        let(:get) do
-          identity_map.get(Person, person.id)
-        end
-
-        it "returns the matching person" do
-          get.should eq(person)
-        end
-      end
-
-      context "when the person does not exist in the map" do
-
-        let(:get) do
-          identity_map.get(Person, person.id)
-        end
-
-        it "returns nil" do
-          get.should be_nil
-        end
-      end
-    end
-
-    context "when getting by selector" do
-
-      let!(:post_one) do
-        Post.new(:person => person)
-      end
-
-      let!(:post_two) do
-        Post.new(:person => person)
-      end
-
-      context "when there are documents in the map" do
-
-        before do
-          identity_map.set_many(post_one, :person_id => person.id)
-          identity_map.set_many(post_two, :person_id => person.id)
-        end
-
-        let(:documents) do
-          identity_map.get(Post, :person_id => person.id)
-        end
-
-        it "returns the matching documents" do
-          documents.should eq([ post_one, post_two ])
-        end
-      end
-
-      context "when there are no documents in the map" do
-
-        let(:documents) do
-          identity_map.get(Post, :person_id => person.id)
-        end
-
-        it "returns nil" do
-          documents.should be_nil
-        end
-      end
-    end
-  end
 
     context "inherited class" do
 
