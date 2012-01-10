1c6575f0fa5f082ba5abdbc374f59344edb63ca9
bcassell <bcassell@umich.edu>
2012-01-09 21:35:22 -0500

    allow sorting of hash

--- lib/mongoid/fields/internal/hash.rb
+++ lib/mongoid/fields/internal/hash.rb
@@ -5,6 +5,20 @@ module Mongoid #:nodoc:
       # Defines the behaviour for hash fields.
       class Hash
         include Serializable
+        
+        def selection(object)
+          serialize(object)
+        end
+
+        def serialize(object)
+          if options[:sorted] == true && object.is_a?(::Hash)
+            sorted_hash = {}
+            object.keys.sort.each {|x| sorted_hash[x] = serialize(object[x])}
+            sorted_hash
+          else
+            object
+          end
+        end
       end
     end
   end
