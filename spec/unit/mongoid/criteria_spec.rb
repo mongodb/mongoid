0a54facb236af9d87fd134bd5ee084047591c998
Durran Jordan <durran@gmail.com>
2012-01-22 08:43:17 +0100

    Add relation proxy methods to prohibited fields. Fixes #1617

--- spec/unit/mongoid/criteria_spec.rb
+++ spec/unit/mongoid/criteria_spec.rb
@@ -811,7 +811,7 @@ describe Mongoid::Criteria do
     context "chaining more than one scope" do
 
       let(:criteria) do
-        Person.accepted.old.knight
+        Person.accepted.knight
       end
 
       let(:chained) do
@@ -820,7 +820,7 @@ describe Mongoid::Criteria do
 
       it "returns the final merged criteria" do
         criteria.selector.should ==
-          { :title => "Sir", :terms => true, :age => { "$gt" => 50 } }
+          { :title => "Sir", :terms => true }
       end
 
       it "always returns a new criteria" do
