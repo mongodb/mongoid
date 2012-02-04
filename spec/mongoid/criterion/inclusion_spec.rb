1f3243e170d8e19b259a6c8e1aa93f31a33a1c62
Hans Hasselberg <me@hans.io>
2012-02-04 23:51:32 +0100

    Remove failing test.
    This example just don't work any more since DateTime stores also
    millisecond, because milliseconds aren't part of DateTime#to_s.
    You could rewrite it to:
    
      Person.where(:lunch_time.gt => time).should eq([ person ])
    
    but that doesn't fit very well imho.

--- spec/mongoid/criterion/inclusion_spec.rb
+++ spec/mongoid/criterion/inclusion_spec.rb
@@ -1269,10 +1269,6 @@ describe Mongoid::Criterion::Inclusion do
         Person.where(:age => "33").should eq([ person ])
       end
 
-      it "typecasts datetimes" do
-        Person.where(:lunch_time => lunch_time.to_s).should eq([ person ])
-      end
-
       it "typecasts dates" do
         Person.where({:dob => dob.to_s}).should eq([ person ])
       end
