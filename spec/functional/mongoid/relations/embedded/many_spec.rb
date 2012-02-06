5b06944459130829292c02db682ca7313a63d9ba
Durran Jordan <durran@gmail.com>
2012-02-06 10:35:43 +0100

    Clear delayed atomic sets before substitution. Fixes #1659

--- spec/functional/mongoid/relations/embedded/many_spec.rb
+++ spec/functional/mongoid/relations/embedded/many_spec.rb
@@ -316,6 +316,7 @@ describe Mongoid::Relations::Embedded::Many do
         end
 
         context "when setting the relation multiple times" do
+
           let(:address_two) do
             Address.new(:street => "kudamm")
           end
