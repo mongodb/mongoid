6824e322bf70177ceba5ef43bc80bb94605060d5
Durran Jordan <durran@gmail.com>
2012-02-11 14:30:09 +0100

    Check for inverse metadata before attempting to get the field. Fixes #1704.

--- lib/mongoid/relations/referenced/many.rb
+++ lib/mongoid/relations/referenced/many.rb
@@ -537,16 +537,16 @@ module Mongoid #:nodoc:
           # @since 2.1.0
           def criteria(metadata, object, type = nil)
             crit = metadata.klass.where(metadata.foreign_key => object)
-            
+
             if metadata.polymorphic?
               crit = crit.where(metadata.type => type.name)
             end
-            
+
             inverse_metadata = metadata.inverse_metadata(metadata.klass)
-            if inverse_metadata.inverse_of_field
+            if inverse_metadata && inverse_metadata.inverse_of_field
               crit = crit.any_in(inverse_metadata.inverse_of_field => [metadata.name, nil])
             end
-            
+
             crit
           end
 
