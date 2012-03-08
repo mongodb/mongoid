f5de0d5506772d22c0a8a3c032b9c34194a76e05
Chad Jablonski <chad@dinocore.net>
2012-03-08 09:08:45 -0800

    Return documents from push on many to many

--- lib/mongoid/relations/referenced/many_to_many.rb
+++ lib/mongoid/relations/referenced/many_to_many.rb
@@ -25,22 +25,24 @@ module Mongoid # :nodoc:
         #
         # @since 2.0.0.beta.1
         def <<(*args)
-          batched do
-            [].tap do |ids|
-              args.flatten.each do |doc|
-                next unless doc
-                append(doc)
+          tap do
+            batched do
+              [].tap do |ids|
+                args.flatten.each do |doc|
+                  next unless doc
+                  append(doc)
+                  if persistable? || _creating?
+                    ids.push(doc.id)
+                    doc.save
+                  else
+                    base.send(metadata.foreign_key).push(doc.id)
+                    base.synced[metadata.foreign_key] = false
+                  end
+                end
                 if persistable? || _creating?
-                  ids.push(doc.id)
-                  doc.save
-                else
-                  base.send(metadata.foreign_key).push(doc.id)
+                  base.push_all(metadata.foreign_key, ids)
                   base.synced[metadata.foreign_key] = false
                 end
-              end
-              if persistable? || _creating?
-                base.push_all(metadata.foreign_key, ids)
-                base.synced[metadata.foreign_key] = false
               end
             end
           end
