cde7b788425e80f1245c4a327911811ce14bc808
Durran Jordan <durran@gmail.com>
2012-03-16 16:44:41 +0100

    Revert "fix issue introduce in 38c4436e63c3449a68775839867bffa6faabe15e . Set the updated_at in create callback"
    This reverts commit 53b274505e1f89130792203e975f1a4461b61579.

--- lib/mongoid/timestamps/updated.rb
+++ lib/mongoid/timestamps/updated.rb
@@ -9,7 +9,6 @@ module Mongoid #:nodoc:
       included do
         field :updated_at, type: Time
         set_callback :save, :before, :set_updated_at, if: :able_to_set_updated_at?
-        set_callback :create, :before, :create_updated_at, if: :able_to_set_updated_at?
       end
 
       # Update the updated_at field on the Document to the current time.
@@ -18,13 +17,11 @@ module Mongoid #:nodoc:
       # @example Set the updated at time.
       #   person.set_updated_at
       def set_updated_at
-        self.updated_at = Time.now.utc unless updated_at_changed?
-      end
-
-      # Set the updated_at field it's Time.now.utc or the created_at if define
-      #
-      def create_updated_at
-        self.updated_at = respond_to?(:created_at) ? created_at : Time.now.utc
+        unless updated_at
+          self.updated_at = respond_to?(:created_at) ? created_at : Time.now.utc
+        else
+          self.updated_at = Time.now.utc unless updated_at_changed?
+        end
       end
 
       # Is the updated timestamp able to be set?
