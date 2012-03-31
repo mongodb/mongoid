d56e113b93e2d83c5c475c7e499d7abe90a1a8fc
ara.t.howard <ara.t.howard@gmail.com>
2012-03-31 13:14:44 -0600

    recurse more naturally with a set leveraging Document#identity

--- lib/mongoid.rb
+++ lib/mongoid.rb
@@ -22,6 +22,7 @@
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 require "delegate"
 require "time"
+require "set"
 require "active_support/core_ext"
 require 'active_support/json'
 require "active_support/inflector"
