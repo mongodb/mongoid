69a3fb5ca06a3d753f2ad2f469ff7e8e0cbc3ad3
Durran Jordan <durran@gmail.com>
2012-04-19 10:36:51 +0200

    Add mongoization to float

---
+++ lib/mongoid/extensions/float.rb
@@ -0,0 +1,27 @@
+# encoding: utf-8
+module Mongoid
+  module Extensions
+    module Float
+
+      module ClassMethods
+
+        # Turn the object from the ruby type we deal with to a Mongo friendly
+        # type.
+        #
+        # @example Mongoize the object.
+        #   Float.mongoize("123.11")
+        #
+        # @param [ Object ] object The object to mongoize.
+        #
+        # @return [ String ] The object mongoized.
+        #
+        # @since 3.0.0
+        def mongoize(object)
+          object ? (__numeric__(object) rescue 0.0) : object
+        end
+      end
+    end
+  end
+end
+
+::Float.__send__(:extend, Mongoid::Extensions::Float::ClassMethods)
