0f96fd0b2dd1ca53d902fcc55bc03a028973055f
Durran Jordan <durran@gmail.com>
2012-04-27 15:37:36 +0200

    Flush out object id specs

--- lib/mongoid/extensions/object_id.rb
+++ lib/mongoid/extensions/object_id.rb
@@ -3,16 +3,44 @@ module Mongoid
   module Extensions
     module ObjectId
 
+      # Evolve the object id.
+      #
+      # @example Evolve the object id.
+      #   object_id.__evolve_object_id__
+      #
+      # @return [ Moped::BSON::ObjectId ] self.
+      #
+      # @since 3.0.0
       def __evolve_object_id__
         self
       end
 
       module ClassMethods
 
+        # Evolve the object into a mongo-friendly value to query with.
+        #
+        # @example Evolve the object.
+        #   ObjectId.evolve(id)
+        #
+        # @param [ Object ] object The object to evolve.
+        #
+        # @return [ Moped::BSON::ObjectId ] The object id.
+        #
+        # @since 3.0.0
         def evolve(object)
           object.__evolve_object_id__
         end
 
+        # Convert the object into a mongo-friendly value to store.
+        #
+        # @example Convert the object.
+        #   ObjectId.mongoize(id)
+        #
+        # @param [ Object ] object The object to convert.
+        #
+        # @return [ Moped::BSON::ObjectId ] The object id.
+        #
+        # @since 3.0.0
         def mongoize(object)
           evolve(object)
         end
--- spec/mongoid/extensions/object_id_spec.rb
+++ spec/mongoid/extensions/object_id_spec.rb
@@ -10,6 +10,321 @@ describe Mongoid::Extensions::ObjectId do
     "21-jump-street"
   end
 
+  describe "#__evolve_object_id__" do
+
+    it "returns self" do
+      object_id.__evolve_object_id__.should eq(object_id)
+    end
+
+    it "returns the same instance" do
+      object_id.__evolve_object_id__.should equal(object_id)
+    end
+  end
+
+  describe ".evolve" do
+
+    context "when the class is using object ids" do
+
+      context "when provided a single object id" do
+
+        let(:evolved) do
+          BSON::ObjectId.evolve(object_id)
+        end
+
+        it "returns the object id" do
+          evolved.should eq(object_id)
+        end
+      end
+
+      context "when provided an array of object ids" do
+
+        let(:other_id) do
+          BSON::ObjectId.new
+        end
+
+        let(:evolved) do
+          BSON::ObjectId.evolve([ object_id, other_id ])
+        end
+
+        it "returns the array of object ids" do
+          evolved.should eq([ object_id, other_id ])
+        end
+      end
+
+      context "when provided a single string" do
+
+        context "when the string is a valid object id" do
+
+          let(:evolved) do
+            BSON::ObjectId.evolve(object_id.to_s)
+          end
+
+          it "evolves to an object id" do
+            evolved.should eq(object_id)
+          end
+        end
+
+        context "when the string is not a valid object id" do
+
+          it "returns the key" do
+            BSON::ObjectId.evolve(composite_key).should eq(
+              composite_key
+            )
+          end
+        end
+
+        context "when the string is empty" do
+
+          let(:evolved) do
+            BSON::ObjectId.evolve("")
+          end
+
+          it "evolves to nil" do
+            evolved.should be_nil
+          end
+        end
+      end
+
+      context "when provided an array" do
+
+        context "when array key of nils" do
+
+          let(:evolved) do
+            BSON::ObjectId.evolve([ nil, nil ])
+          end
+
+          it "returns an empty array" do
+            evolved.should be_empty
+          end
+        end
+
+        context "when the array key is empty strings" do
+
+          let(:evolved) do
+            BSON::ObjectId.evolve([ "", "" ])
+          end
+
+          it "returns an empty array" do
+            evolved.should be_empty
+          end
+        end
+
+        context "when the array key is full of strings" do
+
+          context "when the strings are valid object ids" do
+
+            let(:other_id) do
+              BSON::ObjectId.new
+            end
+
+            let(:evolved) do
+              BSON::ObjectId.evolve([ object_id.to_s, other_id.to_s ])
+            end
+
+            it "evolves to an array of object ids" do
+              evolved.should eq([ object_id, other_id ])
+            end
+          end
+
+          context "when the strings are not valid object ids" do
+
+            let(:other_key) do
+              "hawaii-five-o"
+            end
+
+            let(:evolved) do
+              BSON::ObjectId.evolve([ composite_key, other_key ])
+            end
+
+            it "returns the key" do
+              BSON::ObjectId.evolve(composite_key).should eq(
+                composite_key
+              )
+            end
+          end
+        end
+      end
+
+      context "when provided a hash" do
+
+        context "when the hash key is _id" do
+
+          context "when the value is an object id" do
+
+            let(:hash) do
+              { _id: object_id }
+            end
+
+            let(:evolved) do
+              BSON::ObjectId.evolve(hash)
+            end
+
+            it "returns the hash" do
+              evolved.should eq(hash)
+            end
+          end
+
+          context "when the value is an array of object ids" do
+
+            let(:other_id) do
+              BSON::ObjectId.new
+            end
+
+            let(:hash) do
+              { _id: [ object_id, other_id ] }
+            end
+
+            let(:evolved) do
+              BSON::ObjectId.evolve(hash)
+            end
+
+            it "returns the hash" do
+              evolved.should eq(hash)
+            end
+          end
+
+          context "when the value is a string" do
+
+            let(:hash) do
+              { _id: object_id.to_s }
+            end
+
+            let(:evolved) do
+              BSON::ObjectId.evolve(hash)
+            end
+
+            it "returns the hash with evolved value" do
+              evolved.should eq({ _id: object_id })
+            end
+          end
+
+          context "when the value is an array of strings" do
+
+            let(:other_id) do
+              BSON::ObjectId.new
+            end
+
+            let(:hash) do
+              { _id: [ object_id.to_s, other_id.to_s ] }
+            end
+
+            let(:evolved) do
+              BSON::ObjectId.evolve(hash)
+            end
+
+            it "returns the hash with evolved values" do
+              evolved.should eq({ _id: [ object_id, other_id ] })
+            end
+          end
+        end
+
+        context "when the hash key is id" do
+
+          context "when the value is an object id" do
+
+            let(:hash) do
+              { id: object_id }
+            end
+
+            let(:evolved) do
+              BSON::ObjectId.evolve(hash)
+            end
+
+            it "returns the hash" do
+              evolved.should eq(hash)
+            end
+          end
+
+          context "when the value is an array of object ids" do
+
+            let(:other_id) do
+              BSON::ObjectId.new
+            end
+
+            let(:hash) do
+              { id: [ object_id, other_id ] }
+            end
+
+            let(:evolved) do
+              BSON::ObjectId.evolve(hash)
+            end
+
+            it "returns the hash" do
+              evolved.should eq(hash)
+            end
+          end
+
+          context "when the value is a string" do
+
+            let(:hash) do
+              { id: object_id.to_s }
+            end
+
+            let(:evolved) do
+              BSON::ObjectId.evolve(hash)
+            end
+
+            it "returns the hash with evolved value" do
+              evolved.should eq({ id: object_id })
+            end
+          end
+
+          context "when the value is an array of strings" do
+
+            let(:other_id) do
+              BSON::ObjectId.new
+            end
+
+            let(:hash) do
+              { id: [ object_id.to_s, other_id.to_s ] }
+            end
+
+            let(:evolved) do
+              BSON::ObjectId.evolve(hash)
+            end
+
+            it "returns the hash with evolved values" do
+              evolved.should eq({ id: [ object_id, other_id ] })
+            end
+          end
+        end
+
+        context "when the hash key is not an id" do
+
+          context "when the value is a string" do
+
+            let(:hash) do
+              { key: composite_key }
+            end
+
+            let(:evolved) do
+              BSON::ObjectId.evolve(hash)
+            end
+
+            it "returns the hash" do
+              evolved.should eq(hash)
+            end
+          end
+
+          context "when the value is an array of strings" do
+
+            let(:hash) do
+              { key: [ composite_key ] }
+            end
+
+            let(:evolved) do
+              BSON::ObjectId.evolve(hash)
+            end
+
+            it "returns the hash" do
+              evolved.should eq(hash)
+            end
+          end
+        end
+      end
+    end
+  end
+
   describe ".mongoize" do
 
     context "when the class is using object ids" do
