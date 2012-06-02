cc11134543ebe391207720416fbca65589157d20
Durran Jordan <durran@gmail.com>
2012-06-02 11:43:23 +0200

    Refactor intialize_dup for jruby

--- lib/mongoid/copyable.rb
+++ lib/mongoid/copyable.rb
@@ -23,13 +23,44 @@ module Mongoid #:nodoc:
     # @example Clone the document.
     #   document.clone
     #
+    # @param [ Document ] other The document getting cloned.
+    #
+    # @return [ Document ] The new document.
+    def initialize_copy(other)
+      __copy__(other)
+    end
+
+    # Clone or dup the current +Document+. This will return all attributes with
+    # the exception of the document's id and versions, and will reset all the
+    # instance variables.
+    #
+    # This clone also includes embedded documents.
+    #
     # @example Dup the document.
     #   document.dup
     #
     # @param [ Document ] other The document getting cloned.
     #
     # @return [ Document ] The new document.
-    def initialize_copy(other)
+    def initialize_dup(other)
+      __copy__(other)
+    end
+
+    private
+
+    # Handle the copy of the object.
+    #
+    # @api private
+    #
+    # @example Copy the object.
+    #   document.__copy__(other)
+    #
+    # @param [ Document ] other The other document.
+    #
+    # @return [ Document ] self.
+    #
+    # @since 3.0.0
+    def __copy__(other)
       other.as_document
       instance_variables.each { |name| remove_instance_variable(name) }
       COPYABLES.each do |name|
