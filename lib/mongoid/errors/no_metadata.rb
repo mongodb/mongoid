1a0edaddb0ca9eedb1e07bb31a4b37dbac6559e0
Durran Jordan <durran@gmail.com>
2012-06-30 20:17:01 +0200

    Update rdoc on NoMetadata error

--- lib/mongoid/errors/no_metadata.rb
+++ lib/mongoid/errors/no_metadata.rb
@@ -2,8 +2,17 @@
 module Mongoid
   module Errors
 
+    # Used when trying to persist data when metadata has not been set.
     class NoMetadata < MongoidError
 
+      # Create the new error.
+      #
+      # @example Create the error.
+      #   NoMetadata.new(Address)
+      #
+      # @param [ Class ] klass The document class.
+      #
+      # @since 3.0.0
       def initialize(klass)
         super(compose_message("no_metadata", { klass: klass }))
       end
