b121bf0049d8e288fddd243c1325c1ad71b03d24
Chris Winslett <chris@mongohq.com>
2012-08-31 20:49:31 -0500

    Added Rails logger support for version checking

--- lib/support/ruby_version.rb
+++ lib/support/ruby_version.rb
@@ -8,5 +8,10 @@ if RUBY_VERSION =~ /([\d]+)\.([\d]+)\.([\d]+)/ && $1.to_i <= 1 && $2.to_i <= 9 &
   puts "\n\n\nRuby Version Error: Mongoid 3 requires Ruby 1.9.3+; you are currently using #{RUBY_VERSION}."
   puts "                    Please check your environments documentation for upgrading to Ruby 1.9.3\n\n\n"
 
+  if defined?(Rails)
+    Rails.logger.info "\n\n\nRuby Version Error: Mongoid 3 requires Ruby 1.9.3+; you are currently using #{RUBY_VERSION}."
+    Rails.logger.info "                    Please check your environments documentation for upgrading to Ruby 1.9.3\n\n\n"
+  end
+
   raise Mongoid::RubyVersionError
 end
