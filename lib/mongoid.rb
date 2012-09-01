b844b8d07085ad9d7cee80fe1336413df3228f3c
Chris Winslett <chris@mongohq.com>
2012-08-31 20:45:39 -0500

    Moved to a support directory instead of the mail mongoid directory

--- lib/mongoid.rb
+++ lib/mongoid.rb
@@ -21,7 +21,7 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-require 'mongoid/ruby_version_requirement'
+require 'support/ruby_version'
 
 require "delegate"
 require "time"
