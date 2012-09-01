754c113d0efdadff1125e90004d701271fe8d1d9
Chris Winslett <chris@mongohq.com>
2012-08-31 21:12:38 -0500

    Added version checker for JRuby

--- lib/support/ruby_version.rb
+++ lib/support/ruby_version.rb
@@ -1,17 +1,29 @@
 # Go ahead and fail if not using Ruby 1.9.3, no since in letting people 
 # squarm for answers
-if RUBY_VERSION =~ /([\d]+)\.([\d]+)\.([\d]+)/ && $1.to_i <= 1 && $2.to_i <= 9 && $3.to_i <= 2
-  module Mongoid
-    class RubyVersionError < RuntimeError; end
-  end
+RUBY_VERSION =~ /([\d]+)\.([\d]+)\.([\d]+)/
 
-  puts "\n\n\nRuby Version Error: Mongoid 3 requires Ruby 1.9.3+; you are currently using #{RUBY_VERSION}."
-  puts "                    Please check your environments documentation for upgrading to Ruby 1.9.3\n\n\n"
+major, minor, revision = $1.to_i, $2.to_i, $3.to_i
+
+def raise_version_error(messages)
+
+  messages.each { |m| puts m}
 
   if defined?(Rails)
-    Rails.logger.info "\n\n\nRuby Version Error: Mongoid 3 requires Ruby 1.9.3+; you are currently using #{RUBY_VERSION}."
-    Rails.logger.info "                    Please check your environments documentation for upgrading to Ruby 1.9.3\n\n\n"
+    messages.each { |m| Rails.logger.info m }
   end
 
-  raise Mongoid::RubyVersionError
+  raise
+end
+
+if defined?(JRUBY_VERSION) && major <= 1 && minor <= 9 # JRUB
+  messages = []
+  messages << "\n\n\nRuby Version Error: Mongoid 3 on JRuby requires Ruby 1.9.2+ compatability; start jruby with the --1.9 argument"
+
+  raise_version_error(messages)
+elsif major <= 1 && minor <= 9 && revision <= 2 # MRI
+  messages = []
+  messages << "\n\n\nRuby Version Error: Mongoid 3 requires Ruby 1.9.3+; you are currently using #{RUBY_VERSION}."
+  messages << "                    Please check your environments documentation for upgrading to Ruby 1.9.3\n\n\n"
+
+  raise_version_error(messages)
 end
