5ac072171985c169897dae14dbb64814e4434957
Durran Jordan <durran@gmail.com>
2012-10-28 12:39:51 +0100

    Set empty arrays in identity map properly on 1-n.
    In the case of mixed returns from eager loading of parents with children
    and parents with no children, all the keys should be initialized as
    empty arrays first to avoid double db hits for the parents that had no
    documensts. (Fix provided by Douwe Maan)
    
    [ close #2507 ]

--- lib/mongoid/relations/proxy.rb
+++ lib/mongoid/relations/proxy.rb
@@ -177,15 +177,12 @@ module Mongoid
         def eager_load_ids(metadata, ids)
           klass, foreign_key = metadata.klass, metadata.foreign_key
           eager_loaded = klass.any_in(foreign_key => ids).entries
-          unless eager_loaded.empty?
-            eager_loaded.each do |doc|
-              base_id = doc.__send__(foreign_key)
-              yield(doc, { foreign_key => base_id })
-            end
-          else
-            ids.each do |id|
-              IdentityMap.clear_many(klass, { foreign_key => id })
-            end
+          ids.each do |id|
+            IdentityMap.clear_many(klass, { foreign_key => id })
+          end
+          eager_loaded.each do |doc|
+            base_id = doc.__send__(foreign_key)
+            yield(doc, { foreign_key => base_id })
           end
         end
       end
