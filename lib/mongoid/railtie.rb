a536a5aab37027c4189829c78527035ed2da5f0c
Durran Jordan <durran@gmail.com>
2012-11-04 16:23:36 +0100

    Bring back old preload initializer

--- lib/mongoid/railtie.rb
+++ lib/mongoid/railtie.rb
@@ -132,13 +132,22 @@ module Rails
         end
       end
 
-      # Passenger provides hooks in smart spawning to reset connections. We
-      # will handle this automatically here.
-      initializer "handle passenger smart spawning" do
-        if defined?(PhusionPassenger)
-          PhusionPassenger.on_event(:starting_worker_process) do |forked|
-            p "hi"
-            ::Mongoid.default_session.disconnect if forked
+      initializer "reconnect to master if application is preloaded" do
+        config.after_initialize do
+          # Unicorn clears the START_CTX when a worker is forked, so if we have
+          # data in START_CTX then we know we're being preloaded. Unicorn does
+          # not provide application-level hooks for executing code after the
+          # process has forked, so we reconnect lazily.
+          if defined?(Unicorn) && !Unicorn::HttpServer::START_CTX.empty?
+            ::Mongoid.default_session.disconnect
+          end
+
+          # Passenger provides the :starting_worker_process event for executing
+          # code after it has forked, so we use that and reconnect immediately.
+          if defined?(PhusionPassenger)
+            PhusionPassenger.on_event(:starting_worker_process) do |forked|
+              ::Mongoid.default_session.disconnect if forked
+            end
           end
         end
       end
