b87b3c9ea69b34c1ca6faca27d4c08468902d8a0
Durran Jordan <durran@gmail.com>
2012-12-25 13:40:04 -0500

    Fixing the documents returned by embedded unscoped
    This now properly filters out documents that were marked for
    destruction.
    
    [ fix #2554 ]

--- spec/app/models/word.rb
+++ spec/app/models/word.rb
@@ -7,4 +7,6 @@ class Word
   embeds_many :definitions, validate: false
   embeds_many :word_origins, validate: false
   embeds_one :pronunciation, validate: false
+
+  accepts_nested_attributes_for :definitions, allow_destroy: true
 end
