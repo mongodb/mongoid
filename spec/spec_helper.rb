edbd92b83260c9d9e4566fe7a0b99c4340c0de73
Timo Schilling <timo@schilling.io>
2013-05-17 22:14:45 +0200

    add missing rspec config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -79,6 +79,7 @@ RSpec.configure do |config|
   end
 
   config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.run_all_when_everything_filtered = true
   config.filter_run :focus
   config.filter_run_excluding :skip
 
