55c99032fcde9f1e41cbbd0353e902206dbddc16
Johnny Shields <johnny.shields@gmail.com>
2013-07-29 23:46:46 +0900

    Fix combo of Uniqueness Validator + Aliased + Localized

--- spec/app/models/definition.rb
+++ spec/app/models/definition.rb
@@ -3,5 +3,6 @@ class Definition
   field :description, type: String
   field :p, as: :part, type: String
   field :regular, type: Boolean
+  field :syn, as: :synonyms, localize: true, type: String
   embedded_in :word
 end
