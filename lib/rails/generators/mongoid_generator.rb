3c0735a93bc63c109151c145fbbf5f07fb8da8e0
Arthur Neves <arthurnn@gmail.com>
2013-10-15 21:17:01 -0400

    Remove custom generator for ActiveModel

--- lib/rails/generators/mongoid_generator.rb
+++ lib/rails/generators/mongoid_generator.rb
@@ -11,44 +11,6 @@ module Mongoid
           File.expand_path("../#{base_name}/#{generator_name}/templates", __FILE__)
       end
     end
-
-    class ActiveModel < ::Rails::Generators::ActiveModel
-      def self.all(klass)
-        "#{klass}.all"
-      end
-
-      def self.find(klass, params=nil)
-        "#{klass}.find(#{params})"
-      end
-
-      def self.build(klass, params=nil)
-        if params
-          "#{klass}.new(#{params})"
-        else
-          "#{klass}.new"
-        end
-      end
-
-      def save
-        "#{name}.save"
-      end
-
-      def update_attributes(params=nil)
-        "#{name}.update_attributes(#{params})"
-      end
-
-      def errors
-        "#{name}.errors"
-      end
-
-      def delete
-        "#{name}.delete"
-      end
-
-      def destroy
-        "#{name}.destroy"
-      end
-    end
   end
 end
 
