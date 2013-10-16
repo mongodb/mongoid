4fdb4cb1a3bd8951cc23fc0ea9602030f5677643
Durran Jordan <durran@gmail.com>
2013-10-16 21:37:44 +0200

    Adding spec for #3324

--- spec/mongoid/fields_spec.rb
+++ spec/mongoid/fields_spec.rb
@@ -1250,4 +1250,18 @@ describe Mongoid::Fields do
       end
     end
   end
+
+  context "when a localized field is a boolean" do
+
+    context "when the default is true" do
+
+      let(:definition) do
+        Definition.new
+      end
+
+      it "returns the proper predicate result" do
+        expect(definition).to be_active
+      end
+    end
+  end
 end
