41611480e8df778770d09468b230dea3089202d0
Arthur Neves <arthurnn@gmail.com>
2013-10-16 22:24:43 -0400

    Add more tests for eager_load on many_to_many relations

--- spec/mongoid/relations/eager/has_and_belongs_to_many_spec.rb
+++ spec/mongoid/relations/eager/has_and_belongs_to_many_spec.rb
@@ -2,29 +2,29 @@ require "spec_helper"
 
 describe Mongoid::Relations::Eager::HasAndBelongsToMany do
 
-  let!(:person) do
-    Person.create!(houses: [house])
-  end
-
-  let!(:house) do
-    House.create!
-  end
-
-  let(:houses_metadata) do
-    Person.reflect_on_association(:houses)
-  end
-
-  let(:docs) do
-    Person.all.to_a
-  end
-
-  let(:eager) do
-    described_class.new(Person, [houses_metadata], docs).tap do |b|
-      b.shift_relation
-    end
-  end
-
   describe ".grouped_docs" do
+
+    let(:docs) do
+      Person.all.to_a
+    end
+
+    let!(:person) do
+      Person.create!(houses: [house])
+    end
+
+    let!(:house) do
+      House.create!
+    end
+
+    let(:metadata) do
+      Person.reflect_on_association(:houses)
+    end
+
+    let(:eager) do
+      described_class.new(Person, [metadata], docs).tap do |b|
+        b.shift_relation
+      end
+    end
 
     it "aggregates by the parent primary key" do
       expect(eager.grouped_docs.keys).to eq([person.id])
@@ -32,6 +32,28 @@ describe Mongoid::Relations::Eager::HasAndBelongsToMany do
   end
 
   describe ".set_on_parent" do
+
+    let(:docs) do
+      Person.all.to_a
+    end
+
+    let!(:person) do
+      Person.create!(houses: [house])
+    end
+
+    let!(:house) do
+      House.create!
+    end
+
+    let(:metadata) do
+      Person.reflect_on_association(:houses)
+    end
+
+    let(:eager) do
+      described_class.new(Person, [metadata], docs).tap do |b|
+        b.shift_relation
+      end
+    end
 
     it "sets the relation into the parent" do
       docs.each do |doc|
@@ -42,6 +64,10 @@ describe Mongoid::Relations::Eager::HasAndBelongsToMany do
   end
 
   describe ".includes" do
+
+    let(:person) do
+      Person.create!
+    end
 
     before do
       Person.create!(houses: 3.times.map { House.create! })
@@ -56,6 +82,34 @@ describe Mongoid::Relations::Eager::HasAndBelongsToMany do
           Person.all.includes(:houses).each do |person|
             expect(person.houses).to_not be_nil
           end
+        end
+      end
+    end
+
+    context "when the relation is not polymorphic" do
+
+      let!(:preference) do
+        person.preferences.create(name: "testing")
+      end
+
+      let!(:eager) do
+        Person.includes(:preferences).last
+      end
+
+      it "puts the documents in the parent document" do
+        expect(eager.ivar(:preferences)).to eq([ preference ])
+      end
+
+      it "does not query when touching the association" do
+        expect_query(0) do
+          expect(eager.preferences).to eq([ preference ])
+        end
+      end
+
+      it "does not query when updating the association" do
+        expect_query(0) do
+          eager.preferences.first.name = "new pref"
+          expect(eager.preferences.first.name).to eq("new pref")
         end
       end
     end
