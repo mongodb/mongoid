6441855860ab58ff2234ce6890ac40e05366580b
Arthur Neves <arthurnn@gmail.com>
2013-10-16 21:08:45 -0400

    Add more tests for eager_load on HasMany relations

--- spec/mongoid/relations/eager/has_many_spec.rb
+++ spec/mongoid/relations/eager/has_many_spec.rb
@@ -2,29 +2,29 @@ require "spec_helper"
 
 describe Mongoid::Relations::Eager::HasMany do
 
-  let(:person) do
-    Person.create!
-  end
-
-  let!(:post) do
-    Post.create!(person: person)
-  end
-
-  let(:posts_metadata) do
-    Person.reflect_on_association(:posts)
-  end
-
-  let(:docs) do
-    Person.all.to_a
-  end
-
-  let(:eager) do
-    described_class.new(Person, [posts_metadata], docs).tap do |b|
-      b.shift_relation
-    end
-  end
-
   describe ".grouped_docs" do
+
+    let(:docs) do
+      Person.all.to_a
+    end
+
+    let(:person) do
+      Person.create!
+    end
+
+    before do
+      Post.create!(person: person)
+    end
+
+    let(:metadata) do
+      Person.reflect_on_association(:posts)
+    end
+
+    let(:eager) do
+      described_class.new(Person, [metadata], docs).tap do |b|
+        b.shift_relation
+      end
+    end
 
     it "aggregates by the parent primary key" do
       expect(eager.grouped_docs.keys).to eq([person.id])
@@ -32,6 +32,28 @@ describe Mongoid::Relations::Eager::HasMany do
   end
 
   describe ".set_on_parent" do
+
+    let(:docs) do
+      Person.all.to_a
+    end
+
+    let(:person) do
+      Person.create!
+    end
+
+    let(:metadata) do
+      Person.reflect_on_association(:posts)
+    end
+
+    let(:eager) do
+      described_class.new(Person, [metadata], docs).tap do |b|
+        b.shift_relation
+      end
+    end
+
+    before do
+      Post.create!(person: person)
+    end
 
     it "sets the relation into the parent" do
       docs.each do |doc|
@@ -43,20 +65,126 @@ describe Mongoid::Relations::Eager::HasMany do
 
   describe ".includes" do
 
-    before do
-      3.times { Drug.create!(person: person) }
-      Drug.create!(person: Person.create!)
+    let(:person) do
+      Person.create!
     end
 
     context "when including the has_many relation" do
 
+      before do
+        3.times { Drug.create!(person: person) }
+        Drug.create!(person: Person.create!)
+      end
+
       it "queries twice" do
 
         expect_query(2) do
-
           Person.all.includes(:drugs).each do |person|
             expect(person.drugs).to_not be_nil
           end
+        end
+      end
+    end
+
+    context "when the relation is not polymorphic" do
+
+      context "when the eager load has returned documents" do
+
+        let!(:post) do
+          person.posts.create(title: "testing")
+        end
+
+        let!(:eager) do
+          Person.includes(:posts).last
+        end
+
+        it "puts the documents in the parent document" do
+          expect(eager.ivar(:posts)).to eq([post])
+        end
+
+        it "does not query when touching the association" do
+          expect_query(0) do
+            expect(eager.posts).to eq([post])
+          end
+        end
+
+        it "does not query when updating the association" do
+          expect_query(0) do
+            eager.posts.first.title = "New title"
+          end
+        end
+      end
+
+      context "when the eager load has not returned documents" do
+
+        before do
+          person
+        end
+
+        let!(:eager) do
+          Person.includes(:posts).last
+        end
+
+        it "does not set anything on the parent" do
+          expect(eager.ivar(:posts)).to be_false
+        end
+
+        it "tries to query again when touching the association" do
+          expect_query(1) do
+            expect(eager.posts).to be_empty
+          end
+        end
+      end
+
+      context "when the eager load has not returned documents for some" do
+
+        let!(:person_one) do
+          person
+        end
+
+        let!(:person_two) do
+          Person.create(username: "durran")
+        end
+
+        let!(:post) do
+          person_one.posts.create(title: "testing")
+        end
+
+        let!(:eager) do
+          Person.includes(:posts).asc(:username).to_a
+        end
+
+        it "puts the found documents in the parent document" do
+          expect(eager.first.ivar(:posts)).to eq([post])
+        end
+
+        it "does not set documents not found" do
+          expect(eager.last.ivar(:posts)).to be_false
+        end
+      end
+    end
+
+    context "when the relation is polymorphic" do
+
+      let!(:movie) do
+        Movie.create(name: "Bladerunner")
+      end
+
+      let!(:rating) do
+        movie.ratings.create(value: 10)
+      end
+
+      let!(:eager) do
+        Movie.includes(:ratings).first
+      end
+
+      it "puts the found documents in the parent document" do
+        expect(eager.ivar(:ratings)).to eq([rating])
+      end
+
+      it "does not query when touching the association" do
+        expect_query(0) do
+          expect(eager.ratings).to eq([rating])
         end
       end
     end
