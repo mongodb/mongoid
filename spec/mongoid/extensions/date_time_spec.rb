cb72233de95b3c81719fa9c2a0b880ae43579f0e
Josef Šimánek <josef.simanek@gmail.com>
2013-11-06 22:11:10 +0100

    Convert DateTime to Time to avoid strip of decimal part in DateTime#__mongoize_time__.

--- spec/mongoid/extensions/date_time_spec.rb
+++ spec/mongoid/extensions/date_time_spec.rb
@@ -14,8 +14,27 @@ describe Mongoid::Extensions::DateTime do
         date_time.__mongoize_time__
       end
 
-      pending "does not drop the precision" do
-        expect(mongoized.to_f.to_s).to match(/\.123/)
+      context 'does not drop the precision' do
+
+        it "for utc DateTime" do
+          date_time.stub(:utc?).and_return(true)
+          Mongoid.stub(:use_utc?).and_return(true)
+          expect(mongoized.to_f.to_s).to match(/\.123/)
+        end
+
+        it "when using Active Support timezone" do
+          date_time.stub(:utc?).and_return(false)
+          Mongoid.stub(:use_utc?).and_return(false)
+          Mongoid.stub(:use_activesupport_time_zone?).and_return(true)
+          expect(mongoized.to_f.to_s).to match(/\.123/)
+        end
+
+        it "for DateTime without zone" do
+          date_time.stub(:utc?).and_return(false)
+          Mongoid.stub(:use_utc?).and_return(false)
+          Mongoid.stub(:use_activesupport_time_zone?).and_return(false)
+          expect(mongoized.to_f.to_s).to match(/\.123/)
+        end
       end
     end
 
