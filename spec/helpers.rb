e7d5e3c6ac63a18525f58bb68134e895a340001c
Arthur Neves <arthurnn@gmail.com>
2013-11-14 23:06:46 -0500

    Make QueryCounter public
    QueryCounter might be useful for people running tests
    Also to create examples so we can track issues better

--- spec/helpers.rb
+++ spec/helpers.rb
@@ -1,7 +1,9 @@
+require 'mongoid/support/query_counter'
+
 module Mongoid
   module SpecHelpers
     def expect_query(number, &block)
-      query_counter = QueryCounter.new
+      query_counter = Mongoid::QueryCounter.new
       query_counter.instrument(&block)
       expect(query_counter.events.size).to(eq(number), %[
 Expected to receive #{number} queries, it received #{query_counter.events.size}
@@ -11,27 +13,6 @@ Expected to receive #{number} queries, it received #{query_counter.events.size}
 
     def expect_no_queries(&block)
       expect_query(0, &block)
-    end
-  end
-
-  class QueryCounter
-    attr_reader :events
-
-    def initialize
-      @events = []
-    end
-
-    def instrument
-      subscriber = ActiveSupport::Notifications.subscribe('query.moped') do |*args|
-        @events << ActiveSupport::Notifications::Event.new(*args)
-      end
-      yield
-    ensure
-      ActiveSupport::Notifications.unsubscribe(subscriber)
-    end
-
-    def inspect
-      @events.map { |e| e.payload[:ops].map(&:log_inspect) }.join("\n")
     end
   end
 end
