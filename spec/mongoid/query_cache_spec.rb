7caef6417b9dc6bb0498cf267972c658f68f83e5
Arthur Neves <arthurnn@gmail.com>
2013-11-24 02:09:46 -0500

    Add more tests to query cache

--- spec/mongoid/query_cache_spec.rb
+++ spec/mongoid/query_cache_spec.rb
@@ -39,4 +39,56 @@ describe Mongoid::QueryCache do
       end
     end
   end
+
+  context "when inserting a new document" do
+
+    before do
+      Band.all.to_a
+      Band.create!
+    end
+
+    it "queries again" do
+      expect_query(1) do
+        Band.all.to_a
+      end
+    end
+  end
+
+  context "when deleting all documents" do
+
+    before do
+      Band.create!
+      Band.all.to_a
+      Band.delete_all
+    end
+
+    it "queries again" do
+      expect_query(1) do
+        Band.all.to_a
+      end
+    end
+  end
+
+  context "when destroying all documents" do
+
+    before do
+      Band.create!
+      Band.all.to_a
+      Band.destroy_all
+    end
+
+    it "queries again" do
+      expect_query(1) do
+        Band.all.to_a
+      end
+    end
+  end
+
+  context "when inserting an index" do
+
+    it "does not cache the query" do
+      expect(Mongoid::QueryCache).to receive(:cache_table).never
+      Band.collection.indexes.create(name: 1)
+    end
+  end
 end
