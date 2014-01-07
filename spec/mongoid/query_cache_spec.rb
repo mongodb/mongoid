f155beb4a4be6133969ea849f52e83ec24d769a5
Arthur Neves <arthurnn@gmail.com>
2014-01-06 23:06:25 -0500

    Add QueryCache::Middleware to enable query cache

--- spec/mongoid/query_cache_spec.rb
+++ spec/mongoid/query_cache_spec.rb
@@ -2,10 +2,27 @@ require "spec_helper"
 
 describe Mongoid::QueryCache do
 
+  around do |spec|
+    Mongoid::QueryCache.cache { spec.run }
+  end
+
   context "when querying in the same collection" do
 
     before do
       Band.all.to_a
+    end
+
+    context "when query cache disable" do
+
+      before do
+        Mongoid::QueryCache.enabled = false
+      end
+
+      it "queries again" do
+        expect_query(1) do
+          Band.all.to_a
+        end
+      end
     end
 
     context "with same selector" do
@@ -89,6 +106,50 @@ describe Mongoid::QueryCache do
     it "does not cache the query" do
       expect(Mongoid::QueryCache).to receive(:cache_table).never
       Band.collection.indexes.create(name: 1)
+    end
+  end
+end
+
+describe Mongoid::QueryCache::Middleware do
+
+  let :middleware do
+    Mongoid::QueryCache::Middleware.new(app)
+  end
+
+  context "when not touching mongoid on the app" do
+
+    let(:app) do
+      ->(env) { @enabled = Mongoid::QueryCache.enabled?; [200, env, "app"] }
+    end
+
+    it "returns success" do
+      code, _ = middleware.call({})
+      expect(code).to eq(200)
+    end
+
+    it "enableds the query cache" do
+      middleware.call({})
+      expect(@enabled).to be true
+    end
+  end
+
+  context "when querying on the app" do
+
+    let(:app) do
+      ->(env) {
+        Band.all.to_a
+        [200, env, "app"]
+      }
+    end
+
+    it "returns success" do
+      code, _ = middleware.call({})
+      expect(code).to eq(200)
+    end
+
+    it "cleans the query cache after reponds" do
+      middleware.call({})
+      expect(Mongoid::QueryCache.cache_table).to be_empty
     end
   end
 end
