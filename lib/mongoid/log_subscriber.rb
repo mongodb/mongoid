6f790db12f76d0781379bed08ad29e436fbc8b66
Arthur Neves <arthurnn@gmail.com>
2014-01-23 22:46:41 -0500

    Add RDoc on LogSubscriber [skip ci]

--- lib/mongoid/log_subscriber.rb
+++ lib/mongoid/log_subscriber.rb
@@ -1,6 +1,13 @@
+# encoding: utf-8
 module Mongoid
-
+  # A Log subscriber to the moped queries
+  #
+  # @since 4.0.0
   class LogSubscriber < ActiveSupport::LogSubscriber
+
+    # Log the query operation on moped
+    #
+    # @since 4.0.0
     def query(event)
       return unless logger.debug?
 
@@ -9,10 +16,28 @@ module Mongoid
       debug(payload[:prefix], payload[:ops], runtime)
     end
 
+    # Log the provided operations.
+    #
+    # @example Delegates the operation to moped so it can log it.
+    #   subscriber.debug("MOPED", {}, 30)
+    #
+    # @param [ String ] prefix The prefix for all operations in the log.
+    # @param [ Array ] ops The operations.
+    # @param [ String ] runtime The runtime in formatted ms.
+    #
+    # @since 4.0.0
     def debug(prefix, operations, runtime)
       Moped::Loggable.log_operations(prefix, operations, runtime)
     end
 
+    # Get the logger.
+    #
+    # @example Get the logger.
+    #   subscriber.logger
+    #
+    # @return [ Logger ] The logger.
+    #
+    # @since 4.0.0
     def logger
       Moped.logger
     end
