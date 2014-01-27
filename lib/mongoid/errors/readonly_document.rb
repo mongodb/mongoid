904860dc6a83380a0b5439d01061c738a3bfa212
Durran Jordan <durran@gmail.com>
2014-01-27 14:47:31 +0100

    Add documentation on readonly error

--- lib/mongoid/errors/readonly_document.rb
+++ lib/mongoid/errors/readonly_document.rb
@@ -2,8 +2,20 @@
 module Mongoid
   module Errors
 
+    # Raised when attempting to persist a document that was loaded from the
+    # database with partial fields.
+    #
+    # @since 4.0.0
     class ReadonlyDocument < MongoidError
 
+      # Instnatiate the exception.
+      #
+      # @example Create the error.
+      #   ReadonlyDocument.new(Band)
+      #
+      # @param [ Class ] klass The document class.
+      #
+      # @since 4.0.0
       def initialize(klass)
         super(compose_message("readonly_document", { klass: klass }))
       end
