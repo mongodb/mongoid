453142f5c668242a9b1ea33e6d596ef4a579a888
Durran Jordan <durran@gmail.com>
2014-04-01 16:02:42 +0200

    Bit changed behaviour

--- spec/mongoid/contextual/atomic_spec.rb
+++ spec/mongoid/contextual/atomic_spec.rb
@@ -119,7 +119,7 @@ describe Mongoid::Contextual::Atomic do
       end
 
       it "does not error on non initialized fields" do
-        expect(smiths.reload.likes).to be_nil
+        expect(smiths.reload.likes).to eq(13)
       end
     end
 
@@ -134,7 +134,7 @@ describe Mongoid::Contextual::Atomic do
       end
 
       it "does not error on non initialized fields" do
-        expect(smiths.reload.likes).to be_nil
+        expect(smiths.reload.likes).to eq(10)
       end
     end
   end
