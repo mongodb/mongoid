7c1fea020a02df382f420783fbdb31929dbb8648
Gauthier Delacroix <gauthier.delacroix@gmail.com>
2014-09-05 14:28:53 +0200

    Fix #reload uses id instead of _id

--- lib/mongoid/reloadable.rb
+++ lib/mongoid/reloadable.rb
@@ -21,7 +21,7 @@ module Mongoid
     def reload
       reloaded = _reload
       if Mongoid.raise_not_found_error && reloaded.empty?
-        raise Errors::DocumentNotFound.new(self.class, id, id)
+        raise Errors::DocumentNotFound.new(self.class, _id, _id)
       end
       @attributes = reloaded
       @attributes_before_type_cast = {}
@@ -57,7 +57,7 @@ module Mongoid
     #
     # @since 2.3.2
     def reload_root_document
-      {}.merge(with(read: :primary).collection.find(_id: id).one || {})
+      {}.merge(with(read: :primary).collection.find(_id: _id).one || {})
     end
 
     # Reload the embedded document.
@@ -70,7 +70,7 @@ module Mongoid
     # @since 2.3.2
     def reload_embedded_document
       extract_embedded_attributes({}.merge(
-        _root.with(read: :primary).collection.find(_id: _root.id).one
+        _root.with(read: :primary).collection.find(_id: _root._id).one
       ))
     end
 
