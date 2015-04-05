mongodb/mongoid

commit 9e5a279f306db9905f7cf331c6915a4ff24518f9

Fixes #3993

lib/mongoid/attributes/processing.rb
@@ -92,6 +92,9 @@ module Mongoid
       #
       # @since 2.0.0.rc.7
       def process_attribute(name, value)
+        if store_as = aliased_fields.invert[name.to_s]
+          name = store_as
+        end
         responds = respond_to?("#{name}=")
         raise Errors::UnknownAttribute.new(self.class, name) unless responds
         send("#{name}=", value)

spec/mongoid/copyable_spec.rb
@@ -54,6 +54,30 @@ describe Mongoid::Copyable do
         end
       end
 
+      context "when using store_as" do
+        context "and dynamic attributes are not set" do
+          it "clones" do
+            class StoreAsDupTest1
+              include Mongoid::Document
+              embeds_one :store_as_dup_test2, :store_as => :t
+              field :name
+            end
+
+            class StoreAsDupTest2
+              include Mongoid::Document
+              embedded_in :store_as_dup_test1
+              field :name
+            end
+
+            t = StoreAsDupTest1.new(:name => "hi")
+            t.build_store_as_dup_test2(:name => "there")
+            t.save
+            copy = t.__send__(method)
+            expect(copy.store_as_dup_test2.name).to eq(t.store_as_dup_test2.name)
+          end
+        end
+      end
+
       context "when cloning a document with multiple languages field" do
 
         before do

commit 684fc995b7d3eec1a3be2e9fc2c2bc2ab01ae747

Fix regression I caused in 9e5a279f306db9905f7cf331c6915a4ff24518f9

lib/mongoid/attributes/processing.rb
@@ -92,7 +92,8 @@ module Mongoid
       #
       # @since 2.0.0.rc.7
       def process_attribute(name, value)
-        if store_as = aliased_fields.invert[name.to_s]
+        responds = respond_to?("#{name}=")
+        if !responds && store_as = aliased_fields.invert[name.to_s]
           name = store_as
         end
         responds = respond_to?("#{name}=")

spec/mongoid/relations/eager/belongs_to_spec.rb
@@ -150,5 +150,14 @@ describe Mongoid::Relations::Eager::BelongsTo do
         }.to raise_error(Mongoid::Errors::EagerLoad)
       end
     end
+
+    context "when setting the foreign key id directly" do
+
+      it "works" do
+        id = BSON::ObjectId.new
+        game = Game.new(:person_id => id)
+        expect(game.person_id).to eql(id)
+      end
+    end
   end
 end