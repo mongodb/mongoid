e146a3163f5c568da58798256424db62f4d7e2c2
Andrea Maria Piana <andrea.maria.piana@gmail.com>
2015-05-31 08:12:15 +0100

    Don't throw exception on empty localized fields when calling valid?

--- lib/mongoid/validatable/uniqueness.rb
+++ lib/mongoid/validatable/uniqueness.rb
@@ -123,7 +123,7 @@ module Mongoid
         field = document.database_field_name(attribute)
 
         if localized?(document, field)
-          conditions = value.inject([]) { |acc, (k,v)| acc << { "#{field}.#{k}" => filter(v) } }
+          conditions = (value || {}).inject([]) { |acc, (k,v)| acc << { "#{field}.#{k}" => filter(v) } }
           selector = { "$or" => conditions }
         else
           selector = { field => filter(value) }
