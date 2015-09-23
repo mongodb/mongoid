8d8843b71e5adf56bea922c911bd1dc6b27286a6
Emily Stolfo <emily@10gen.com>
2015-09-23 17:18:47 -0400

    MONGOID-4119 Match on criteria selector and on

--- lib/mongoid/contextual/aggregable/mongo.rb
+++ lib/mongoid/contextual/aggregable/mongo.rb
@@ -124,8 +124,8 @@ module Mongoid
         def pipeline(field)
           db_field = "$#{database_field_name(field)}"
           pipeline = []
-          pipeline << { "$match" => { field => { :$exists => true }  } }
-          pipeline << { "$match" => { field => criteria.selector[field.to_s] } }
+          pipeline << { "$match" => criteria.selector }
+          pipeline << { "$match" => { field => { :$exists => true } } }
           pipeline << { "$sort" => criteria.options[:sort] } if criteria.options[:sort]
           pipeline << { "$skip" => criteria.options[:skip] } if criteria.options[:skip]
           pipeline << { "$limit" => criteria.options[:limit] } if criteria.options[:limit]
