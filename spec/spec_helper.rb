b3ea3c8eee9c2e99fb33964cb11de6de1aceebed
Emily <emily@mongodb.com>
2015-12-08 10:53:48 +0100

    Update reference to ismaster in spec helper for driver 2.2.0

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -41,7 +41,7 @@ if (ENV['CI'] == 'travis')
   client = Mongo::Client.new(['127.0.0.1:27017'])
   while starting
     begin
-      client.command(Mongo::Server::Monitor::STATUS)
+      client.command(Mongo::Server::Monitor::Connection::ISMASTER)
       break
     rescue Mongo::Error::OperationFailure => e
       sleep(2)
