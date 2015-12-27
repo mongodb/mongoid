02d0ef5754fef0e9e56d2f969fe3a2ca3695f340
Siva Gollapalli <siva@joshsoftware.com>
2015-12-27 13:14:56 +0530

    MONGOID-3246 Modified example into context block

--- spec/mongoid/relations/referenced/many_to_many_spec.rb
+++ spec/mongoid/relations/referenced/many_to_many_spec.rb
@@ -3536,39 +3536,43 @@ describe Mongoid::Relations::Referenced::ManyToMany do
     end
   end
 
-  example "HABTM should update refereces from two sides" do
-    class Project
-      include Mongoid::Document
+  context "HABTM" do
+    before do
+      class Project
+        include Mongoid::Document
 
-      field :n, type: String, as: :name
+        field :n, type: String, as: :name
 
-      has_and_belongs_to_many :distributors,
-        foreign_key: :d_ids,
-        inverse_of: 'p',
-        inverse_class_name: 'Distributor'
+        has_and_belongs_to_many :distributors,
+          foreign_key: :d_ids,
+          inverse_of: 'p',
+          inverse_class_name: 'Distributor'
+      end
+
+      class Distributor
+        include Mongoid::Document
+
+        field :n, type: String, as: :name
+
+        has_and_belongs_to_many :projects,
+          foreign_key: :p_ids,
+          inverse_of: 'd',
+          inverse_class_name: 'Project'
+      end
     end
 
-    class Distributor
-      include Mongoid::Document
+    it "should assign relation from both sides" do
+      p1 = Project.create name: 'Foo'
+      p2 = Project.create name: 'Bar'
+      d1 = Distributor.create name: 'Rock'
+      d2 = Distributor.create name: 'Soul'
 
-      field :n, type: String, as: :name
-
-      has_and_belongs_to_many :projects,
-        foreign_key: :p_ids,
-        inverse_of: 'd',
-        inverse_class_name: 'Project'
+      p1.distributors << d1
+      expect(p1.d_ids).to match_array([d1.id])
+      expect(d1.p_ids).to match_array([p1.id])
+      d2.projects << p2
+      expect(d2.p_ids).to match_array([p2.id])
+      expect(p2.d_ids).to match_array([d2.id])
     end
-
-    p1 = Project.create name: 'Foo'
-    p2 = Project.create name: 'Bar'
-    d1 = Distributor.create name: 'Rock'
-    d2 = Distributor.create name: 'Soul'
-
-    p1.distributors << d1
-    expect(p1.d_ids).to match_array([d1.id])
-    expect(d1.p_ids).to match_array([p1.id])
-    d2.projects << p2
-    expect(d2.p_ids).to match_array([p2.id])
-    expect(p2.d_ids).to match_array([d2.id])
   end
 end
