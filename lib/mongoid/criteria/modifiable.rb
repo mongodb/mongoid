7306a34328244c17e18d144296eb66a0eb3f893a
Emily <emily@mongodb.com>
2016-02-22 12:38:48 +0100

    MONGOID-3887 Allow a hash field to be set through criteria create methods

--- lib/mongoid/criteria/modifiable.rb
+++ lib/mongoid/criteria/modifiable.rb
@@ -173,7 +173,7 @@ module Mongoid
       # @since 3.0.0
       def create_document(method, attrs = nil, &block)
         attributes = selector.reduce(attrs ? attrs.dup : {}) do |hash, (key, value)|
-          unless key.to_s =~ /\$/ || value.is_a?(Hash)
+          unless key.to_s =~ /\$/ || (value.is_a?(Hash) && !attribute_names.include?(key))
             hash[key] = value
           end
           hash
