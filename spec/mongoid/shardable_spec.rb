227d657568fcaa59bea038eaec01d9c165a7ce2a
Emily <emily@mongodb.com>
2016-04-01 11:53:40 +0200

    MONGOID-3735 Allow a relation to be defined as a shard key

--- spec/mongoid/shardable_spec.rb
+++ spec/mongoid/shardable_spec.rb
@@ -22,27 +22,37 @@ describe Mongoid::Shardable do
   describe ".shard_key" do
 
     let(:klass) do
-      Class.new do
-        include Mongoid::Shardable
-      end
+      Band
     end
 
     before do
-      klass.shard_key(:name)
+      Band.shard_key(:name)
     end
 
     it "specifies a shard key on the collection" do
       expect(klass.shard_key_fields).to eq([:name])
+    end
+
+    context 'when a relation is used as the shard key' do
+
+      let(:klass) do
+        Game
+      end
+
+      before do
+        Game.shard_key(:person)
+      end
+
+      it "converts the shard key to the foreign key field" do
+        expect(klass.shard_key_fields).to eq([:person_id])
+      end
     end
   end
 
   describe "#shard_key_selector" do
 
     let(:klass) do
-      Class.new do
-        include Mongoid::Shardable
-        attr_accessor :name
-      end
+      Band
     end
 
     let(:object) do
