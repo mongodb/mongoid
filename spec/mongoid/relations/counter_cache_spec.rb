8889366dbb9ab7fe219b48ec883a2616eec711d1
Emily <emily@mongodb.com>
2016-04-06 13:42:26 +0200

    Apply persistence options when updating counters

--- spec/mongoid/relations/counter_cache_spec.rb
+++ spec/mongoid/relations/counter_cache_spec.rb
@@ -85,6 +85,26 @@ describe Mongoid::Relations::CounterCache do
         expect(subscription.reload[:packs_count]).to eq(1)
       end
     end
+
+    context 'when there are persistence options set' do
+
+      let(:subscription) do
+        Subscription.new
+      end
+
+      before do
+        subscription.with(collection: 'other') do |sub|
+          sub.save
+          sub.packs.create
+        end
+      end
+
+      it 'applies the persistence options when resetting the counter' do
+        subscription.with(collection: 'other') do |sub|
+          expect(sub.reload[:packs_count]).to eq(1)
+        end
+      end
+    end
   end
 
   describe ".reset_counters" do
@@ -256,6 +276,26 @@ describe Mongoid::Relations::CounterCache do
         expect(person.reload.drugs_count).to eq(3)
       end
     end
+
+    context 'when there are persistence options set' do
+
+      let(:person) do
+        Person.new
+      end
+
+      before do
+        person.with(collection: 'other') do |per|
+          per.save
+          per.drugs.create
+        end
+      end
+
+      it 'applies the persistence options when resetting the counter' do
+        person.with(collection: 'other') do |per|
+          expect(per.drugs_count).to eq(1)
+        end
+      end
+    end
   end
 
   describe "#decrement_counter" do
@@ -284,6 +324,27 @@ describe Mongoid::Relations::CounterCache do
 
       it "returns 0" do
         expect(person.reload.drugs_count).to eq(0)
+      end
+    end
+
+    context 'when there are persistence options set' do
+
+      let(:person) do
+        Person.new
+      end
+
+      before do
+        person.with(collection: 'other') do |per|
+          per.save
+          drug = per.drugs.create
+          drug.destroy
+        end
+      end
+
+      it 'applies the persistence options when resetting the counter' do
+        person.with(collection: 'other') do |per|
+          expect(per.reload.drugs_count).to eq(0)
+        end
       end
     end
   end
