0dbf79bb12b9b70673c9709302c60edd45742928
Richard Machielse <richardmachielse@gmail.com>
2016-05-18 23:32:23 +0200

    Add failing self referencing habtm spec

---
+++ spec/app/models/vertex.rb
@@ -0,0 +1,6 @@
+class Vertex
+  include Mongoid::Document
+
+  has_and_belongs_to_many :parents, inverse_of: :children, class_name: 'Vertex'
+  has_and_belongs_to_many :children, inverse_of: :parents, class_name: 'Vertex'
+end
