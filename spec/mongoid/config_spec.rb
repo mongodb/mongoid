46cc6875d97ed685ed0d592e9a27c8a39faf9506
Emily <emily@mongodb.com>
2016-12-01 16:18:44 +0100

    MONGOID-4327 Ensure that max_staleness is exposed as a read pref option

--- spec/mongoid/config_spec.rb
+++ spec/mongoid/config_spec.rb
@@ -282,7 +282,7 @@ describe Mongoid::Config do
         context "when a default is provided" do
 
           before do
-            described_class.load!(file)
+            described_class.load!(file, :test_with_max_staleness)
           end
 
           let(:default) do
@@ -301,7 +301,7 @@ describe Mongoid::Config do
 
             it "sets the read option" do
               expect(options["read"]).to eq({ "mode" => :primary_preferred,
-                                              "tag_sets" => [{ "use" => "web" }]})
+                                              "max_staleness" => 100 })
             end
           end
         end
