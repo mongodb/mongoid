cdf9f76ea9d5cef9941c8b54da1563696a7404c2
FENG Zhichao <flankerfc@gmail.com>
2017-03-24 11:16:02 +0800

    Failing spec: changes in callbacks of an embedded document not persisted.

--- spec/app/models/page.rb
+++ spec/app/models/page.rb
@@ -6,11 +6,16 @@ class Page
   embedded_in :book, touch: true
   embeds_many :notes
   field :content, :type => String
+  field :page_number, :type => Integer
 
   after_initialize do
     if self[:content]
       self[:text] = self[:content]
       self.remove_attribute(:content)
     end
+  end
+
+  before_validation do
+    self.page_number = 1 unless self.page_number
   end
 end
