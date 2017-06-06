2239d89096503c5cad9fe321c569199eeaac2cc6
Emily <emily@mongodb.com>
2017-06-06 11:39:04 +0200

    Update evergreen test script

--- .evergreen/run-tests.sh
+++ .evergreen/run-tests.sh
@@ -14,7 +14,20 @@ export CI=evergreen
 export JRUBY_OPTS="--server -J-Xms512m -J-Xmx1G"
 
 source ~/.rvm/scripts/rvm
+
+# Don't errexit because this may call scripts which error
+set +o errexit
 rvm use $RVM_RUBY
+set -o errexit
+
+# Ensure we're using the right ruby
+python - <<EOH
+ruby = "${RVM_RUBY}".split("-")[0]
+version = "${RVM_RUBY}".split("-")[1]
+assert(ruby in "`ruby --version`")
+assert(version in "`ruby --version`")
+EOH
+
 gem install bundler
 
 if [ "$DRIVER" == "master" ]; then
