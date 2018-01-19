89e6abb1f045e25a5df4baf94b694df4693b9580
Emily <emily@mongodb.com>
2018-01-19 14:43:04 +0100

    Set BUNDLE_GEMFILE when running bundle exec in Evergreen

--- .evergreen/run-tests.sh
+++ .evergreen/run-tests.sh
@@ -41,10 +41,12 @@ gem install bundler
 
 if [ $DRIVER == "master" ]; then
   bundle install --gemfile=gemfiles/driver_master.gemfile
+    export BUNDLE_GEMFILE=gemfiles/driver_master.gemfile
+    BUNDLE_GEMFILE=gemfiles/driver_master.gemfile bundle exec rake spec
 elif [ $RAILS == "master" ]; then
     bundle install --gemfile=gemfiles/rails_master.gemfile
+    BUNDLE_GEMFILE=gemfiles/rails_master.gemfile bundle exec rake spec
 else
   bundle install
+  bundle exec rake spec
 fi
-
-bundle exec rake spec
