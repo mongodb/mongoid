51ae03b338242d42dd375357f10351e713a8ec8e
Emily <emily@mongodb.com>
2018-01-19 15:12:43 +0100

    Catch error in spec thrown by ActiveSupport when a descendant doesn't have a particular callback defined

--- spec/mongoid/interceptable_spec.rb
+++ spec/mongoid/interceptable_spec.rb
@@ -526,7 +526,9 @@ describe Mongoid::Interceptable do
           end
 
           after(:all) do
-            Band.reset_callbacks(:rearrange)
+            # ActiveSupport may raise an error when trying to reset callbacks on all of Band's
+            # descendants, regardless of whether they have a particular callback defined.
+            begin; Band.reset_callbacks(:rearrange); rescue; end
           end
 
           let(:attributes) do
