8eddbdaee7c261b21d67a57fb31dc0e306a04f38
Emily <emily@mongodb.com>
2018-05-02 18:33:37 -0400

    Update updatable spec for server 3.6 not considering bad.key an invalid key

--- spec/mongoid/persistable/updatable_spec.rb
+++ spec/mongoid/persistable/updatable_spec.rb
@@ -283,7 +283,7 @@ describe Mongoid::Persistable::Updatable do
 
         it "raises an error" do
           expect {
-            person.update_attributes(map: { "bad.key" => "value" })
+            person.update_attributes(map: { "$bad.key" => "value" })
           }.to raise_error(Mongo::Error::OperationFailure)
         end
       end
@@ -319,7 +319,7 @@ describe Mongoid::Persistable::Updatable do
 
         it "raises an error" do
           expect {
-            person.send(method, map: { "bad.key" => "value" })
+            person.send(method, map: { "$bad.key" => "value" })
           }.to raise_error(Mongo::Error::OperationFailure)
         end
       end
