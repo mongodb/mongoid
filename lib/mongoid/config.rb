6409303b994dcdbb307d762d2af0b30a002e2913
Oleg Pudeyev <oleg@bsdpower.com>
2018-07-02 22:53:11 -0400

    Improve documentation

--- lib/mongoid/config.rb
+++ lib/mongoid/config.rb
@@ -5,8 +5,8 @@ require "mongoid/config/validators"
 
 module Mongoid
 
-  # This module defines all the configuration options for Mongoid, including the
-  # database connections.
+  # This module defines all the configuration options for Mongoid, including
+  # the database connections.
   module Config
     extend self
     extend Options
@@ -24,7 +24,17 @@ module Mongoid
     option :duplicate_fields_exception, default: false
     option :use_activesupport_time_zone, default: true
     option :use_utc, default: false
+
+    # The log level.
+    #
+    # It must be set prior to referencing clients or Mongo.logger,
+    # changes to this option are not be propagated to any clients and
+    # loggers that already exist.
+    #
+    # Additionally, only when the clients are configured via the
+    # configuration file is the log level given by this option honored.
     option :log_level, default: :info
+
     option :belongs_to_required_by_default, default: true
     option :app_name, default: nil
     option :background_indexing, default: false
