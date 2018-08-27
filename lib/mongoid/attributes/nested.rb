679abbd043d422981af4bda3aaeacd64a4bc314d
Saghm Rossi <saghmrossi@gmail.com>
2018-08-27 18:00:52 -0400

    MONGOID-4543 enable autosave for accepts_nested_attributes_for (#4526)

--- lib/mongoid/attributes/nested.rb
+++ lib/mongoid/attributes/nested.rb
@@ -43,15 +43,21 @@ module Mongoid
         #   to a class method to reject documents with.
         # @option *args [ Integer ] :limit The max number to create.
         # @option *args [ true, false ] :update_only Only update existing docs.
+        # @options *args [ true, false ] :autosave Whether autosave should be enabled on the
+        #   association. Note that since the default is true, setting autosave to nil will still
+        #   enable it.
         def accepts_nested_attributes_for(*args)
-          options = args.extract_options!
+          options = args.extract_options!.dup
+          options[:autosave] = true if options[:autosave].nil?
+
           options[:reject_if] = REJECT_ALL_BLANK_PROC if options[:reject_if] == :all_blank
           args.each do |name|
             meth = "#{name}_attributes="
             self.nested_attributes["#{name}_attributes"] = meth
             association = relations[name.to_s]
             raise Errors::NestedAttributesMetadataNotFound.new(self, name) unless association
-            autosave_nested_attributes(association)
+            autosave_nested_attributes(association) if options[:autosave]
+
             re_define_method(meth) do |attrs|
               _assigning do
                 if association.polymorphic? and association.inverse_type
@@ -76,7 +82,12 @@ module Mongoid
         #
         # @since 3.1.4
         def autosave_nested_attributes(association)
-          if association.autosave?
+          # In order for the autosave functionality to work properly, the association needs to be
+          # marked as autosave despite the fact that the option isn't present. Because the method
+          # Association#autosave? is implemented by checking the autosave option, this is the most
+          # straightforward way to mark it.
+          if association.autosave? || (association.options[:autosave].nil? && !association.embedded?)
+            association.options[:autosave] = true
             Association::Referenced::AutoSave.define_autosave!(association)
           end
         end
