e9d73495e61a5c259c55e2e9537ec9f12e40385f
Saghm Rossi <saghmrossi@gmail.com>
2018-08-27 14:25:24 -0400

    document autosave

--- lib/mongoid/attributes/nested.rb
+++ lib/mongoid/attributes/nested.rb
@@ -43,6 +43,8 @@ module Mongoid
         #   to a class method to reject documents with.
         # @option *args [ Integer ] :limit The max number to create.
         # @option *args [ true, false ] :update_only Only update existing docs.
+        # @options *args [ true, false ] :autosave Whether autosave should be enabled on the
+        #   association.
         def accepts_nested_attributes_for(*args)
           options = args.extract_options!.dup
           options[:autosave] = true if options[:autosave].nil?
