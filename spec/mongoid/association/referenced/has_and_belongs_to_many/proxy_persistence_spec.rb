1cb1188f76f4e1d181bad3480779a14c5bd887a6
Oleg Pudeyev <oleg@bsdpower.com>
2019-02-02 22:26:54 -0500

    Fix MONGOID-4626 Querying unscoped associations ignores primary_key/foreign_key depending on association

---
+++ spec/mongoid/association/referenced/has_and_belongs_to_many/proxy_persistence_spec.rb
@@ -0,0 +1,56 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+require_relative '../has_and_belongs_to_many_models'
+
+describe Mongoid::Association::Referenced::HasAndBelongsToMany::Proxy do
+  context 'with primary_key and foreign_key given' do
+    let(:company) { HabtmmCompany.create!(c_id: 123) }
+    let(:employee) { HabtmmEmployee.create!(e_id: 456) }
+
+    before do
+      company.employees << employee
+    end
+
+    it 'references correct field' do
+      expect(company.e_ids).to eq([456])
+    end
+
+    describe 'nullify' do
+      before do
+        company.employees << repl_employee
+        company.save!
+        expect(company.employees.length).to eq(2)
+        repl_employee.habtmm_company_ids = [company.id]
+        repl_employee.save!
+        repl_employee.reload
+        expect(repl_employee.habtmm_company_ids).to eq([company.id])
+      end
+
+      context 'without replacement' do
+        let(:repl_employee) { HabtmmEmployee.create!(e_id: 444) }
+
+        it 'disassociates child from parent' do
+          company.employees.nullify
+          repl_employee.reload
+          expect(repl_employee.habtmm_company_ids).to eq([])
+        end
+      end
+
+      context 'with replacement' do
+        let(:repl_employee) { HabtmmEmployee.create!(e_id: 444) }
+
+        before do
+          company.employees << repl_employee
+          expect(company.employees.length).to eq(2)
+        end
+
+        it 'keeps replacement associated with the parent' do
+          company.employees.nullify([repl_employee])
+          repl_employee.reload
+          expect(repl_employee.habtmm_company_ids).to eq([company.id])
+        end
+      end
+    end
+  end
+end
