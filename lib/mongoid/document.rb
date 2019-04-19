07e2d67ea91f1f0274b177003c6fa1635c637abf
Oleg Pudeyev <39304720+p-mongo@users.noreply.github.com>
2019-04-19 16:52:33 -0400

    Fix MONGOID-4595 Test failure against rails master (#4560) (#4625)

--- lib/mongoid/document.rb
+++ lib/mongoid/document.rb
@@ -180,6 +180,13 @@ module Mongoid
 
     # Calls #as_json on the document with additional, Mongoid-specific options.
     #
+    # @note Rails 6 changes return value of as_json for non-primitive types
+    #   such as BSON::ObjectId. In Rails <= 5, as_json returned these as
+    #   instances of the class. In Rails 6, these are returned serialized to
+    #   primitive types (e.g. {"$oid"=>"5bcfc40bde340b37feda98e9"}).
+    #   See https://github.com/rails/rails/commit/2e5cb980a448e7f4ab00df6e9ad4c1cc456616aa
+    #   for more information.
+    #
     # @example Get the document as json.
     #   document.as_json(compact: true)
     #
