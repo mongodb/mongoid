71351cd58a0220ac2e280d522e9dda44ae6ffcdd
Oleg Pudeyev <39304720+p-mongo@users.noreply.github.com>
2019-05-23 09:50:15 -0400

    Fix MONGOID-4645 belongs_to in namespaced children fails to detect a non-namespaced association (#4633)
    * Fix MONGOID-4645 belongs_to in namespaced children fails to detect a non-namespaced association
    
    * Add a docstring

--- spec/mongoid/association/referenced/has_one_models.rb
+++ spec/mongoid/association/referenced/has_one_models.rb
@@ -14,3 +14,35 @@ class HomAccreditation
   field :degree, type: String
   field :year, type: Integer, default: 2012
 end
+
+class HomAccreditation::Child
+  include Mongoid::Document
+
+  belongs_to :hom_college
+end
+
+module HomNs
+  class PrefixedParent
+    include Mongoid::Document
+
+    has_one :child, class_name: 'PrefixedChild'
+  end
+
+  class PrefixedChild
+    include Mongoid::Document
+
+    belongs_to :parent, class_name: 'PrefixedParent'
+  end
+end
+
+class HomPolymorphicParent
+  include Mongoid::Document
+
+  has_one :p_child, as: :parent
+end
+
+class HomPolymorphicChild
+  include Mongoid::Document
+
+  belongs_to :p_parent, polymorphic: true
+end
