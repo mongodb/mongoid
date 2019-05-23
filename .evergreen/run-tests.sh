adb2407dc7b5db5a144d4ecafd378d4b51e9e3e9
Oleg Pudeyev <oleg@bsdpower.com>
2019-05-23 16:07:18 -0400

    Sync evergreen configuration and gemfiles from master to fix failures against rails 6

--- .evergreen/run-tests.sh
+++ .evergreen/run-tests.sh
@@ -13,41 +13,96 @@ MONGODB_URI=${MONGODB_URI:-}
 export CI=evergreen
 export JRUBY_OPTS="--server -J-Xms512m -J-Xmx1G"
 
-source ~/.rvm/scripts/rvm
+ls -l /opt
 
 # Necessary for jruby
-export JAVACMD=/opt/java/jdk8/bin/java
-export PATH=$PATH:/opt/java/jdk8/bin
-
-if [ "$RVM_RUBY" == "ruby-head" ]; then
-  rvm reinstall $RVM_RUBY
+# Use toolchain java if it exists
+if [ -f /opt/java/jdk8/bin/java ]; then
+  export JAVACMD=/opt/java/jdk8/bin/java
+  export PATH=$PATH:/opt/java/jdk8/bin
+fi
+  
+# ppc64le has it in a different place
+if test -z "$JAVACMD" && [ -f /usr/lib/jvm/java-1.8.0/bin/java ]; then
+  export JAVACMD=/usr/lib/jvm/java-1.8.0/bin/java
+  export PATH=$PATH:/usr/lib/jvm/java-1.8.0/bin
 fi
 
-# Don't errexit because this may call scripts which error
-set +o errexit
-rvm use $RVM_RUBY
-set -o errexit
+if test "$RVM_RUBY" = "ruby-head"; then
+  # 12.04, 14.04 and 16.04 are good
+  wget -O ruby-head.tar.bz2 http://rubies.travis-ci.org/ubuntu/`lsb_release -rs`/x86_64/ruby-head.tar.bz2
+  tar xf ruby-head.tar.bz2
+  export PATH=`pwd`/ruby-head/bin:`pwd`/ruby-head/lib/ruby/gems/2.6.0/bin:$PATH
+  ruby --version
+  ruby --version |grep dev
+  
+  #rvm reinstall $RVM_RUBY
+else
+  if true; then
+  
+  # For testing toolchains:
+  toolchain_url=https://s3.amazonaws.com//mciuploads/mongo-ruby-toolchain/ubuntu1404/8cd47ac2cf636710740a6d79167f055e4c0a0154/mongo_ruby_driver_toolchain_ubuntu1404_patch_8cd47ac2cf636710740a6d79167f055e4c0a0154_5c452b76e3c3312273591db4_19_01_21_02_16_23.tar.gz
+  curl -fL $toolchain_url |tar zxf -
+  export PATH=`pwd`/rubies/$RVM_RUBY/bin:$PATH
+  
+  else
+  
+  # Normal operation
+  if ! test -d $HOME/.rubies/$RVM_RUBY/bin; then
+    echo "Ruby directory does not exist: $HOME/.rubies/$RVM_RUBY/bin" 1>&2
+    echo "Contents of /opt:" 1>&2
+    ls -l /opt 1>&2 || true
+    echo ".rubies symlink:" 1>&2
+    ls -ld $HOME/.rubies 1>&2 || true
+    echo "Our rubies:" 1>&2
+    ls -l $HOME/.rubies 1>&2 || true
+    exit 2
+  fi
+  export PATH=$HOME/.rubies/$RVM_RUBY/bin:$PATH
+  
+  fi
+  
+  ruby --version
 
-# Ensure we're using the right ruby
-python - <<EOH
+  # Ensure we're using the right ruby
+  python - <<EOH
 ruby = "${RVM_RUBY}".split("-")[0]
 version = "${RVM_RUBY}".split("-")[1]
 assert(ruby in "`ruby --version`")
 assert(version in "`ruby --version`")
 EOH
 
-echo 'updating rubygems'
-gem update --system
+  # We shouldn't need to update rubygems, and there is value in
+  # testing on whatever rubygems came with each supported ruby version
+  #echo 'updating rubygems'
+  #gem update --system
 
-gem install bundler
+  # Only install bundler when not using ruby-head.
+  # ruby-head comes with bundler and gem complains
+  # because installing bundler would overwrite the bundler binary
+  gem install bundler -v '<2'
+fi
 
+echo "We are in `pwd`"
 
-if [ $DRIVER == "master" ]; then
+if test "$DRIVER" = "master"; then
   bundle install --gemfile=gemfiles/driver_master.gemfile
   BUNDLE_GEMFILE=gemfiles/driver_master.gemfile bundle exec rake spec
-elif [ $RAILS == "master" ]; then
-  bundle install --gemfile=gemfiles/rails_master.gemfile
-  BUNDLE_GEMFILE=gemfiles/rails_master.gemfile bundle exec rake spec
+elif test "$DRIVER" = "stable"; then
+  bundle install --gemfile=gemfiles/driver_stable.gemfile
+  BUNDLE_GEMFILE=gemfiles/driver_stable.gemfile bundle exec rake spec
+elif test "$DRIVER" = "stable-jruby"; then
+  bundle install --gemfile=gemfiles/driver_stable_jruby.gemfile
+  BUNDLE_GEMFILE=gemfiles/driver_stable_jruby.gemfile bundle exec rake spec
+elif test "$RAILS" = "master-jruby"; then
+  bundle install --gemfile=gemfiles/rails_master_jruby.gemfile
+  BUNDLE_GEMFILE=gemfiles/rails_master_jruby.gemfile bundle exec rake spec
+elif test -n "$RAILS"; then
+  bundle install --gemfile=gemfiles/rails_"$RAILS".gemfile
+  BUNDLE_GEMFILE=gemfiles/rails_"$RAILS".gemfile bundle exec rake spec
+elif test "$I18N" = "1.0"; then
+  bundle install --gemfile=gemfiles/i18n-1.0.gemfile
+  BUNDLE_GEMFILE=gemfiles/i18n-1.0.gemfile bundle exec rake spec
 else
   bundle install
   bundle exec rake spec
--- Gemfile
+++ Gemfile
@@ -5,11 +5,33 @@ gem 'rake'
 gem 'actionpack', '~> 5.1'
 gem 'activemodel', '~> 5.1'
 
+# https://jira.mongodb.org/browse/MONGOID-4614
+if RUBY_VERSION < '2.3'
+  gem 'i18n', '~> 1.0', '>= 1.1', '< 1.5'
+  # nokogiri does not support 2.2 anymore.
+  # https://github.com/sparklemotion/nokogiri/issues/1841
+  # We are getting it as a transitive dependency
+  gem 'nokogiri', '<1.10'
+else
+  gem 'i18n', '~> 1.0', '>= 1.1'
+end
 
 group :test do
+  gem 'rspec-retry'
   gem 'benchmark-ips'
   gem 'rspec', '~> 3.7'
   gem 'rspec-expectations', '~> 3.7', '<= 3.8.2'
   #gem 'rspec-expectations', git: 'https://github.com/p-mongo/rspec-expectations', ref: 'pr-1112-3.8'
   gem 'fuubar'
+  gem 'rfc'
+  platforms :mri do
+    gem 'timeout-interrupt'
+  end
+end
+
+group :development, :testing do
+  gem 'yard'
+  platforms :mri do
+    gem 'byebug'
+  end
 end
