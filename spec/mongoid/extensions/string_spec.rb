18ff3352537be45d1c278a1542c074d8598e6a2d
Patryk Kotarski <patryk.kotarski@iubenda.com>
2019-05-29 12:03:32 +0200

    Initial commmit

--- spec/mongoid/extensions/string_spec.rb
+++ spec/mongoid/extensions/string_spec.rb
@@ -309,50 +309,78 @@ describe Mongoid::Extensions::String do
 
     context "when the string is an integer" do
 
-      it "returns true" do
+      it "is numeric" do
         expect("1234").to be_numeric
+      end
+
+      it "returns true" do
+        expect("1234".numeric?).to eq(true)
       end
     end
 
     context "when string is a float" do
 
-      it "returns true" do
+      it "is numeric" do
         expect("1234.123").to be_numeric
+      end
+
+      it "returns true" do
+        expect("1234.123".numeric?).to eq(true)
       end
     end
 
     context "when the string is has exponents" do
 
-      it "returns true" do
+      it "is numeric" do
         expect("1234.123123E4").to be_numeric
+      end
+
+      it "returns true" do
+        expect("1234.123123E4".numeric?).to eq(true)
       end
     end
 
     context "when the string is non numeric" do
 
-      it "returns false" do
+      it "is not numeric" do
         expect("blah").to_not be_numeric
+      end
+
+      it "returns false" do
+        expect("blah".numeric?).to eq(false)
       end
     end
 
     context "when the string is NaN" do
 
-      it "returns true" do
+      it "is numeric" do
         expect("NaN").to be_numeric
+      end
+
+      it "returns true" do
+        expect("NaN".numeric?).to eq(true)
       end
     end
 
     context "when the string is Infinity" do
 
-      it "returns true" do
+      it "is numeric" do
         expect("Infinity").to be_numeric
+      end
+
+      it "returns true" do
+        expect("Infinity".numeric?).to eq(true)
       end
     end
 
     context "when the string is -Infinity" do
 
-      it "returns true" do
+      it "is numeric" do
         expect("-Infinity").to be_numeric
+      end
+
+      it "returns true" do
+        expect("-Infinity".numeric?).to eq(true)
       end
     end
   end
