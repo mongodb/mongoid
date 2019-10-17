f6228fca1cf2e69b78670d7f6a87a548b2d1266c
Oleg Pudeyev <oleg@bsdpower.com>
2019-10-17 16:17:10 -0400

    Expand ElemMatch docstring

--- lib/mongoid/matchable/elem_match.rb
+++ lib/mongoid/matchable/elem_match.rb
@@ -6,14 +6,24 @@ module Mongoid
 
     class ElemMatch < Default
 
-      # Return true if a given predicate matches a sub document entirely
+      # Returns true the attribute is an array with at least one element
+      # that matches the condition in the value.
+      #
+      # Value must be a one-element hash with the key of $elemMatch
+      # (string or symbol), and the value being an $elemMatch condition, as
+      # specified in
+      # https://docs.mongodb.com/manual/reference/operator/query/elemMatch/.
+      # In particular, the condition can be a hash of key-value pairs of
+      # literal values which the attribute's elements will be directly
+      # compared to, or condition can be a complex condition containing e.g.
+      # $and/$or/$nor/$not logical operators.
       #
       # @example Do the values match?
       #   matcher._matches?({"$elemMatch" => {"a" => 1, "b" => 2}})
       #
       # @param [ Hash ] value The values to check.
       #
-      # @return [ true, false ] If the values match.
+      # @return [ true, false ] If the attribute matches the value.
       def _matches?(value)
         condition = condition_value(value, '$elemMatch')
 
