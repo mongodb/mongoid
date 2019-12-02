1483fddb57a1ed28a37d49e0f8beb31977d44806
Oleg Pudeyev <39304720+p-mongo@users.noreply.github.com>
2019-12-02 15:38:21 -0500

    Make reset_strategies! return nil as documented (rather than a [nil, nil] array) (#4696)

--- lib/mongoid/criteria/queryable/mergeable.rb
+++ lib/mongoid/criteria/queryable/mergeable.rb
@@ -45,7 +45,7 @@ module Mongoid
           use(:__union__)
         end
 
-        # Reset the stratgies to nil, used after cloning.
+        # Clear the current strategy and negating flag, used after cloning.
         #
         # @example Reset the strategies.
         #   mergeable.reset_strategies!
@@ -54,7 +54,8 @@ module Mongoid
         #
         # @since 1.0.0
         def reset_strategies!
-          self.strategy, self.negating = nil, nil
+          self.strategy = nil
+          self.negating = nil
         end
 
         private
