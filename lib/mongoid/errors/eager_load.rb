cd844a9f5726a6be63e1b849f9aee927941beabf
Oleg Pudeyev <oleg@bsdpower.com>
2020-04-19 21:29:38 -0400

    reinstate exceptions for backwards compatibility

---
+++ lib/mongoid/errors/eager_load.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+# encoding: utf-8
+
+module Mongoid
+  module Errors
+
+    # This error is raised when attempting to eager load a many to many
+    # association.
+    class EagerLoad < MongoidError
+
+      # Create the new eager load error.
+      #
+      # @example Create the new eager load error.
+      #   EagerLoad.new(:preferences)
+      #
+      # @param [ Symbol ] name The name of the association.
+      #
+      # @since 2.2.0
+      def initialize(name)
+        super(compose_message("eager_load", { name: name }))
+      end
+    end
+  end
+end
---
+++ spec/mongoid/errors/eager_load_spec.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+# encoding: utf-8
+
+require "spec_helper"
+
+describe Mongoid::Errors::EagerLoad do
+
+  describe "#message" do
+
+    let(:error) do
+      described_class.new(:ratable)
+    end
+
+    it "contains the problem in the message" do
+      expect(error.message).to include(
+        "Eager loading :ratable is not supported since it is a polymorphic"
+      )
+    end
+
+    it "contains the summary in the message" do
+      expect(error.message).to include(
+        "Mongoid cannot currently determine the classes it needs to eager"
+      )
+    end
+
+    it "contains the resolution in the message" do
+      expect(error.message).to include(
+        "Don't attempt to perform this action and have patience"
+      )
+    end
+  end
+end
