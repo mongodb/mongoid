a403f11c907287fe2a2eb5d95070ffa268576604
Oleg Pudeyev <oleg@bsdpower.com>
2020-05-15 02:29:03 -0400

    Fix MONGOID-4882 Embedded documents not setting parent relation if declared with a class_name having "::" prefix

--- spec/integration/associations/embedded_spec.rb
+++ spec/integration/associations/embedded_spec.rb
@@ -3,6 +3,7 @@
 
 require 'spec_helper'
 require_relative '../../mongoid/association/embedded/embeds_many_models'
+require_relative '../../mongoid/association/embedded/embeds_one_models'
 
 describe 'embedded associations' do
   context 'without default order' do
@@ -116,6 +117,59 @@ describe 'embedded associations' do
       end
 
       it_behaves_like 'an embedded association'
+    end
+  end
+
+  describe 'parent association' do
+    let(:parent) do
+      parent_cls.new
+    end
+
+    context 'embeds_one' do
+
+      shared_examples 'is set' do
+        it 'is set' do
+          parent.child = child_cls.new
+          parent.child.parent.should == parent
+        end
+      end
+
+      context 'class_name set without leading ::' do
+        let(:parent_cls) { EomParent }
+        let(:child_cls) { EomChild }
+
+        it_behaves_like 'is set'
+      end
+
+      context 'class_name set with leading ::' do
+        let(:parent_cls) { EomCcParent }
+        let(:child_cls) { EomCcChild }
+
+        it_behaves_like 'is set'
+      end
+    end
+
+    context 'embeds_many' do
+
+      let(:child) { parent.legislators.new }
+
+      shared_examples 'is set' do
+        it 'is set' do
+          child.congress.should == parent
+        end
+      end
+
+      context 'class_name set without leading ::' do
+        let(:parent_cls) { EmmCongress }
+
+        it_behaves_like 'is set'
+      end
+
+      context 'class_name set with leading ::' do
+        let(:parent_cls) { EmmCcCongress }
+
+        it_behaves_like 'is set'
+      end
     end
   end
 end
