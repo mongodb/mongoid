c15df4f5fbd5ac8b91640b268f397fc85ac949f5
Neil Shweky <neilshweky@gmail.com>
2020-07-20 13:38:59 -0400

    MONGOID-4933 fix indentation

--- lib/mongoid/errors/invalid_discriminator_key.rb
+++ lib/mongoid/errors/invalid_discriminator_key.rb
@@ -2,29 +2,28 @@
 # encoding: utf-8
 
 module Mongoid
-    module Errors
-  
-      # This error is raised when trying to create a global discriminator key that 
-      # conflicts with an already defined method.
-      class InvalidDiscriminatorKey < MongoidError
-  
-        # Create the new error.
-        #
-        # @example Create the error.
-        #   InvalidDiscriminatorKey.new(:invalid)
-        #
-        # @param [ Symbol ] name The method name.
-        def initialize(name)
-          super(
-            compose_message(
-              "invalid_discriminator_key",
-              {
-                name: name
-              }
-            )
+  module Errors
+
+    # This error is raised when trying to create a global discriminator key that 
+    # conflicts with an already defined method.
+    class InvalidDiscriminatorKey < MongoidError
+
+      # Create the new error.
+      #
+      # @example Create the error.
+      #   InvalidDiscriminatorKey.new(:invalid)
+      #
+      # @param [ Symbol ] name The method name.
+      def initialize(name)
+        super(
+          compose_message(
+            "invalid_discriminator_key",
+            {
+              name: name
+            }
           )
-        end
+        )
       end
     end
   end
-  
+end
