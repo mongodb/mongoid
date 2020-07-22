eb6dc55b6d9facb03f6410a157322aa63f638e9b
Oleg Pudeyev <39304720+p-mongo@users.noreply.github.com>
2020-07-22 10:26:17 -0400

    MONGOID-4929 Test passing $options to $regex when embedded matching (#4825)
    Co-authored-by: Oleg Pudeyev <oleg@bsdpower.com>

--- lib/mongoid/matcher/field_expression.rb
+++ lib/mongoid/matcher/field_expression.rb
@@ -8,7 +8,33 @@ module Mongoid
           condition.all? do |k, cond_v|
             k = k.to_s
             if k.start_with?('$')
-              FieldOperator.get(k).matches?(exists, value, cond_v)
+              if %w($regex $options).include?(k)
+                unless condition.key?('$regex')
+                  raise Errors::InvalidQuery, "$regex is required if $options is given: #{Errors::InvalidQuery.truncate_expr(condition)}"
+                end
+
+                if k == '$regex'
+                  if options = condition['$options']
+                    cond_v = case cond_v
+                    when Regexp
+                      BSON::Regexp::Raw.new(cond_v.source, options)
+                    when BSON::Regexp::Raw
+                      BSON::Regexp::Raw.new(cond_v.pattern, options)
+                    else
+                      BSON::Regexp::Raw.new(cond_v, options)
+                    end
+                  elsif String === cond_v
+                    cond_v = BSON::Regexp::Raw.new(cond_v)
+                  end
+
+                  FieldOperator.get(k).matches?(exists, value, cond_v)
+                else
+                  # $options are matched as part of $regex
+                  true
+                end
+              else
+                FieldOperator.get(k).matches?(exists, value, cond_v)
+              end
             elsif Hash === value
               sub_exists, sub_value, expanded =
                 Matcher.extract_attribute(value, k)
