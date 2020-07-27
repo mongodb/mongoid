f12846cab8de1c8f52f4c3563cb1df1ee70fc9f8
Oleg Pudeyev <oleg@bsdpower.com>
2020-07-27 18:30:39 -0400

    MONGOID-4893 Add Range tests for embedded matchers

--- lib/mongoid/matcher/lte.rb
+++ lib/mongoid/matcher/lte.rb
@@ -4,6 +4,10 @@ module Mongoid
     # @api private
     module Lte
       module_function def matches?(exists, value, condition)
+        case condition
+        when Range
+          raise Errors::InvalidQuery, "$lte argument cannot be a Range: #{Errors::InvalidQuery.truncate_expr(condition)}"
+        end
         FieldOperator.apply_array_field_operator(exists, value, condition) do |v|
           FieldOperator.apply_comparison_operator(:<=, v, condition)
         end
