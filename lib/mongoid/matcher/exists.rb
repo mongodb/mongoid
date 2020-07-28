b9acd267f4eaeb6a7c1cb742007396087d481be5
Oleg Pudeyev <39304720+p-mongo@users.noreply.github.com>
2020-07-28 10:23:55 -0400

    MONGOID-4893 Add Range tests for embedded matchers (#4840)
    Co-authored-by: Oleg Pudeyev <oleg@bsdpower.com>

--- lib/mongoid/matcher/exists.rb
+++ lib/mongoid/matcher/exists.rb
@@ -4,6 +4,10 @@ module Mongoid
     # @api private
     module Exists
       module_function def matches?(exists, value, condition)
+        case condition
+        when Range
+          raise Errors::InvalidQuery, "$exists argument cannot be a Range: #{Errors::InvalidQuery.truncate_expr(condition)}"
+        end
         exists == (condition || false)
       end
     end
--- lib/mongoid/matcher/gte.rb
+++ lib/mongoid/matcher/gte.rb
@@ -4,6 +4,10 @@ module Mongoid
     # @api private
     module Gte
       module_function def matches?(exists, value, condition)
+        case condition
+        when Range
+          raise Errors::InvalidQuery, "$gte argument cannot be a Range: #{Errors::InvalidQuery.truncate_expr(condition)}"
+        end
         FieldOperator.apply_array_field_operator(exists, value, condition) do |v|
           FieldOperator.apply_comparison_operator(:>=, v, condition)
         end
