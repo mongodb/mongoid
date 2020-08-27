d87a35607969dee687f5ffa7f26271bb05814dc0
Oleg Pudeyev <39304720+p-mongo@users.noreply.github.com>
2020-08-27 10:56:01 -0400

    Fix MONGOID-4980 Atomic push incorrectly determines whether there is a conflict (#4869)
    * Currently, Mongoid::Atomic::Modifiers#push_conflict? has a regex which is too loose. The regex should match exactly the first segment of a key.
    
    addresses <--> addresses.0.street should match
    addresses <--> dresses should not match
    
    * make the test into an integration test
    
    * cover all operations
    
    * redo the fix without regular expressions
    
    Co-authored-by: shields <shields@tablecheck.com>
    Co-authored-by: Oleg Pudeyev <oleg@bsdpower.com>

---
+++ spec/integration/atomic/modifiers_spec.rb
@@ -0,0 +1,117 @@
+# frozen_string_literal: true
+# encoding: utf-8
+
+require "spec_helper"
+
+describe Mongoid::Atomic::Modifiers do
+
+  let(:modifiers) do
+    described_class.new
+  end
+
+  context 'when performing multiple operations with similar keys' do
+
+    let(:pushes) do
+      { "addresses.0.locations" => { "street" => "Bond St" } }
+    end
+
+    let(:similar1) do
+      { "dresses" => { "color" => "red" } }
+    end
+
+    let(:similar2) do
+      { "ses.0.foo" => { "baz" => "qux" } }
+    end
+
+    before do
+      modifiers.send(operation, pushes)
+      modifiers.send(operation, similar1)
+      modifiers.send(operation, similar2)
+    end
+
+    context "push" do
+      let(:operation) { :push }
+
+      it "adds all modifiers to top level" do
+        expect(modifiers).to eq({ "$push" => {
+                                    "addresses.0.locations" => { '$each' => [{ "street" => "Bond St" }] },
+                                    "dresses" => { '$each' => [{ "color" => "red" }] },
+                                    "ses.0.foo" => { '$each' => [{ "baz" => "qux" }] }
+                                 } })
+      end
+    end
+
+    context "pull" do
+      let(:operation) { :pull }
+
+      it "adds all modifiers to top level" do
+        expect(modifiers).to eq({ "$pull" => {
+                                    "addresses.0.locations" => { "street" => "Bond St" },
+                                    "dresses" => { "color" => "red" },
+                                    "ses.0.foo" => { "baz" => "qux" },
+                                 } })
+      end
+    end
+
+    context "pull_all" do
+      let(:operation) { :pull_all }
+
+      it "adds all modifiers to top level" do
+        expect(modifiers).to eq({ "$pullAll" => {
+                                    "addresses.0.locations" => { "street" => "Bond St" },
+                                    "dresses" => { "color" => "red" },
+                                    "ses.0.foo" => { "baz" => "qux" },
+                                 } })
+      end
+    end
+
+    context "add_to_set" do
+      let(:operation) { :add_to_set }
+
+      it "adds all modifiers to top level" do
+        expect(modifiers).to eq({ "$addToSet" => {
+                                    "addresses.0.locations" => { '$each' => { "street" => "Bond St" } },
+                                    "dresses" => { '$each' => { "color" => "red" } },
+                                    "ses.0.foo" => { '$each' => { "baz" => "qux" } }
+                                 } })
+      end
+    end
+
+    context "set" do
+      let(:operation) { :set }
+
+      it "adds all modifiers to top level" do
+        expect(modifiers).to eq({ "$set" => {
+                                    "addresses.0.locations" => { "street" => "Bond St" },
+                                    "dresses" => { "color" => "red" },
+                                    "ses.0.foo" => { "baz" => "qux" },
+                                 } })
+      end
+    end
+
+    context "unset" do
+      let(:operation) { :unset }
+
+      let(:pushes) do
+        [:foobar]
+      end
+
+      let(:similar1) do
+        [:foo]
+      end
+
+      let(:similar2) do
+        [:bar]
+      end
+
+      it "adds all modifiers to top level" do
+        expect(modifiers).to eq("$unset" => {
+          foobar: true,
+          foo: true,
+          bar: true,
+        })
+      end
+    end
+  end
+
+end
