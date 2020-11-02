3e8b5b6453375f715ba2fcdd4952aa48430dc55e
Katie Sadoff <katie.sadoff@mongodb.com>
2020-11-02 09:08:33 -0500

    removed mongo id tests

--- spec/mongoid/extensions/stringified_symbol_spec.rb
+++ spec/mongoid/extensions/stringified_symbol_spec.rb
@@ -44,37 +44,6 @@ describe Mongoid::StringifiedSymbol do
     end
   end
 
-  describe "#mongoid_id?" do
-
-    context "when the string is id" do
-
-      it "returns true" do
-        expect(:id).to be_mongoid_id
-      end
-    end
-
-    context "when the string is _id" do
-
-      it "returns true" do
-        expect(:_id).to be_mongoid_id
-      end
-    end
-
-    context "when the string contains id" do
-
-      it "returns false" do
-        expect(:identity).to_not be_mongoid_id
-      end
-    end
-
-    context "when the string contains _id" do
-
-      it "returns false" do
-        expect(:something_id).to_not be_mongoid_id
-      end
-    end
-  end
-
   describe ".mongoize" do
 
     context "when the object is not a symbol" do
