e07153902a9a5b5845d8f65be441d90341bef8a1
Katie Sadoff <katie.sadoff@mongodb.com>
2020-11-18 16:38:34 -0500

    changed comments

--- lib/mongoid/matcher/bits_any_clear.rb
+++ lib/mongoid/matcher/bits_any_clear.rb
@@ -10,15 +10,15 @@ module Mongoid
         end
         case condition
           when Array
-          # array of bits
             condition.any? do |c|
               value & (1<<c) == 0
             end
         when BSON::Binary
           int_cond = condition.data.split('').map { |n| '%02x' % n.ord }.join.to_i(16)
+          # either all clear or not (all set and all clear)
           (value & int_cond == 0) || (!(value & int_cond == int_cond) && (value & int_cond > 0))
         when Integer
-          # todo: simplify
+          # either all clear or not (all set and all clear)
           (value & condition == 0) || (!(value & condition == condition) && (value & condition > 0))
         else
           raise Errors::InvalidQuery, "Unknown $bitsAllClear argument #{condition}"
