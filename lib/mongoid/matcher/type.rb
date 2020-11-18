b0c6625182435cf9b11572c9760435d4bea14396
Katie Sadoff <katie.sadoff@mongodb.com>
2020-11-18 12:28:35 -0500

    added all tests except for Date and Decimal128

--- lib/mongoid/matcher/type.rb
+++ lib/mongoid/matcher/type.rb
@@ -15,16 +15,16 @@ module Mongoid
           #TODO: MATCH BSON TYPES
 
           # Double
-          Float === value
+          Float === value || BSON::Float === value
         when 2
           # String
-          String === value
+          String === value || BSON::String === value
         when 3
           # Object
-          Object === value
+          Object === value || BSON::Object === value
         when 4
           # Array
-          Array === value
+          Array === value || BSON::Array === value
         when 5
           # Binary data
           BSON::Binary === value
@@ -36,34 +36,34 @@ module Mongoid
           BSON::ObjectId === value
         when 8
           # Boolean
-          TrueClass === value || FalseClass === value
+          TrueClass === value || FalseClass === value || BSON::Boolean === value
         when 9
           # Date
-          Date === value
+          BSON::Date === value
         when 10
           # Null
-          NilClass === value
+          BSON::NilClass === value
         when 11
           # Regex
           Regexp::Raw === value || ::Regexp === value
         when 12
           # DBPointer deprecated
-          DBPointer === value
+          BSON::DbPointer === value
         when 13
           # JavaScript
-          Code === value
+          BSON::Code === value
         when 14
           # Symbol deprecated
           Symbol === value || BSON::Symbol::Raw === value
         when 15
           # Javascript with code deprecated
-          CodeWithScope === value
+          BSON::CodeWithScope === value
         when 16
           # 32-bit int
           BSON::Int32 === value
         when 17
           # Timestamp
-          Time === value
+          BSON::Timestamp === value
         when 18
           # Long
           BSON::Int64 === value
