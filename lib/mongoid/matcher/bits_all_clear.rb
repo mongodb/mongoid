21f125ad2a43335ac46e8515f2eb6b6c0fd96129
Oleg Pudeyev <oleg@bsdpower.com>
2020-11-20 17:31:50 -0500

    DRY remaining bit operators

--- lib/mongoid/matcher/bits_all_clear.rb
+++ lib/mongoid/matcher/bits_all_clear.rb
@@ -3,25 +3,17 @@ module Mongoid
 
     # @api private
     module BitsAllClear
-      module_function def matches?(exists, value, condition)
-        case value
-        when BSON::Binary
-          value = value.data.split('').map { |n| '%02x' % n.ord }.join.to_i(16)
+      include Bits
+      extend self
+
+      def array_matches?(value, condition)
+        condition.all? do |c|
+          value & (1<<c) == 0
         end
-        case condition
-        when Array
-          condition.all? do |c|
-            value & (1<<c) == 0
-          end
-        when BSON::Binary
-          #condition is a binary string
-          int_cond = condition.data.split('').map { |n| '%02x' % n.ord }.join.to_i(16)
-          value & int_cond == 0
-        when Integer
-          value & condition == 0
-        else
-          raise Errors::InvalidQuery, "Unknown $bitsAllClear argument #{condition}"
-        end
+      end
+
+      def int_matches?(value, condition)
+        value & condition == 0
       end
     end
   end
--- lib/mongoid/matcher/bits_any_clear.rb
+++ lib/mongoid/matcher/bits_any_clear.rb
@@ -3,26 +3,18 @@ module Mongoid
 
     # @api private
     module BitsAnyClear
-      module_function def matches?(exists, value, condition)
-        case value
-        when BSON::Binary
-          value = value.data.split('').map { |n| '%02x' % n.ord }.join.to_i(16)
+      include Bits
+      extend self
+
+      def array_matches?(value, condition)
+        condition.any? do |c|
+          value & (1<<c) == 0
         end
-        case condition
-          when Array
-            condition.any? do |c|
-              value & (1<<c) == 0
-            end
-        when BSON::Binary
-          int_cond = condition.data.split('').map { |n| '%02x' % n.ord }.join.to_i(16)
-          # either all clear or not (all set and all clear)
-          (value & int_cond == 0) || (!(value & int_cond == int_cond) && (value & int_cond > 0))
-        when Integer
-          # either all clear or not (all set and all clear)
-          (value & condition == 0) || (!(value & condition == condition) && (value & condition > 0))
-        else
-          raise Errors::InvalidQuery, "Unknown $bitsAllClear argument #{condition}"
-        end
+      end
+
+      def int_matches?(value, condition)
+        # either all clear or not (all set and all clear)
+        (value & condition == 0) || (!(value & condition == condition) && (value & condition > 0))
       end
     end
   end
