9d3264c2716188a840207007d4d37e51165fe9bd
Oleg Pudeyev <oleg@bsdpower.com>
2020-11-20 18:10:33 -0500

    fix the comment

--- lib/mongoid/matcher/bits_any_clear.rb
+++ lib/mongoid/matcher/bits_any_clear.rb
@@ -13,8 +13,9 @@ module Mongoid
       end
 
       def int_matches?(value, condition)
-        # either all clear or not (all set and all clear)
-        (value & condition == 0) || (!(value & condition == condition) && (value & condition > 0))
+        # either all clear or not (all set and any set)
+        (value & condition == 0) ||
+          (!(value & condition == condition) && (value & condition > 0))
       end
     end
   end
