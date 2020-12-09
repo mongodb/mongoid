41c155fb0201de6563f189e9527d0e5d41185c2d
Oleg Pudeyev <oleg@bsdpower.com>
2020-12-09 16:58:28 -0500

    revert removed test code

--- spec/mongoid/persistable/savable_spec.rb
+++ spec/mongoid/persistable/savable_spec.rb
@@ -270,12 +270,30 @@ describe Mongoid::Persistable::Savable do
           truck.crates.first.toys.build(name: "Teddy bear")
           truck.crates.build(volume: 0.8)
 
+          # The following is equivalent to the two lines above:
+          #
+          # truck.crates_attributes = {
+          #   '0' => {
+          #     "toys_attributes" => {
+          #       "0" => {
+          #         "name" => "Teddy bear"
+          #       }
+          #     },
+          #     "id" => crate.id.to_s
+          #   },
+          #   "1" => {
+          #     "volume" => 0.8
+          #   }
+          # }
+
           expect(truck.crates.size).to eq 2
           expect(truck.crates[0].volume).to eq 0.4
           expect(truck.crates[0].toys.size).to eq 1
           expect(truck.crates[0].toys[0].name).to eq "Teddy bear"
           expect(truck.crates[1].volume).to eq 0.8
           expect(truck.crates[1].toys.size).to eq 0
+
+          #expect(truck.atomic_updates[:conflicts]).to eq nil
 
           expect { truck.save! }.not_to raise_error
 
