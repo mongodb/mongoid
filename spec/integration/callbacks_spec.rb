d6c9b32e53ac8fa52d13697aa8a16ba5655d34cd
Oleg Pudeyev <oleg@bsdpower.com>
2020-12-20 08:22:50 -0500

    MONGOID-4476 update test

--- spec/integration/callbacks_spec.rb
+++ spec/integration/callbacks_spec.rb
@@ -6,77 +6,146 @@ require_relative './callbacks_models'
 
 describe 'callbacks integration tests' do
   context 'when modifying attributes in a callback' do
-    context 'top level document' do
-      let(:instance) do
+
+    context 'when creating top-level document' do
+      context 'top level document' do
+        let(:instance) do
+          Galaxy.create!
+        end
+
+        it 'writes the attribute value into the model' do
+          instance.age.should == 100_000
+        end
+
+        it 'persists the attribute value' do
+          Galaxy.find(instance.id).age.should == 100_000
+        end
+      end
+
+      context 'embedded document' do
+        shared_examples 'persists the attribute value' do
+          it 'writes the attribute value into the model' do
+            instance.stars.first.age.should == 42_000
+          end
+
+          it 'persists the attribute value' do
+            Galaxy.find(instance.id).stars.first.age.should == 42_000
+          end
+        end
+
+        context 'set as a document instance' do
+          let(:instance) do
+            Galaxy.create!(stars: [Star.new])
+          end
+
+          include_examples 'persists the attribute value'
+        end
+
+        context 'set as attributes on parent' do
+          let(:instance) do
+            Galaxy.create!(stars: [{}])
+          end
+
+          include_examples 'persists the attribute value'
+        end
+      end
+
+      context 'nested embedded document' do
+        shared_examples 'persists the attribute value' do
+          it 'writes the attribute value into the model' do
+            instance.stars.first.planets.first.age.should == 2_000
+          end
+
+          it 'persists the attribute value' do
+            Galaxy.find(instance.id).stars.first.planets.first.age.should == 2_000
+          end
+        end
+
+        context 'set as a document instance' do
+          let(:instance) do
+            Galaxy.create!(stars: [Star.new(
+              planets: [Planet.new],
+            )])
+          end
+
+          include_examples 'persists the attribute value'
+        end
+
+        context 'set as a attributes on parent' do
+          let(:instance) do
+            Galaxy.create!(stars: [
+              planets: [{}],
+            ])
+          end
+
+          include_examples 'persists the attribute value'
+        end
+      end
+    end
+
+    context 'when updating top-level document' do
+      let!(:instance) do
         Galaxy.create!
       end
 
-      it 'writes the attribute value into the model' do
-        instance.age.should == 100_000
-      end
+      context 'embedded document' do
+        shared_examples 'persists the attribute value' do
+          it 'writes the attribute value into the model' do
+            instance.stars.first.age.should == 42_000
+          end
 
-      it 'persists the attribute value' do
-        Galaxy.find(instance.id).age.should == 100_000
-      end
-    end
-
-    context 'embedded document' do
-      shared_examples 'persists the attribute value' do
-        it 'writes the attribute value into the model' do
-          instance.stars.first.age.should == 42_000
+          it 'persists the attribute value' do
+            Galaxy.find(instance.id).stars.first.age.should == 42_000
+          end
         end
 
-        it 'persists the attribute value' do
-          Galaxy.find(instance.id).stars.first.age.should == 42_000
-        end
-      end
+        context 'set as a document instance' do
+          before do
+            instance.stars = [Star.new]
+            instance.save!
+          end
 
-      context 'set as a document instance' do
-        let(:instance) do
-          Galaxy.create!(stars: [Star.new])
-        end
-
-        include_examples 'persists the attribute value'
-      end
-
-      context 'set as attributes on parent' do
-        let(:instance) do
-          Galaxy.create!(stars: [{}])
+          include_examples 'persists the attribute value'
         end
 
-        include_examples 'persists the attribute value'
-      end
-    end
+        context 'set as attributes on parent' do
+          before do
+            instance.stars = [{}]
+            instance.save!
+          end
 
-    context 'nested embedded document' do
-      shared_examples 'persists the attribute value' do
-        it 'writes the attribute value into the model' do
-          instance.stars.first.planets.first.age.should == 2_000
-        end
-
-        it 'persists the attribute value' do
-          Galaxy.find(instance.id).stars.first.planets.first.age.should == 2_000
+          include_examples 'persists the attribute value'
         end
       end
 
-      context 'set as a document instance' do
-        let(:instance) do
-          Galaxy.create!(stars: [Star.new(
-            planets: [Planet.new],
-          )])
+      context 'nested embedded document' do
+        shared_examples 'persists the attribute value' do
+          it 'writes the attribute value into the model' do
+            instance.stars.first.planets.first.age.should == 2_000
+          end
+
+          it 'persists the attribute value' do
+            Galaxy.find(instance.id).stars.first.planets.first.age.should == 2_000
+          end
         end
 
-        include_examples 'persists the attribute value'
-      end
+        context 'set as a document instance' do
+          before do
+            instance.stars = [Star.new(planets: [Planet.new])]
+            instance.save!
+          end
 
-      context 'set as a attributes on parent' do
-        let(:instance) do
-          Galaxy.create!(stars: [
-            planets: [{}],
-          ])
+          include_examples 'persists the attribute value'
         end
 
-        include_examples 'persists the attribute value'
+        context 'set as a attributes on parent' do
+          before do
+            instance.stars = [planets: [{}]]
+            instance.save!
+          end
+
+          include_examples 'persists the attribute value'
+        end
       end
     end
   end
