c5863bbf9f63271b0e4b23e5e9955f809f0df982
Oleg Pudeyev <oleg@bsdpower.com>
2021-02-23 19:00:19 -0500

    Try building with more heap

--- .evergreen/functions.sh
+++ .evergreen/functions.sh
@@ -58,7 +58,7 @@ set_env_vars() {
   fi
   export CI=evergreen
   # JRUBY_OPTS were initially set for Mongoid
-  export JRUBY_OPTS="-J-Xms512m -J-Xmx1G"
+  export JRUBY_OPTS="-J-Xms512m -J-Xmx2G"
 }
 
 setup_ruby() {
