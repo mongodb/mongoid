a02971455910619c5d7a5ed327b7c6ff3529d62a
Dmitry Rybakov <dmitry.rybakov@mongodb.com>
2021-04-28 09:30:58 +0200

    MONGOID-4668 Add test for regexp field (#4984)
    If a document has a field wit type Regexp, it is still possible to assign strings to this field. In this case the field still should be persisted as regexp.

--- spec/support/models/mop.rb
+++ spec/support/models/mop.rb
@@ -13,4 +13,14 @@ class Mop
   # transformed differently based on the type of field being queried.
   field :int_field, type: Integer
   field :array_field, type: Array
+  field :date_field, type: Date
+  field :time_field, type: Time
+  field :datetime_field, type: DateTime
+  field :big_decimal_field, type: BigDecimal
+  field :decimal128_field, type: BSON::Decimal128
+  field :symbol_field, type: Symbol
+  field :bson_symbol_field, type: BSON::Symbol::Raw
+  field :regexp_field, type: Regexp
+  field :bson_regexp_field, type: BSON::Regexp::Raw
 end
+
