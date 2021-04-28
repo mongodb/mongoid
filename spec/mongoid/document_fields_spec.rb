a8e69364a545949f1afd5b02ec280654cc6292b9
Dmitry Rybakov <dmitry.rybakov@mongodb.com>
2021-04-28 09:30:58 +0200

    MONGOID-4668 Add test for regexp field (#4984)
    If a document has a field wit type Regexp, it is still possible to assign strings to this field. In this case the field still should be persisted as regexp.

--- spec/mongoid/document_fields_spec.rb
+++ spec/mongoid/document_fields_spec.rb
@@ -85,4 +85,30 @@ describe Mongoid::Document do
       end
     end
   end
+
+  context 'Regexp field' do
+    shared_examples "persists strings as regexp" do |type|
+      it 'persists strings as regexp' do
+        mop = Mop.create!(regexp_field: 'foo')
+        expect(mop.regexp_field).to be_a Regexp
+        expect(Mop.find(mop.id).regexp_field).to be_a BSON::Regexp::Raw
+        expect(
+          Mop.collection.find(
+            "_id" => mop.id,
+            "regexp_field" => { "$type" => type }
+          ).count
+        ).to be == 1
+      end
+    end
+
+    context "< 3.2" do
+      max_server_version("3.1")
+      include_examples "persists strings as regexp", 11
+    end
+
+    context ">= 3.2" do
+      min_server_version("3.2")
+      it_behaves_like "persists strings as regexp", "regex"
+    end
+  end
 end
