1a9fcd833693164cd248fe25874b0024628fc536
Johnny Shields <johnny.shields@gmail.com>
2021-08-23 22:18:10 +0900

    MONGOID-5099 Improve error when loading an empty or invalid config file (#5032)
    * Fixes MONGOID-5099
    
    Improves error when loading an empty or invalid file and adds specs to #load_yaml method. Note that certain errors (file missing, ERB, YAML parsing) bubble up as per existing behavior.
    
    This change is only intended to avoid an internal cryptic error happening within Mongoid's code (NoMethodError (undefined method `[]' for false:FalseClass). Please also note that this does not change the conditions under which an exception occurs, it just changes the class of the exception.
    
    * repair exception docstring
    
    * fix test setup
    
    * tweak exception message
    
    * raise a specific exception for the case of empty config file
    
    * exception constructors are private
    
    * fix the test
    
    Co-authored-by: shields <shields@tablecheck.com>
    Co-authored-by: Oleg Pudeyev <code@olegp.name>

--- spec/mongoid/config/environment_spec.rb
+++ spec/mongoid/config/environment_spec.rb
@@ -5,9 +5,19 @@ require "spec_helper"
 
 describe Mongoid::Config::Environment do
 
-  after(:all) do
-    Rails = RailsTemp
-    Object.send(:remove_const, :RailsTemp)
+  around do |example|
+    if defined?(Rails)
+      SavedRails = Rails
+      example.run
+      Object.send(:remove_const, :Rails) if defined?(Rails)
+      Rails = SavedRails
+      Object.send(:remove_const, :SavedRails)
+    else
+      example.run
+      if defined?(Rails)
+        Object.send(:remove_const, :Rails)
+      end
+    end
   end
 
   describe "#env_name" do
@@ -22,11 +32,6 @@ describe Mongoid::Config::Environment do
               def env; "production"; end
             end
           end
-        end
-
-        after do
-          RailsTemp = Rails
-          Object.send(:remove_const, :Rails)
         end
 
         it "returns the rails environment" do
@@ -83,6 +88,79 @@ describe Mongoid::Config::Environment do
         expect { described_class.env_name }.to raise_error(
           Mongoid::Errors::NoEnvironment
         )
+      end
+    end
+  end
+
+  describe "#load_yaml" do
+    let(:path) { 'mongoid.yml' }
+    let(:environment) {}
+    before { allow(Rails).to receive('env').and_return('test') }
+
+    subject { described_class.load_yaml(path, environment) }
+
+    context 'when file not found' do
+      let(:path) { 'not/a/valid/path'}
+
+      it { expect { subject }.to raise_error(Errno::ENOENT) }
+    end
+
+    context 'when file found' do
+      before do
+        allow(File).to receive(:new).with('mongoid.yml').and_return(StringIO.new(file_contents))
+      end
+
+      let(:file_contents) do
+        <<~FILE
+          test:
+            clients: ['test']
+          development:
+            clients: ['dev']
+        FILE
+      end
+
+      context 'when file cannot be parsed as YAML' do
+        let(:file_contents) { "*\nbad:%123abc" }
+
+        it { expect { subject }.to raise_error(Psych::SyntaxError) }
+      end
+
+      context 'when file contains ERB errors' do
+        let(:file_contents) { '<%= foo %>' }
+
+        it { expect { subject }.to raise_error(NameError) }
+      end
+
+      context 'when file is empty' do
+        let(:file_contents) { '' }
+
+        it { expect { subject }.to raise_error(Mongoid::Errors::EmptyConfigFile) }
+      end
+
+      context 'when file does not contain a YAML Hash object' do
+        let(:file_contents) { '["this", "is", "an", "array"]' }
+
+        it { expect { subject }.to raise_error(Mongoid::Errors::InvalidConfigFile) }
+      end
+
+      context 'when environment not specified' do
+        it 'uses the rails environment' do
+          is_expected.to eq("clients"=>["test"])
+        end
+      end
+
+      context 'when environment is specified' do
+        let(:environment) { 'development' }
+
+        it 'uses the specified environment' do
+          is_expected.to eq("clients"=>["dev"])
+        end
+      end
+
+      context 'when environment is missing' do
+        let(:environment) { 'staging' }
+
+        it { is_expected.to be_nil }
       end
     end
   end
