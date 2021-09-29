a64ff3c9546d8a09132129334081464f05e8724b
Dmitry Rybakov <dmitry.rybakov@mongodb.com>
2021-09-29 18:02:50 +0200

    Skip test

--- spec/mongoid/association/referenced/belongs_to/proxy_spec.rb
+++ spec/mongoid/association/referenced/belongs_to/proxy_spec.rb
@@ -321,6 +321,8 @@ describe Mongoid::Association::Referenced::BelongsTo::Proxy do
           end
 
           it 'applies the persistence options when saving the child' do
+            skip 'https://jira.mongodb.org/browse/MONGOID-5190'
+
             expect(other_people_count).to eq(1)
             expect(other_posts_count).to eq(1)
           end
