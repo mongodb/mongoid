83ea9a3190398b374a38a4c730d93ee8ac150884
Jonathan Hyman <jon@braze.com>
2021-10-28 10:59:00 -0400

    Else -> elsif for clarity.

--- lib/mongoid/matcher/eq_impl.rb
+++ lib/mongoid/matcher/eq_impl.rb
@@ -26,19 +26,17 @@ module Mongoid
           # When doing a comparison with Time objects, compare using millisecond precision
           if value.kind_of?(Time) && condition.kind_of?(Time)
             time_eq?(value, condition)
+          elsif value.is_a?(Array) && condition.kind_of?(Time)
+            value.map do |v|
+              if v.kind_of?(Time)
+                time_rounded_to_millis(v)
+              else
+                v
+              end
+            end.include?(time_rounded_to_millis(condition))
           else
-            if value.is_a?(Array) && condition.kind_of?(Time)
-              value.map do |v|
-                if v.kind_of?(Time)
-                  time_rounded_to_millis(v)
-                else
-                  v
-                end
-              end.include?(time_rounded_to_millis(condition))
-            else
-              value == condition ||
-              value.is_a?(Array) && value.include?(condition)
-            end
+            value == condition ||
+            value.is_a?(Array) && value.include?(condition)
           end
         end
       end
