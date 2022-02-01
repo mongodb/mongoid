d9a64defe2815d005aa47808a0a236125ee5852a
Oleg Pudeyev <39304720+p-mongo@users.noreply.github.com>
2022-02-01 10:52:20 -0500

    MONGOID-5215 Document that or/nor conditions make default scope one of the disjunction branches (#5135)
    Co-authored-by: Oleg Pudeyev <code@olegp.name>

--- spec/integration/criteria/default_scope_spec.rb
+++ spec/integration/criteria/default_scope_spec.rb
@@ -18,4 +18,55 @@ describe 'Criteria and default scope' do
       sort_options.keys.should == %w(name status)
     end
   end
+
+  context 'default scope + logical operator' do
+
+    context 'logical operator applied to a criteria' do
+      let(:base) { Appointment.where }
+
+      it 'has default scope' do
+        base.selector.should == {'active' => true}
+      end
+
+      context '.or' do
+        let(:criteria) do
+          base.or(timed: true)
+        end
+
+        it 'adds new condition in parallel to default scope conditions' do
+          criteria.selector.should == {'$or' => [
+            {'active' => true},
+            {'timed' => true},
+          ]}
+        end
+      end
+
+      context '.any_of' do
+        let(:criteria) do
+          base.any_of(timed: true)
+        end
+
+        it 'maintains default scope conditions' do
+          criteria.selector.should == {'active' => true, 'timed' => true}
+        end
+      end
+    end
+
+    context 'logical operator called on the class' do
+      let(:base) { Appointment }
+
+      context '.or' do
+        let(:criteria) do
+          base.or(timed: true)
+        end
+
+        it 'adds new condition in parallel to default scope conditions' do
+          criteria.selector.should == {'$or' => [
+            {'active' => true},
+            {'timed' => true},
+          ]}
+        end
+      end
+    end
+  end
 end
