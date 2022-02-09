3e527d017e2c0d19946bd9da7f65a79edeb26159
Neil Shweky <neilshweky@gmail.com>
2022-02-09 12:27:48 -0500

    Revert "MONGOID-???? - Remove BSON::ObjectId#as_json as we will defer to the bson-ruby gem implementation. (#5057)"
    This reverts commit e8aae8896403794285083205456489ff525dc298.

--- spec/mongoid/extensions_spec.rb
+++ spec/mongoid/extensions_spec.rb
@@ -2,6 +2,20 @@
 
 require "spec_helper"
 
+describe BSON::ObjectId do
+
+  describe "#as_json" do
+
+    let(:object_id) do
+      described_class.new
+    end
+
+    it "returns the $oid plus string" do
+      expect(object_id.as_json).to eq("$oid" => object_id.to_s)
+    end
+  end
+end
+
 describe BSON::Document do
 
   describe "#symbolize_keys" do
