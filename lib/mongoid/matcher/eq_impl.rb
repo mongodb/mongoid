acbeab4fdbc0d8e5b97c22b87d94b107bb48b83a
Neil Shweky <neilshweky@gmail.com>
2022-02-15 14:48:24 -0500

    MONGOID-5237 Add Feature Flag: _matches? should use millisecond precision when comparing Time objects (#5149)
    * MONGOID-5237 put feature flag in the code
    
    * MONGOID-5237 add docs and config tests
    
    * MONGOID-5237 fix comment
    
    * MONGOID-5237 refactor tests
    
    * MONGOID-5237 update docs again

--- lib/mongoid/matcher/eq_impl.rb
+++ lib/mongoid/matcher/eq_impl.rb
@@ -24,19 +24,24 @@ module Mongoid
 =end
         else
           # When doing a comparison with Time objects, compare using millisecond precision
-          if value.kind_of?(Time) && condition.kind_of?(Time)
-            time_eq?(value, condition)
-          elsif value.is_a?(Array) && condition.kind_of?(Time)
-            value.map do |v|
-              if v.kind_of?(Time)
-                time_rounded_to_millis(v)
-              else
-                v
-              end
-            end.include?(time_rounded_to_millis(condition))
-          else
+          if Mongoid.compare_time_by_ms
             value == condition ||
             value.is_a?(Array) && value.include?(condition)
+          else
+            if value.kind_of?(Time) && condition.kind_of?(Time)
+              time_eq?(value, condition)
+            elsif value.is_a?(Array) && condition.kind_of?(Time)
+              value.map do |v|
+                if v.kind_of?(Time)
+                  time_rounded_to_millis(v)
+                else
+                  v
+                end
+              end.include?(time_rounded_to_millis(condition))
+            else
+              value == condition ||
+              value.is_a?(Array) && value.include?(condition)
+            end
           end
         end
       end
