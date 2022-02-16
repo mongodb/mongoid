1a3ef6a011cf389d7291126ed9dcdc23e57606f1
Neil Shweky <neilshweky@gmail.com>
2022-02-16 09:29:19 -0500

    MONGOID-5234 Add Feature Flag: Remove BSON::ObjectId#as_json override (#5141)
    * Revert "MONGOID-???? - Remove BSON::ObjectId#as_json as we will defer to the bson-ruby gem implementation. (#5057)"
    
    This reverts commit e8aae8896403794285083205456489ff525dc298.
    
    * MONGOID-5234 add feature flag for as_json
    
    * MONGOID-5234 add docs for new feature flag
    
    * MONGOID-5234 remove irrelevant tests and add docstring
    
    * MONGOID-5234 add default to configuration docs
    
    * MONGOID-5234 use dots instead of ::
    
    * MONGOID-5234 put test back
    
    * MONGOID-5234 update documentation and flip the flag
    
    * MONGOID-5234 fix up as_json comment
    
    * MONGOID-5234 update release notes
    
    * MONGOID-5234 change method signature

--- spec/mongoid/extensions_spec.rb
+++ spec/mongoid/extensions_spec.rb
@@ -2,6 +2,48 @@
 
 require "spec_helper"
 
+describe BSON::ObjectId do
+
+  describe "#as_json" do
+
+    let(:object_id) do
+      described_class.new
+    end
+
+    context "when use_bson_ruby_as_json is set" do
+      around do |example|
+        saved_flag = Mongoid.use_bson_ruby_as_json
+        Mongoid.use_bson_ruby_as_json = true
+        begin
+          example.run
+        ensure
+          Mongoid.use_bson_ruby_as_json = saved_flag
+        end
+      end
+
+      it "uses bson-ruby's implementation of as_json" do
+        expect(object_id.as_json).to eq(object_id.bson_ruby_as_json)
+      end
+    end
+
+    context "when use_bson_ruby_as_json is not set" do
+      around do |example|
+        saved_flag = Mongoid.use_bson_ruby_as_json
+        Mongoid.use_bson_ruby_as_json = false
+        begin
+          example.run
+        ensure
+          Mongoid.use_bson_ruby_as_json = saved_flag
+        end
+      end
+
+      it "returns the $oid plus string" do
+        expect(object_id.as_json).to eq("$oid" => object_id.to_s)
+      end
+    end
+  end
+end
+
 describe BSON::Document do
 
   describe "#symbolize_keys" do
