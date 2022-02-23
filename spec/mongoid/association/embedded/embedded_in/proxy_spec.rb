5e5846587e434598123ee47b03e359ea47bd50b2
Neil Shweky <neilshweky@gmail.com>
2022-02-23 16:28:15 -0500

    MONGOID-5039 add embeds_many tests

--- spec/mongoid/association/embedded/embedded_in/proxy_spec.rb
+++ spec/mongoid/association/embedded/embedded_in/proxy_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "spec_helper"
+require_relative '../embeds_many_models'
 
 describe Mongoid::Association::Embedded::EmbeddedIn::Proxy do
 
@@ -507,7 +508,7 @@ describe Mongoid::Association::Embedded::EmbeddedIn::Proxy do
     end
   end
 
-  context "when the same class is embedded multiple times" do
+  context "when the same class is embedded multiple times; embeds_one" do
 
     let(:customer) do
       Customer.new
@@ -541,12 +542,70 @@ describe Mongoid::Association::Embedded::EmbeddedIn::Proxy do
 
     context "inverse assignment" do
 
-      it "correctly sets the association for the embedded class" do
-
+      it "raises an error when trying to set the association" do
         customer.work_address = CustomerAddress.new
 
         expect do
           customer.work_address.addressable = customer
+        end.to raise_error(Mongoid::Errors::AmbiguousRelationship)
+      end
+    end
+
+    context "when there is an explicit inverse_of" do
+
+      let(:customer) { EmmCustomer.new }
+
+      it "correctly sets the association for the embedded class" do
+        customer.work_address = EmmCustomerAddress.new
+
+        expect do
+          customer.work_address.addressable = customer
+        end.to_not raise_error
+
+        expect(customer.work_address.addressable).to eq(customer)
+      end
+    end
+  end
+
+  context "when the same class is embedded multiple times; embeds_many" do
+
+    let(:customer) do
+      EmmCustomer.new
+    end
+
+    context "assignment after saving" do
+
+      it "correctly sets the association for the embedded class" do
+
+        customer.close_friends = [EmmFriend.new]
+        customer.acquaintances = [EmmFriend.new]
+
+        expect(customer.close_friends._association.store_as).to eq("close_friends")
+        expect(customer.acquaintances._association.store_as).to eq("acquaintances")
+
+        expect(customer.close_friends[0].instance_eval { _association.store_as }).to eq("close_friends")
+        expect(customer.acquaintances[0].instance_eval { _association.store_as }).to eq("acquaintances")
+
+        customer.save!
+
+        customer.close_friends = [EmmFriend.new]
+        customer.acquaintances = [EmmFriend.new]
+
+        expect(customer.close_friends._association.store_as).to eq("close_friends")
+        expect(customer.acquaintances._association.store_as).to eq("acquaintances")
+
+        expect(customer.close_friends[0].instance_eval { _association.store_as }).to eq("close_friends")
+        expect(customer.acquaintances[0].instance_eval { _association.store_as }).to eq("acquaintances")
+      end
+    end
+
+    context "inverse assignment" do
+
+      it "raises an error when trying to set the association" do
+        customer.acquaintances = [EmmFriend.new]
+
+        expect do
+          customer.acquaintances[0].befriendable = customer
         end.to raise_error(Mongoid::Errors::AmbiguousRelationship)
       end
     end
