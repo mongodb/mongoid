52e80209c493024934f96162cf91cacb837813ca
Neil Shweky <neilshweky@gmail.com>
2022-03-30 17:50:13 -0400

    MONGOID-4716 Fix eager loading of nested referenced associations (#5194)
    * MONGOID-4716 failing test
    
    * MONGOID-4716 fix nested eager loading
    
    * MONGOID-4716 fix bugs and clean tests
    
    * MONGOID-4716 add has_many tests

--- lib/mongoid/association/eager_loadable.rb
+++ lib/mongoid/association/eager_loadable.rb
@@ -20,13 +20,31 @@ module Mongoid
         end
       end
 
-      def preload(relations, docs)
-        relations.group_by(&:inverse_class_name)
-            .values
-            .each do |associations|
-          associations.group_by(&:relation)
-              .each do |relation, association|
-            relation.eager_loader(association, docs).run
+      # Load the associations for the given documents. This will be done
+      # recursively to load the associations of the given documents'
+      # subdocuments.
+      #
+      # @param [ Array<Association> ] association The associations to load.
+      # @param [ Array<Document> ] document The documents.
+      def preload(associations, docs)
+        assoc_map = associations.group_by(&:inverse_class_name)
+        docs_map = { klass.to_s => docs.to_set }
+        queue = [ klass.to_s ]
+
+        while klass = queue.shift
+          if as = assoc_map.delete(klass)
+            as.group_by(&:relation)
+            .each do |relation, assocs|
+              assocs.each { |a| queue << a.class_name }
+
+              docs = docs_map[klass] || []
+              res = relation.eager_loader(assocs, docs.to_a).run
+
+              res.group_by(&:class).each do |k, vs|
+                docs_map[k.to_s] ||= [].to_set
+                docs_map[k.to_s].merge(vs)
+              end
+            end
           end
         end
       end
