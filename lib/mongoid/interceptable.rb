959ff6c6bdb73bc6bab1a980cfc39d8ceb979f63
Neil Shweky <neilshweky@gmail.com>
2022-04-12 13:11:23 -0400

    MONGOID-3267 remove white space

--- lib/mongoid/interceptable.rb
+++ lib/mongoid/interceptable.rb
@@ -168,7 +168,6 @@ module Mongoid
       @pending_callbacks = value
     end
 
-
     # Run the pending callbacks. If the callback is :apply_defaults, we will apply
     # the defaults for this document. Otherwise, the callback is passed to the
     # run_callbacks function.
