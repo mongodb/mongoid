7e6b10ec20f77c6066623aeb7c1b7fcfe8774906
Neil Shweky <neilshweky@gmail.com>
2022-04-13 10:59:58 -0400

    MONGOID-4686 deprecate Document#to_a on 7.4 (#5218)
    * MONGOID-4686 deprecate to_a
    
    * MONGOID-4686 create 7.5 release notes

--- lib/mongoid/document.rb
+++ lib/mongoid/document.rb
@@ -143,6 +143,8 @@ module Mongoid
     #   document.to_a
     #
     # @return [ Array<Document> ] An array with the document as its only item.
+    #
+    # @deprecated
     def to_a
       [ self ]
     end
