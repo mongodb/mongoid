fd2719832711a7317616a7518baf38b44484c9fa
shields <shields@tablecheck.com>
2022-04-21 00:06:13 +0900

    Add class-level mongoizer for ActionController::Parameters

--- lib/mongoid/extensions/action_controller_parameters.rb
+++ lib/mongoid/extensions/action_controller_parameters.rb
@@ -13,10 +13,26 @@ module Mongoid
       def mongoize
         Hash.mongoize(self)
       end
+
+      module ClassMethods
+
+        # Turn the object from the Ruby type into a Mongo-friendly type.
+        #
+        # @example Mongoize the object.
+        #   ActionController::Parameters.mongoize([ 1, 2, 3 ])
+        #
+        # @param [ Object ] object The object to mongoize.
+        #
+        # @return [ Hash ] The object mongoized.
+        def mongoize(object)
+          Hash.mongoize(object)
+        end
+      end
     end
   end
 end
 
 if defined?(::ActionController::Parameters)
   ::ActionController::Parameters.__send__(:include, Mongoid::Extensions::ActionControllerParameters)
+  ::ActionController::Parameters.extend(Mongoid::Extensions::ActionControllerParameters::ClassMethods)
 end
