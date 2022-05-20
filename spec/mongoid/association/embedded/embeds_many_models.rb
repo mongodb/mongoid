5c1c60fc9f2bc8656e78187f50bd942773df93d8
Neil Shweky <neilshweky@gmail.com>
2022-05-20 15:20:56 -0400

    MONGOID-5352 assign_attributes is working incorrectly since 7.3.4 (#5274)
    * MONGOID-5352 add fix for this issue, other tests failing
    
    * MONGOID-5352 fix two tests, two to go
    
    * MONGOID-5352 fix all tests
    
    * MONGOID-5352 fix some tests
    
    * MONGOID-5352 don't set caches before setting association
    
    * MONGOID-5352 rework solution
    
    * Update lib/mongoid/association/embedded/batchable.rb
    
    * MONGOID-5352 answer comments
    
    * MONGOID-5352 update comment

--- spec/mongoid/association/embedded/embeds_many_models.rb
+++ spec/mongoid/association/embedded/embeds_many_models.rb
@@ -162,3 +162,29 @@ class EmmHatch
   # No :class_name option on this association intentionally.
   embedded_in :tank
 end
+
+class EmmPost
+  include Mongoid::Document
+
+  embeds_many :company_tags, class_name: "EmmCompanyTag"
+  embeds_many :user_tags, class_name: "EmmUserTag"
+end
+
+
+class EmmCompanyTag
+  include Mongoid::Document
+
+  field :title, type: String
+
+  embedded_in :post, class_name: "EmmPost"
+end
+
+
+class EmmUserTag
+  include Mongoid::Document
+
+  field :title, type: String
+
+  embedded_in :post, class_name: "EmmPost"
+end
+
