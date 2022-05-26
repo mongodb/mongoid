6447b9ccb1faade06c7cbd00e718c4718ac5ae54
Johnny Shields <johnny.shields@gmail.com>
2022-05-27 01:45:30 +0900

    Update attributes.rb

--- lib/mongoid/attributes.rb
+++ lib/mongoid/attributes.rb
@@ -359,10 +359,10 @@ module Mongoid
       value.present?
     end
 
-    def __read_attribute__(name, field = nil, write_default_value = false)
+    def __read_attribute__(name, field = nil, called_from_getter = false)
       raw = read_raw_attribute(name)
       field ||= fields[name.to_s]
-      if field && write_default_value && lazy_settable?(field, raw)
+      if field && called_from_getter && lazy_settable?(field, raw)
         write_attribute(name, field.eval_default(self))
       else
         value = field ? field.demongoize(raw) : raw
