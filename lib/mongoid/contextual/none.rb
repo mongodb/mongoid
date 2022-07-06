e80671639d1475d239d470a365e11847c9d17f41
shields <shields@tablecheck.com>
2022-07-06 11:59:58 +0900

    Follow-ups to tally PR: - Remove unused variable "k" in #extract value - Add missing code comment - Minor code comment fixes

--- lib/mongoid/contextual/none.rb
+++ lib/mongoid/contextual/none.rb
@@ -100,10 +100,10 @@ module Mongoid
       # @example Get the values for null context.
       #   context.tally(:name)
       #
-      # @param [ String, Symbol ] arg Field to tally.
+      # @param [ String, Symbol ] _field Field to tally.
       #
       # @return [ Hash ] An empty Hash.
-      def tally(arg)
+      def tally(_field)
         {}
       end
 
