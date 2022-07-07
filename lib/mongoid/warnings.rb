5260338b215fdadc11fa2ab7c735779f1f8e94cd
Neil Shweky <neilshweky@gmail.com>
2022-07-07 15:07:54 -0400

    MONGOID-4998 add mongoid warning module

---
+++ lib/mongoid/warnings.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module Mongoid
+
+  # Encapsulates behavior around logging and caching warnings so they are only
+  # logged once.
+  #
+  # @api private
+  module Warnings
+
+    class << self
+      def add_warning(id, message)
+        singleton_class.class_eval do
+          define_method("warn_#{id}") do
+            unless instance_variable_get("@#{id}")
+              Mongoid.logger.warn(message)
+              instance_variable_set("@#{id}", true)
+            end
+          end
+        end
+      end
+    end
+
+    add_warning :id_sort_deprecated, 'The :id_sort option has been deprecated. Use Mongo#take to get a document without a sort on _id.'
+  end
+end
+
