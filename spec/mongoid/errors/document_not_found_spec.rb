559faf6be202b14293b1ea6d0197b4101552874a
Neil Shweky <neilshweky@gmail.com>
2022-07-08 12:58:20 -0400

    MONGOID-4547 fix document_not_found tests

--- spec/mongoid/errors/document_not_found_spec.rb
+++ spec/mongoid/errors/document_not_found_spec.rb
@@ -146,13 +146,13 @@ describe Mongoid::Errors::DocumentNotFound do
 
       it "contains the summary in the message" do
         expect(error.message).to include(
-          "Mongoid attempted to find a document from the class Person but none exist."
+          "Mongoid attempted to find a document of the class Person but none exist."
         )
       end
 
       it "contains the resolution in the message" do
         expect(error.message).to include(
-          "Create a document of class Person."
+          "Create a document of class Person or use a finder method that returns nil when no documents are found instead of raising an exception."
         )
       end
     end
