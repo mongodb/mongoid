cd933f824ce3e9eca248130bbbd9cec6f2f8770c
Neil Shweky <neilshweky@gmail.com>
2022-07-10 02:14:05 -0400

    MONGOID-5222 fix boolean mongoize

--- lib/mongoid/extensions/boolean.rb
+++ lib/mongoid/extensions/boolean.rb
@@ -13,7 +13,7 @@ module Mongoid
       #
       # @return [ true | false | nil ] The object mongoized or nil.
       def mongoize(object)
-        return if object.blank?
+        return if object.nil?
         if object.to_s =~ (/\A(true|t|yes|y|on|1|1.0)\z/i)
           true
         elsif object.to_s =~ (/\A(false|f|no|n|off|0|0.0)\z/i)
