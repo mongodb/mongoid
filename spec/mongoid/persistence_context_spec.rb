ef6e8206368c10e19c76774858c00f4eab6fd389
Neil Shweky <neilshweky@gmail.com>
2022-08-03 15:10:52 -0400

    MONGOID-5438 retry_test

--- spec/mongoid/persistence_context_spec.rb
+++ spec/mongoid/persistence_context_spec.rb
@@ -121,7 +121,7 @@ describe Mongoid::PersistenceContext do
         end
 
         context 'when the cluster is not the same as that of the persistence context on the current thread' do
-          retry_tests
+          retry_test
 
           let!(:client) do
             persistence_context.client
