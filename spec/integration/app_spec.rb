e8a908f2ce95d9000926abe0b3a8859ebb2ce0a5
Jamis Buck <jamis.buck@mongodb.com>
2023-02-03 15:29:18 -0700

    revert changes to mongoid.yml (in favor of #5539)

--- spec/integration/app_spec.rb
+++ spec/integration/app_spec.rb
@@ -127,15 +127,6 @@ describe 'Mongoid application tests' do
         config_text = File.read(mongoid_config_file)
         config_text.should =~ /mongoid_test_config_development/
         config_text.should =~ /mongoid_test_config_test/
-
-        # deprecated options should not be included
-        config_text.should_not =~ /background_indexing/
-
-        # make sure the different option types are emitted
-        config_text.should =~ /# app_name: nil/
-        config_text.should =~ /# discriminator_key: "_type"/
-        config_text.should =~ /# join_contexts: false/
-        config_text.should =~ /# log_level: :info/
       end
     end
 
