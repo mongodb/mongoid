b95b7facc0447d88381a6ca583d59756bede08f6
Johnny Shields <johnny.shields@gmail.com>
2023-02-22 16:49:56 +0900

    Revert gems

--- gemfiles/standard.rb
+++ gemfiles/standard.rb
@@ -15,9 +15,17 @@ def standard_dependencies
     # Evergreen configuration generation
     gem 'erubi'
     gem 'tilt'
+    gem 'solargraph'
   end
 
   group :development, :test do
+    gem 'rspec-core', '~> 3.10'
+    gem 'rubocop', '~> 1.45.1'
+    gem 'rubocop-performance', '~> 1.16.0'
+    gem 'rubocop-rails', '~> 2.17.4'
+    gem 'rubocop-rake', '~> 0.6.0'
+    gem 'rubocop-rspec', '~> 2.18.1'
+
     platform :mri do
       gem 'byebug'
     end
