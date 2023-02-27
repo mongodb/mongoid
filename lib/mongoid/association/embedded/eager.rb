555714ef756ca2c76e25cf507730dfd1a7f62dba
Jamis Buck <jamis.buck@mongodb.com>
2023-02-27 16:58:15 -0700

    Fix the comments on the Embedded::Eager implementation

--- lib/mongoid/association/embedded/eager.rb
+++ lib/mongoid/association/embedded/eager.rb
@@ -4,13 +4,16 @@ module Mongoid
   module Association
     module Embedded
 
-      # Eager class for has_many associations.
+      # Eager class for embedded associations (embedded_in, embeds_many,
+      # embeds_one).
       class Eager < Association::Eager
 
         private
 
+        # Embedded associations have no preload phase, since the embedded
+        # documents are loaded with the parent document. This method is
+        # implemented as a no-op to represent that.
         def preload
-          # Embedded associations have no preload phase
         end
       end
 
