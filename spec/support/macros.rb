fbf5c19ae4f1e63f32df179e1bbf2924e7d230df
johnnyshields <johnny.shields@gmail.com>
2023-04-11 22:30:57 +0900

    Mongoid 9.0: Remove Mongoid Query Cache, replace with Mongo

--- spec/support/macros.rb
+++ spec/support/macros.rb
@@ -73,7 +73,7 @@ module Mongoid
 
     def query_cache_enabled
       around do |example|
-        Mongoid::QueryCache.cache do
+        Mongo::QueryCache.cache do
           example.run
         end
       end
@@ -81,10 +81,10 @@ module Mongoid
 
     def override_query_cache(enabled)
       around do |example|
-        cache_enabled = Mongoid::QueryCache.enabled?
-        Mongoid::QueryCache.enabled = enabled
+        cache_enabled = Mongo::QueryCache.enabled?
+        Mongo::QueryCache.enabled = enabled
         example.run
-        Mongoid::QueryCache.enabled = cache_enabled
+        Mongo::QueryCache.enabled = cache_enabled
       end
     end
 
