e1cf309b338c11711021e86882da228098c0449b
Dmitry Rybakov <dmitry.rybakov@mongodb.com>
2023-05-04 09:07:06 +0200

    MONGOID-5581 Remove rspec-mocks-diag gem (#5613)

--- gemfiles/standard.rb
+++ gemfiles/standard.rb
@@ -16,11 +16,11 @@ def standard_dependencies
     # Evergreen configuration generation
     gem 'erubi'
     gem 'tilt'
-    gem 'solargraph'
+    gem 'solargraph', platform: :mri
   end
 
   group :development, :test do
-    gem 'rspec-core', '~> 3.10'
+    gem 'rspec', '~> 3.12'
     gem 'rubocop', '~> 1.45.1'
     gem 'rubocop-performance', '~> 1.16.0'
     gem 'rubocop-rails', '~> 2.17.4'
@@ -40,8 +40,6 @@ def standard_dependencies
     gem 'timecop'
     gem 'rspec-retry'
     gem 'benchmark-ips'
-    gem 'rspec-expectations', '~> 3.7', '>= 3.8.4'
-    gem 'rspec-mocks-diag', '~> 3.0'
     gem 'fuubar'
     gem 'rfc'
     gem 'childprocess'
