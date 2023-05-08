7f4a852ae7c0acb020253872fc8b649fdfaca16e
Dmitry Rybakov <dmitry.rybakov@mongodb.com>
2023-05-08 09:38:08 +0200

    Fix code review remarks

--- lib/mongoid/clients/factory.rb
+++ lib/mongoid/clients/factory.rb
@@ -56,10 +56,7 @@ module Mongoid
         raise Errors::NoClientsConfig.new unless configuration
         config = configuration.dup
         uri = config.delete(:uri)
-        database = config.delete(:database)
-        if database.nil?
-          database = Mongo::URI.get(uri).database
-        end
+        database = config.delete(:database) || Mongo::URI.get(uri).database
         hosts = config.delete(:hosts)
         opts = config.delete(:options) || {}
         if opts.key?(:auto_encryption_options)
