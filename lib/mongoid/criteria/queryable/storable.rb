d62d5eb934affed721d27779d63bf949a8e0dbe5
John Maguire <john@xap.ie>
2023-07-06 12:42:07 +0100

    Mongoid::Criteria::Queryable::Storable#add_field_expression when value is a hash with symbol operator key add values with same operator keys using

--- lib/mongoid/criteria/queryable/storable.rb
+++ lib/mongoid/criteria/queryable/storable.rb
@@ -48,7 +48,7 @@ module Mongoid
             if value.is_a?(Hash) && selector[field].is_a?(Hash) &&
               value.keys.all? { |key|
                 key_s = key.to_s
-                key_s.start_with?('$') && !selector[field].key?(key_s)
+                key_s.start_with?('$') && !selector[field].keys.map(&:to_s).include?(key_s)
               }
             then
               # Multiple operators can be combined on the same field by
