87c7d9a8975da1719dfebc006520574ddc1144f3
Dmitry Rybakov <dmitry.rybakov@mongodb.com>
2023-07-07 16:23:53 +0200

    RUBY-5621 Fix disconnect_clients spec (#5659)

--- spec/mongoid_spec.rb
+++ spec/mongoid_spec.rb
@@ -51,10 +51,8 @@ describe Mongoid do
     end
 
     it "disconnects from all active clients" do
-      pending 'https://jira.mongodb.org/browse/MONGOID-5621'
-
       clients.each do |client|
-        expect(client.cluster).to receive(:disconnect!).and_call_original
+        expect(client).to receive(:close).and_call_original
       end
       Mongoid.disconnect_clients
     end
