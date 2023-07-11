c7807f0564c5dc914e8de78dc50e259169663f98
Jamis Buck <jamis.buck@mongodb.com>
2023-07-11 09:39:49 -0600

    Prepare for release of 8.0.5 (#5662)
    * version bump
    
    * check for signed gems without resorting to Docker

--- release.sh
+++ release.sh
@@ -17,32 +17,14 @@ VERSION=`ruby -Ilib -r$VERSION_REQUIRE -e "puts $VERSION_CONSTANT_NAME"`
 echo "Releasing $NAME $VERSION"
 echo
 
-for variant in mri; do
-  docker build -f release/$variant/Dockerfile -t $RELEASE_NAME-$variant .
-
-  docker kill $RELEASE_NAME-$variant || true
-  docker container rm $RELEASE_NAME-$variant || true
-
-  docker run -d --name $RELEASE_NAME-$variant -it $RELEASE_NAME-$variant
-
-  docker exec $RELEASE_NAME-$variant /app/release/$variant/build.sh
-
-  if test $variant = jruby; then
-    docker cp $RELEASE_NAME-$variant:/app/pkg/$NAME-$VERSION-java.gem .
-  else
-    docker cp $RELEASE_NAME-$variant:/app/pkg/$NAME-$VERSION.gem .
-  fi
-
-  docker kill $RELEASE_NAME-$variant
-done
+./release/mri/build.sh
+cp pkg/$NAME-$VERSION.gem .
 
 echo
 echo Built: $NAME-$VERSION.gem
-#echo Built: $NAME-$VERSION-java.gem
 echo
 
 git tag -a v$VERSION -m "Tagging release: $VERSION"
 git push origin v$VERSION
 
 gem push $NAME-$VERSION.gem
-#gem push $NAME-$VERSION-java.gem
