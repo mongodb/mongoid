bbd88c7211b04e3be472863888bf306f1bdbd02a
Jamis Buck <jamis.buck@mongodb.com>
2023-08-24 15:24:05 -0600

    no need to use Docker to check if a gem is signed

--- release/mri/build.sh
+++ release/mri/build.sh
@@ -4,7 +4,4 @@ set -e
 
 rm -f *.lock
 rm -f *.gem pkg/*.gem
-bundle install --without=test
-# Uses bundler gem tasks, outputs the built gem file to pkg subdir.
-rake build
-/app/release/verify-signature.sh pkg/*.gem
+rake build verify
