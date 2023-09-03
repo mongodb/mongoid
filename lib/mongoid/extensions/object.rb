a5751be6dff221f01d568047d179c5435360c469
johnnyshields <27655+johnnyshields@users.noreply.github.com>
2023-09-03 16:37:50 +0900

    Remove Object#__setter__ monkey-patch. This is a trivial method so I've just inlined it. (It was already inlined in several other places.)

--- lib/mongoid/extensions/object.rb
+++ lib/mongoid/extensions/object.rb
@@ -44,16 +44,6 @@ module Mongoid
         self
       end
 
-      # Try to form a setter from this object.
-      #
-      # @example Try to form a setter.
-      #   object.__setter__
-      #
-      # @return [ String ] The object as a string plus =.
-      def __setter__
-        "#{self}="
-      end
-
       # Get the value of the object as a mongo friendly sort value.
       #
       # @example Get the object as sort criteria.
