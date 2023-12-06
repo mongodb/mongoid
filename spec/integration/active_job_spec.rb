ccf59de77409a6a08b3e2b1909b618bf015af85a
Dmitry Rybakov <dmitry.rybakov@mongodb.com>
2023-12-06 11:43:20 +0100

    Revert some changes since we drop 5.2

--- spec/integration/active_job_spec.rb
+++ spec/integration/active_job_spec.rb
@@ -2,13 +2,10 @@
 # rubocop:todo all
 
 require 'spec_helper'
+require 'active_job'
+require 'mongoid/railties/bson_object_id_serializer'
 
 describe 'ActiveJob Serialization' do
-  min_rails_version '6.0'
-
-  require 'active_job'
-  require 'mongoid/railties/bson_object_id_serializer'
-
   skip unless defined?(ActiveJob)
 
   class TestBsonObjectIdSerializerJob < ActiveJob::Base
