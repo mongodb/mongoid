ddad43482a0634a1af526bbf6cec1edde35c1559
Dmitry Rybakov <dmitry.rybakov@mongodb.com>
2024-01-23 16:54:18 +0100

    MONGOID-5735 Deprecate aggregable API

--- lib/mongoid/contextual/aggregable/none.rb
+++ lib/mongoid/contextual/aggregable/none.rb
@@ -15,9 +15,11 @@ module Mongoid
         # @param [ String | Symbol ] _field The field name.
         #
         # @return [ Hash ] A Hash with count, sum of 0 and max, min, avg of nil.
+        # @deprecated
         def aggregates(_field)
           Aggregable::EMPTY_RESULT.dup
         end
+        Mongoid.deprecate(self, :aggregates)
 
         # Always returns zero.
         #
@@ -26,9 +28,11 @@ module Mongoid
         # @param [ Symbol ] _field The field to sum.
         #
         # @return [ Integer ] Always zero.
+        # @deprecated
         def sum(_field = nil)
           0
         end
+        Mongoid.deprecate(self, :sum)
 
         # Always returns nil.
         #
@@ -37,9 +41,11 @@ module Mongoid
         # @param [ Symbol ] _field The field to avg.
         #
         # @return [ nil ] Always nil.
+        # @deprecated
         def avg(_field)
           nil
         end
+        Mongoid.deprecate(self, :avg)
 
         # Always returns nil.
         #
@@ -48,9 +54,11 @@ module Mongoid
         # @param [ Symbol ] _field The field to min.
         #
         # @return [ nil ] Always nil.
+        # @deprecated
         def min(_field = nil)
           nil
         end
+        Mongoid.deprecate(self, :min)
 
         # Always returns nil.
         #
@@ -59,7 +67,9 @@ module Mongoid
         # @param [ Symbol ] _field The field to max.
         #
         # @return [ nil ] Always nil.
+        # @deprecated
         alias :max :min
+        Mongoid.deprecate(self, :max)
       end
     end
   end
