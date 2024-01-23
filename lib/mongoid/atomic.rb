622efa026e85511468e588a6ebd62a170eed7ac7
Dmitry Rybakov <dmitry.rybakov@mongodb.com>
2024-01-23 16:04:31 +0100

    MONGOID-5709 Fix atomic_path calculation (#5773) (#5775)

--- lib/mongoid/atomic.rb
+++ lib/mongoid/atomic.rb
@@ -178,13 +178,15 @@ module Mongoid
     #
     # @return [ Object ] The associated path.
     def atomic_paths
-      @atomic_paths ||= begin
-        if _association
-          _association.path(self)
-        else
-          Atomic::Paths::Root.new(self)
-        end
-      end
+      return @atomic_paths if @atomic_paths
+
+      paths = if _association
+                _association.path(self)
+              else
+                Atomic::Paths::Root.new(self)
+              end
+
+      paths.tap { @atomic_paths = paths unless new_record? }
     end
 
     # Get all the attributes that need to be pulled.
--- spec/integration/associations/has_and_belongs_to_many_spec.rb
+++ spec/integration/associations/has_and_belongs_to_many_spec.rb
@@ -2,6 +2,28 @@
 
 require 'spec_helper'
 
+module HabtmSpec
+  class Page
+    include Mongoid::Document
+    embeds_many :blocks, class_name: 'HabtmSpec::Block'
+  end
+
+  class Block
+    include Mongoid::Document
+    embedded_in :page, class_name: 'HabtmSpec::Page'
+  end
+
+  class ImageBlock < Block
+    has_and_belongs_to_many :attachments, inverse_of: nil, class_name: 'HabtmSpec::Attachment'
+    accepts_nested_attributes_for :attachments
+  end
+
+  class Attachment
+    include Mongoid::Document
+    field :file, type: String
+  end
+end
+
 describe 'has_and_belongs_to_many associations' do
 
   context 'when an anonymous class defines a has_and_belongs_to_many association' do
@@ -16,6 +38,24 @@ describe 'has_and_belongs_to_many associations' do
       expect { klass }.to_not raise_error
       expect { klass.new.movies }.to_not raise_error
       expect(klass.new.movies.build).to be_a Movie
+    end
+  end
+
+  context 'when an embedded has habtm relation' do
+    let(:attachment) { HabtmSpec::Attachment.create!(file: 'foo.jpg') }
+
+    let(:page) { HabtmSpec::Page.create! }
+
+    let(:image_block) do
+      image_block = page.blocks.build({
+        _type: 'HabtmSpec::ImageBlock',
+        attachment_ids: [ attachment.id.to_s ],
+        attachments_attributes: { '1234' => { file: 'bar.jpg', id: attachment.id.to_s } }
+      })
+    end
+
+    it 'does not raise on save' do
+      expect { image_block.save! }.not_to raise_error
     end
   end
 end
