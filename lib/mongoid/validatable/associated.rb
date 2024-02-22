eff5a5f74e05e4f43900ac5364e2db99b455cda3
Jamis Buck <jamis.buck@mongodb.com>
2024-02-21 17:00:57 -0700

    tread very carefully so we don't trigger a load while validating

--- lib/mongoid/validatable/associated.rb
+++ lib/mongoid/validatable/associated.rb
@@ -49,7 +49,23 @@ module Mongoid
       # @param [ Symbol ] attribute The association to validate.
       def validate_association(document, attribute)
         valid = document.validating do
-          Array(document.ivar(attribute)).all? do |value|
+          # grab the proxy from the instance variable directly; we don't want
+          # any loading logic to run; we just want to see if it's already
+          # been loaded.
+          proxy = document.ivar(attribute)
+          next true unless proxy
+
+          # if the variable exists, now we see if it is a proxy, or an actual
+          # document. It might be a literal document instead of a proxy if this
+          # document was created with a Document instance as a provided attribute,
+          # e.g. "Post.new(message: Message.new)".
+          target = proxy.respond_to?(:_target) ? proxy._target : proxy
+          next true if target.respond_to?(:_loaded?) && !target._loaded?
+
+          # Now, treating the target as an array, look at each element
+          # and see if it is valid, but only if it has already been
+          # persisted, or changed, and hasn't been flagged for destroy.
+          Array.wrap(target).all? do |value|
             if value && !value.flagged_for_destroy? && (!value.persisted? || value.changed?)
               value.validated? ? true : value.valid?
             else
