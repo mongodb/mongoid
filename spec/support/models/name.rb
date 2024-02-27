cddda5f18c55c794cb905f3a271d621db88e6a21
Jamis Buck <jamis.buck@mongodb.com>
2024-02-27 12:02:15 -0700

    MONGOID-5740 Fix performance regression on belongs_to validation (backport to 8.0-stable) (#5794)
    * MONGOID-5704 Fix performance regression on belongs_to validation (#5787)
    
    * MONGOID-5704 don't try to validate persisted, unchanged associations
    
    * we just need the attribute name
    
    * fix failing specs
    
    * tread very carefully so we don't trigger a load while validating
    
    * hopefully fix has-many behavior
    
    * documentation
    
    * simplify
    
    * make sure attributes getter is defined
    
    * Fix typo -- records are not loaded *from the database*
    
    Co-authored-by: Dmitry Rybakov <160598371+comandeo-mongo@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Dmitry Rybakov <160598371+comandeo-mongo@users.noreply.github.com>
    
    * use `rake ci` instead of `rake spec` to avoid side effects
    
    ---------
    
    Co-authored-by: Dmitry Rybakov <160598371+comandeo-mongo@users.noreply.github.com>

--- spec/support/models/name.rb
+++ spec/support/models/name.rb
@@ -4,6 +4,8 @@ class Name
   include Mongoid::Document
   include Mongoid::Attributes::Dynamic
 
+  validate :is_not_jamis
+
   field :_id, type: String, overwrite: true, default: ->{
     "#{first_name}-#{last_name}"
   }
@@ -22,5 +24,13 @@ class Name
 
   def set_parent=(set = false)
     self.parent_title = namable.title if set
+  end
+
+  private
+
+  def is_not_jamis
+    if first_name == 'Jamis' && last_name == 'Buck'
+      errors.add(:base, :invalid)
+    end
   end
 end
