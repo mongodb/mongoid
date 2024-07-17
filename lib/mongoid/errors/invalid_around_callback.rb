877f41cc757bc697827b99e2a17fe0d69cb5017a
Adviti Mishra <adviti@umich.edu>
2024-07-17 10:38:32 -0400

    invalid around callback error file is linter-approved

--- lib/mongoid/errors/invalid_around_callback.rb
+++ lib/mongoid/errors/invalid_around_callback.rb
@@ -1,18 +1,15 @@
 # frozen_string_literal: true
-# rubocop:todo all
 
 module Mongoid
   module Errors
-
     # This error is raised when an around callback is
     # defined by the user without a yield
     class InvalidAroundCallback < MongoidError
-
       # Create the new error.
       #
       # @api private
       def initialize
-        super(compose_message("invalid_around_callback"))
+        super(compose_message('invalid_around_callback'))
       end
     end
   end
