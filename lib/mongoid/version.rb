bd8aca83305cc120a5bd41eccb02e7ce93f6fc20
Jamis Buck <jamis.buck@mongodb.com>
2025-06-17 16:20:40 -0600

    Wire up the new release automation stuff

--- lib/mongoid/version.rb
+++ lib/mongoid/version.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
-# rubocop:todo all
 
 module Mongoid
+  # The current version of Mongoid
+  #
+  # Note that this file is automatically updated via `rake candidate:create`.
+  # Manual changes to this file will be overwritten by that rake task.
   VERSION = "9.0.2"
 end
