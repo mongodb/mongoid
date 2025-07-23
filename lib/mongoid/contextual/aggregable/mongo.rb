6d2814d7e15596867418024eb275cf06debb4ccd
Akshay Goel <akshay58538@gmail.com>
2025-07-24 00:24:34 +0530

    Update mongo.rb to add hint in the aggregates

--- lib/mongoid/contextual/aggregable/mongo.rb
+++ lib/mongoid/contextual/aggregable/mongo.rb
@@ -27,7 +27,8 @@ module Mongoid
         #   If no documents are found, then returned Hash will have
         #   count, sum of 0 and max, min, avg of nil.
         def aggregates(field)
-          result = collection.aggregate(pipeline(field), session: _session).to_a
+          hint = options[:hint]
+          result = collection.aggregate(pipeline(field), session: _session, hint: hint).to_a
           if result.empty?
             Aggregable::EMPTY_RESULT.dup
           else
