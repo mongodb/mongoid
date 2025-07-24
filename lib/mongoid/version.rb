b28ea22f3efc31fee8404a1cbdb7eb31ac423f93
Jamis Buck <jamis.buck@mongodb.com>
2025-07-24 10:30:50 -0600

    Bump version to 8.0.11

--- lib/mongoid/version.rb
+++ lib/mongoid/version.rb
@@ -5,5 +5,5 @@ module Mongoid
   #
   # Note that this file is automatically updated via `rake candidate:create`.
   # Manual changes to this file will be overwritten by that rake task.
-  VERSION = '8.0.10'
+  VERSION = '8.0.11'
 end
