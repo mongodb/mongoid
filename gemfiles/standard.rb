400fd98b46650b99d246c035be521afbcdde9e11
Jamis Buck <jamis.buck@mongodb.com>
2025-09-08 15:01:58 -0600

    remove rubocop and associated workflow

--- gemfiles/standard.rb
+++ gemfiles/standard.rb
@@ -15,10 +15,6 @@ def standard_dependencies
 
   group :development, :test do
     gem 'rspec-core', '~> 3.10'
-    gem 'rubocop', '~> 1.45.1'
-    gem 'rubocop-performance', '~> 1.16.0'
-    gem 'rubocop-rake', '~> 0.6.0'
-    gem 'rubocop-rspec', '~> 2.18.1'
 
     platform :mri do
       gem 'byebug'
