bb95cc3f60ca18cc801b3a4d1e60761a6769f24e
Dmitry Rybakov <dmitry.rybakov@mongodb.com>
2025-11-17 10:39:31 +0100

    Bump version to 7.6.0

--- lib/mongoid/version.rb
+++ lib/mongoid/version.rb
@@ -5,5 +5,5 @@ module Mongoid
   #
   # Note that this file is automatically updated via `rake candidate:create`.
   # Manual changes to this file will be overwritten by that rake task.
-  VERSION = '7.5.4'
+  VERSION = '7.6.0'
 end
