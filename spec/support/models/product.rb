741ae8b6b9540b0e060caac898160784a46b3e83
Jamis Buck <jamis.buck@mongodb.com>
2025-11-21 08:46:51 -0700

    MONGOID-5900, MONGOID-5901 Fix #pluck on associations (#6067)
    * use the specialized implementation of pluck
    
    * use a custom pluck implementation
    
    * new file
    
    * incorporate Johnny's changes from #6044
    
    * use same submodules as master
    
    * Make changes suggested by copilot review
    
    * rubocop appeasement
    
    * fix broken refactoring

--- spec/support/models/product.rb
+++ spec/support/models/product.rb
@@ -18,4 +18,6 @@ class Product
   validates :website, format: { with: URI.regexp, allow_blank: true }
 
   embeds_one :seo, as: :seo_tags, cascade_callbacks: true, autobuild: true
+
+  belongs_to :company
 end
