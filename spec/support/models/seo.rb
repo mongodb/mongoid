2843c02c88e846a55b8b1549bb393b84a893b079
Jamis Buck <jamis.buck@mongodb.com>
2025-11-24 15:41:53 -0700

    MONGOID-5900, MONGOID-5901 Fix #pluck on associations (#6067) (#6068)
    * use the specialized implementation of pluck
    
    * use a custom pluck implementation
    
    * new file
    
    * incorporate Johnny's changes from #6044
    
    * use same submodules as master
    
    * Make changes suggested by copilot review
    
    * rubocop appeasement
    
    * fix broken refactoring

--- spec/support/models/seo.rb
+++ spec/support/models/seo.rb
@@ -5,6 +5,8 @@ class Seo
   include Mongoid::Document
   include Mongoid::Timestamps
   field :title, type: String
+  field :name, type: String, localize: true
+  field :desc, as: :description, type: String, localize: true
 
   embedded_in :seo_tags, polymorphic: true
 end
