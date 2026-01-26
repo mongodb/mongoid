f7a0d3556466b6a8b27add3e6b5b32377c8bf921
Dmitry Rybakov <160598371+comandeo-mongo@users.noreply.github.com>
2026-01-26 23:57:55 +0100

    Bump version to 8.1.12 (#6100)
    Co-authored-by: Jamis Buck <jamis.buck@mongodb.com>

--- lib/mongoid/version.rb
+++ lib/mongoid/version.rb
@@ -5,5 +5,5 @@ module Mongoid
   #
   # Note that this file is automatically updated via `rake candidate:create`.
   # Manual changes to this file will be overwritten by that rake task.
-  VERSION = '8.1.11'
+  VERSION = '8.1.12'
 end
