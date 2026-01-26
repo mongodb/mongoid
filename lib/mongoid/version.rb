0623af687a234392ed4b8872f474a9208b908500
Dmitry Rybakov <160598371+comandeo-mongo@users.noreply.github.com>
2026-01-27 00:04:05 +0100

    Bump version to 9.0.10 (#6099)

--- lib/mongoid/version.rb
+++ lib/mongoid/version.rb
@@ -5,5 +5,5 @@ module Mongoid
   #
   # Note that this file is automatically updated via `rake candidate:create`.
   # Manual changes to this file will be overwritten by that rake task.
-  VERSION = '9.0.9'
+  VERSION = '9.0.10'
 end
