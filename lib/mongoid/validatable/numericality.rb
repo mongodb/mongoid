6ee42dd7bc561f1a79bfc281a3b6e2759102ce90
Jamis Buck <jamis.buck@mongodb.com>
2026-01-30 13:03:57 -0700

    MONGOID-5842 fix numericality validator when given non-numeric strings
    Because Mongoid's typecasting functionality differs from that of Rails,
    the numericality validator was incorrectly accepting non-numeric strings
    if `allow_blank: true` was specified.

--- lib/mongoid/validatable/numericality.rb
+++ lib/mongoid/validatable/numericality.rb
@@ -5,6 +5,18 @@ module Mongoid
     # A specialization of the ActiveModel numericality validator, which adds
     # logic to recognize and accept BSON::Decimal128 as a number.
     class NumericalityValidator < ActiveModel::Validations::NumericalityValidator
+      # Reimplements EachValidator#validate in order to work around Mongoid's
+      # nonstandard type-casting of String values.
+      def validate(record)
+        attributes.each do |attribute|
+          value = raw_value_for_validation(record, attribute)
+          next if (value.nil? && options[:allow_nil]) || (value.blank? && options[:allow_blank])
+
+          value = prepare_value_for_validation(value, record, attribute)
+          validate_each(record, attribute, value)
+        end
+      end
+
       private
 
       # Ensure that BSON::Decimal128 is treated as a BigDecimal during the
@@ -13,6 +25,13 @@ module Mongoid
         result = super
 
         result.is_a?(BSON::Decimal128) ? result.to_big_decimal : result
+      end
+
+      def raw_value_for_validation(record, attr)
+        attribute = record.database_field_name(attr)
+        return nil if record.relations.key?(attribute)
+
+        record.read_attribute_before_type_cast(attribute)
       end
     end
   end
