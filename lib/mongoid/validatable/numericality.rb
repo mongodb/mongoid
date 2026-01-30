7467dfddde90a44cea11c716889529c1c97ee021
Jamis Buck <jamis.buck@mongodb.com>
2026-01-30 14:53:43 -0700

    accommodate Rails 6.0

--- lib/mongoid/validatable/numericality.rb
+++ lib/mongoid/validatable/numericality.rb
@@ -7,6 +7,8 @@ module Mongoid
     class NumericalityValidator < ActiveModel::Validations::NumericalityValidator
       # Reimplements EachValidator#validate in order to work around Mongoid's
       # nonstandard type-casting of String values.
+      #
+      # @param [ Document ] record The document to validate.
       def validate(record)
         attributes.each do |attribute|
           value = raw_value_for_validation(record, attribute)
@@ -21,12 +23,12 @@ module Mongoid
 
       # Ensure that BSON::Decimal128 is treated as a BigDecimal during the
       # validation step.
-      def prepare_value_for_validation(value, record, attr_name)
-        result = super
-
-        result.is_a?(BSON::Decimal128) ? result.to_big_decimal : result
+      def prepare_value_for_validation(value, _record, _attr_name)
+        value.is_a?(BSON::Decimal128) ? value.to_big_decimal : value
       end
 
+      # Fetches the raw value from the record, prior to any typecasting being
+      # applied.
       def raw_value_for_validation(record, attr)
         attribute = record.database_field_name(attr)
         return nil if record.relations.key?(attribute)
