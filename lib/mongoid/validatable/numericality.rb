a0f88c5f361cb016c29d465335227b0a5227467a
Jamis Buck <jamis.buck@mongodb.com>
2026-02-03 14:40:30 -0700

    MONGOID-5842 fix numericality validator when given non-numeric strings (#6108)
    * MONGOID-5842 fix numericality validator when given non-numeric strings
    
    Because Mongoid's typecasting functionality differs from that of Rails,
    the numericality validator was incorrectly accepting non-numeric strings
    if `allow_blank: true` was specified.
    
    * Add a helper for creating one-off test model classes
    
    * accommodate Rails 6.0

--- lib/mongoid/validatable/numericality.rb
+++ lib/mongoid/validatable/numericality.rb
@@ -5,14 +5,35 @@ module Mongoid
     # A specialization of the ActiveModel numericality validator, which adds
     # logic to recognize and accept BSON::Decimal128 as a number.
     class NumericalityValidator < ActiveModel::Validations::NumericalityValidator
+      # Reimplements EachValidator#validate in order to work around Mongoid's
+      # nonstandard type-casting of String values.
+      #
+      # @param [ Document ] record The document to validate.
+      def validate(record)
+        attributes.each do |attribute|
+          value = raw_value_for_validation(record, attribute)
+          next if (value.nil? && options[:allow_nil]) || (value.blank? && options[:allow_blank])
+
+          value = prepare_value_for_validation(value, record, attribute)
+          validate_each(record, attribute, value)
+        end
+      end
+
       private
 
       # Ensure that BSON::Decimal128 is treated as a BigDecimal during the
       # validation step.
-      def prepare_value_for_validation(value, record, attr_name)
-        result = super
+      def prepare_value_for_validation(value, _record, _attr_name)
+        value.is_a?(BSON::Decimal128) ? value.to_big_decimal : value
+      end
 
-        result.is_a?(BSON::Decimal128) ? result.to_big_decimal : result
+      # Fetches the raw value from the record, prior to any typecasting being
+      # applied.
+      def raw_value_for_validation(record, attr)
+        attribute = record.database_field_name(attr)
+        return nil if record.relations.key?(attribute)
+
+        record.read_attribute_before_type_cast(attribute)
       end
     end
   end
--- spec/mongoid/validatable/numericality_spec.rb
+++ spec/mongoid/validatable/numericality_spec.rb
@@ -4,47 +4,142 @@
 require "spec_helper"
 
 describe ActiveModel::Validations::NumericalityValidator do
-
   describe "#validate_each" do
+    context "when allow_blank is false" do
+      let(:model_class) do
+        test_model do
+          field :amount, type: BigDecimal
+          validates_numericality_of :amount, allow_blank: false
+        end
+      end
 
-    before(:all) do
-      class TestModel
-        include Mongoid::Document
-        field :amount, type: BigDecimal
-        validates_numericality_of :amount, allow_blank: false
+      context "when the value is non numeric" do
+
+        let(:model) do
+          model_class.new(amount: "asdf")
+        end
+
+        it "returns false" do
+          expect(model).to_not be_valid
+        end
+      end
+
+      context 'when the value is numeric' do
+        let(:model) { model_class.new(amount: '15.0') }
+
+        it 'returns true' do
+          expect(model).to be_valid
+        end
+      end
+
+      context 'when the value is a BSON::Decimal128' do
+        min_rails_version '6.1'
+
+        let(:model) { model_class.new(amount: BSON::Decimal128.new('15.0')) }
+
+        it 'returns true' do
+          expect(model).to be_valid
+        end
       end
     end
 
-    after(:all) do
-      Object.send(:remove_const, :TestModel)
-    end
-
-    context "when the value is non numeric" do
-
-      let(:model) do
-        TestModel.new(amount: "asdf")
+    context "when allow_blank is true and amount is Integer" do
+      let(:model_class) do
+        test_model do
+          field :amount, type: Integer
+          validates_numericality_of :amount, allow_blank: true
+        end
       end
 
-      it "returns false" do
-        expect(model).to_not be_valid
+      context "when the value is blank" do
+
+        let(:model) do
+          model_class.new(amount: "")
+        end
+
+        it "returns true" do
+          expect(model).to be_valid
+        end
+      end
+
+      context "when the value is a nonempty string" do
+
+        let(:model) do
+          model_class.new(amount: "A non-numeric string")
+        end
+
+        it "returns false" do
+          expect(model).to_not be_valid
+        end
       end
     end
 
-    context 'when the value is numeric' do
-      let(:model) { TestModel.new(amount: '15.0') }
+    context 'when allow_nil is true' do
+      let(:model_class) do
+        test_model do
+          field :amount, type: Integer
+          validates_numericality_of :amount, allow_nil: true
+        end
+      end
 
-      it 'returns true' do
-        expect(model).to be_valid
+      context 'when the value is blank' do
+        let(:model) do
+          model_class.new(amount: '')
+        end
+
+        it 'is invalid' do
+          expect(model).to_not be_valid
+        end
+      end
+
+      context 'when the value is nil' do
+        let(:model) do
+          model_class.new(amount: nil)
+        end
+
+        it 'is valid' do
+          expect(model).to be_valid
+        end
+      end
+    end
+ 
+    context "when allow_blank is true and amount is Float" do
+      let(:model_class) do
+        test_model do
+          field :amount, type: Float
+          validates_numericality_of :amount, allow_blank: true
+        end
+      end
+
+      context "when the value is a nonempty string" do
+
+        let(:model) do
+          model_class.new(amount: "A non-numeric string")
+        end
+
+        it "returns false" do
+          expect(model).to_not be_valid
+        end
       end
     end
 
-    context 'when the value is a BSON::Decimal128' do
-      min_rails_version '6.1'
+    context "when allow_blank is true and amount is BigDecimal" do
+      let(:model_class) do
+        test_model do
+          field :amount, type: BigDecimal
+          validates_numericality_of :amount, allow_blank: true
+        end
+      end
 
-      let(:model) { TestModel.new(amount: BSON::Decimal128.new('15.0')) }
+      context "when the value is a nonempty string" do
 
-      it 'returns true' do
-        expect(model).to be_valid
+        let(:model) do
+          model_class.new(amount: "A non-numeric string")
+        end
+
+        it "returns false" do
+          expect(model).to_not be_valid
+        end
       end
     end
   end
