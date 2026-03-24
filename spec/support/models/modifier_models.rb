c9cec28070c9c5a9668f5ac89640768d19cd1bcf
Jamis Buck <jamis.buck@mongodb.com>
2026-03-24 08:49:52 -0600

    rubocop appeasement

--- spec/support/models/modifier_models.rb
+++ spec/support/models/modifier_models.rb
@@ -3,12 +3,14 @@
 module ModifierModels
   class Library
     include Mongoid::Document
+
     field :name, type: String
     embeds_many :books, class_name: 'ModifierModels::Book'
   end
 
   class Book
     include Mongoid::Document
+
     embedded_in :library, class_name: 'ModifierModels::Library'
     field :title, type: String
     embeds_one :foreword, class_name: 'ModifierModels::Foreword'
@@ -16,6 +18,7 @@ module ModifierModels
 
   class Foreword
     include Mongoid::Document
+
     embedded_in :book, class_name: 'ModifierModels::Book'
     field :text, type: String
   end
