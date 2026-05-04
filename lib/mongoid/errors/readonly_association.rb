cf23a93f9a39507de47b685edf09ba9b2037c5d7
Jamis Buck <jamis.buck@mongodb.com>
2026-05-04 13:18:27 -0600

    MONGOID-5057 Add YARD docs to ReadonlyAssociation#initialize

--- lib/mongoid/errors/readonly_association.rb
+++ lib/mongoid/errors/readonly_association.rb
@@ -5,6 +5,13 @@ module Mongoid
     # Raised when attempting to write to a :through association, which is
     # read-only.
     class ReadonlyAssociation < MongoidError
+      # Instantiate the exception.
+      #
+      # @example Create the error.
+      #   ReadonlyAssociation.new(Physician, association)
+      #
+      # @param [ Class ] klass The owner class.
+      # @param [ Mongoid::Association::Relatable ] association The through association.
       def initialize(klass, association)
         super(
           compose_message(
