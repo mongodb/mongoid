43e0ba1968788eca41a7b4454302eb0219c02664
Jamis Buck <jamis.buck@mongodb.com>
2026-05-04 13:20:51 -0600

    MONGOID-5057 Add THROUGH_MACRO_MAPPING and :through routing in define_association!

---
+++ lib/mongoid/association/referenced/has_one_through.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+module Mongoid
+  module Association
+    module Referenced
+      class HasOneThrough
+        include Relatable
+
+        ASSOCIATION_OPTIONS = %i[class_name source through scope].freeze
+        VALID_OPTIONS = (ASSOCIATION_OPTIONS + SHARED_OPTIONS).freeze
+
+        def embedded?
+          false
+        end
+
+        def setup!
+          self
+        end
+
+        def relation
+          Proxy
+        end
+
+        # Placeholder proxy class; replaced in a later task.
+        class Proxy # rubocop:disable Lint/EmptyClass
+        end
+      end
+    end
+  end
+end
