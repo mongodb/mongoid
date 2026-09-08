bbc2fac82d4ca1a1c7fcb698b665c51fa03a86e3
Jamis Buck <jamis.buck@mongodb.com>
2026-09-08 15:10:48 -0600

    MONGOID-5993 Close $where allowlist gap in the string form of where (8.0 backport)
    Backport 5993 to 8.0, along with the prerequisite pieces of MONGOID-5939 it depends on: the allow_unsafe_query_operators config option, the expr_query allowlist guard, and the string-form guard on Selectable#where. Skipped 5939's unrelated Gemfile change. Resolved a config.rb conflict where 8.0 has the allow_reparenting_via_nested_attributes option where 8.1 had the #config singleton method.

--- lib/mongoid/config.rb
+++ lib/mongoid/config.rb
@@ -181,6 +181,18 @@ module Mongoid
     # See https://jira.mongodb.org/browse/MONGOID-5981 for details.
     option :in_memory_regexp_time_limit, default: 5.0
 
+    # When true (default), all top-level query operators are passed through
+    # to MongoDB without restriction when using +where+/+find_by+. Set to
+    # false to enable a strict allowlist that rejects operators like +$where+
+    # and +$function+, which can execute arbitrary JavaScript when user-supplied
+    # input reaches the query builder. This also governs the string form of
+    # +where+ (e.g. +where("this.name == 'admin'")+), which compiles to
+    # +$where+ and is rejected the same way when this flag is false.
+    #
+    # See https://jira.mongodb.org/browse/MONGOID-5939 and
+    # https://jira.mongodb.org/browse/MONGOID-5993 for details.
+    option :allow_unsafe_query_operators, default: true
+
     # When this flag is true, it will be possible to add a record to a
     # "has_many" or "has_and_belongs_to_many" association by passing that
     # record's id in the nested attributes for another parent record, even
