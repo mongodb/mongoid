fb91c425e84fec83e698a744226f12c62784c49e
Jamis Buck <jamis.buck@mongodb.com>
2026-09-17 14:59:10 -0600

    version bump

--- lib/mongoid/version.rb
+++ lib/mongoid/version.rb
@@ -5,5 +5,5 @@ module Mongoid
   #
   # Note that this file is automatically updated via `rake candidate:create`.
   # Manual changes to this file will be overwritten by that rake task.
-  VERSION = '9.1.0'
+  VERSION = '9.1.1'
 end
